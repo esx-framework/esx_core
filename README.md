8c9bb80a9f441e129955df248deb2fbc60bfba52
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-07-31 16:05:49 +0200

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fxserver-esx_boilerplate
 FXSServer ESX Boilerplate
+
+This a sample script for es_extended
---
+++ client/main.lua
@@ -0,0 +1,9 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
