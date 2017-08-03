e8f64ce38d6c251d80880a43ffd315f3375091f9
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-08-03 16:29:51 +0200

    Fix spaces

--- client/main.lua
+++ client/main.lua
@@ -10,10 +10,10 @@ end)
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
-    PlayerData = xPlayer   
+  PlayerData = xPlayer   
 end)
 
 RegisterNetEvent('esx:setJob')
 AddEventHandler('esx:setJob', function(job)
-    PlayerData.job = job
+  PlayerData.job = job
 end)
