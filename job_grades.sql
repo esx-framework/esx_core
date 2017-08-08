7015139b3399fb5c0b1a4ef21c893df8e5ce0583
renaiku <corbeau.lenaic@gmail.com>
2017-08-08 20:04:58 +0200

    sql

---
+++ job_grades.sql
@@ -0,0 +1,9 @@
+INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+('unemployed', 0, 'rsa', 'RSA', 0, '{}', '{}'),
+('lumberjack', 0, 'interim', 'Intérimaire', 0, '{}', '{}'),
+('fisherman', 0, 'interim', 'Intérimaire', 0, '{}', '{}'),
+('fuel', 0, 'interim', 'Intérimaire', 0, '{}', '{}'),
+('reporter', 0, 'employee', 'Intérimaire', 0, '{}', '{}'),
+('textil', 0, 'interim', 'Intérimaire', 0, '{}', '{}'),
+('miner', 0, 'interim', 'Intérimaire', 0, '{}', '{}'),
+('slaughterer', 0, 'interim', 'Intérimaire', 0, , '{}', '{}');
---
+++ jobs.sql
@@ -0,0 +1,9 @@
+INSERT INTO `jobs` (`name`, `label`) VALUES
+('unemployed', 'Chômeur'),
+('slaughterer', 'Abatteur'),
+('fisherman', 'Pêcheur'),
+('miner', 'Mineur'),
+('lumberjack', 'Bûcheron'),
+('fuel', 'Raffineur'),
+('reporter', 'Journaliste'),
+('textil', 'Couturier'),
