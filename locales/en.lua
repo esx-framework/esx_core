dcb3c667408b8202b0cfefbe75794877f639f728
angerthosenear <angerthosenear@gmail.com>
2017-08-13 17:34:04 -0500

    Grammatical fix (en)

--- locales/en.lua
+++ locales/en.lua
@@ -2,5 +2,5 @@ Locales['en'] = {
 	['invalid_amount']    = '~r~invalid amount',
 	['deposit_money']          = 'you have deposited ~g~$',
 	['withdraw_money']         = 'you withdrawn ~g~$', 
-	['press_e_atm'] = 'press sure ~INPUT_PICKUP~ for deposit or withdraw from ~g~ATM~s~.',  
+	['press_e_atm'] = 'press ~INPUT_PICKUP~ for deposit or withdrawal from ~g~ATM~s~.',  
 }
