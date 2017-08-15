086c4619ab8dc189f133b7233207bf47438652a6
Don <don@edge-gamers.com>
2017-08-15 07:23:05 -0700

    Multi-language support
    Added multi-language support

--- config.lua
+++ config.lua
@@ -6,6 +6,7 @@ Config.MarkerColor            = {r = 50, g = 50, b = 204}
 Config.EnablePlayerManagement = false
 Config.EnableArmoryManagement = false
 Config.MaxInService           = -1
+Config.Locale       		  = 'en'
 
 Config.PoliceStations = {
 	
---
+++ locales/fr.lua
@@ -0,0 +1,91 @@
+Locales['fr'] = {
+	--Cloakroom
+		['cloakroom'] = 'vestiaire',
+		['citizen_wear'] = 'tenue Civil',
+		['police_wear'] = 'tenue Policier',
+		['open_cloackroom'] = 'appuez sur ~INPUT_CONTEXT~ pour vous changer',
+	--Armory
+		['get_weapon'] = 'prendre Arme',
+		['put_weapon'] = 'déposer Arme',
+		['buy_weapons'] = 'acheter Armes',
+		['armory'] = 'armurerie',
+		['open_armory'] = 'appuez sur ~INPUT_CONTEXT~ pour accéder à l\'armurerie',
+	--Vehicles
+		['vehicle_menu'] = 'véhicule',
+		['vehicle_out'] = 'il y a déja un véhicule de sorti',
+		['vehicle_spawner'] = 'appuez sur ~INPUT_CONTEXT~ pour sortir un véhicule',
+		['store_vehicle'] = 'appuez sur ~INPUT_CONTEXT~ pour ranger le véhicule',
+		['service_max'] = 'Service complet : ',
+	--Action Menu
+		['citizen_interaction'] = 'interaction citoyen',
+		['vehicle_interaction'] = 'interaction véhicule',
+		['object_spawner'] = 'placer objets',
+
+		['id_card'] = 'carte d\'identité',
+		['search'] = 'fouiller',
+		['handcuff'] = 'menotter / Démenotter',
+		['put_in_vehicle'] = 'mettre dans véhicule',
+		['fine'] = 'Amende',
+		['no_players_nearby'] = 'aucun joueur à proximité',
+
+		['vehicle_info'] = 'infos véhicule',
+		['pick_lock'] = 'crocheter véhicule',
+		['vehicle_unlocked'] = 'véhicule ~g~déverouillé~s~',
+		['no_vehicles_nearby'] = 'aucun véhicule à proximité',
+		['traffic_interaction'] = 'Interaction Voirie',
+		['cone'] = 'plot',
+		['barrier'] = 'barrière',
+		['spikestrips'] = 'herse',
+		['box'] = 'caisse',
+		['cash'] = 'caisse',
+	--ID Card Menu
+		['name'] = 'nom : ',
+		['bac'] = 'alcoolémie : ',
+	--Body Search Menu
+		['confiscate_dirty'] = 'confisquer argent sale : $',
+		['guns_label'] = '--- Armes ---',
+		['confiscate'] = 'confisquer ',
+		['inventory_label'] = '--- Inventaire ---',
+		['confiscate_inv'] = 'confisquer x',
+
+		['traffic_offense'] = 'code de la route',
+		['minor_offense'] = 'délit mineur',
+		['average_offense'] = 'délit moyen',
+		['major_offense'] = 'délit grave',
+		['fine_total'] = 'amende : ',
+	--Vehicle Info Menu
+		['plate'] = 'N°: ',
+		['owner_unknown'] = 'propriétaire : Inconnu',
+		['owner'] = 'propriétaire : ',
+	--Weapons Menus
+		['get_weapon_menu'] = 'armurerie - Prendre Arme',
+		['put_weapon_menu'] = 'armurerie - Déposer Arme',
+		['buy_weapon_menu'] = 'armurerie - Acheter Armes',
+		['not_enough_money'] = 'vous n\'avez pas assez d\'argent',
+	--Boss Menu
+		['take_company_money'] = 'retirer argent société',
+		['deposit_money'] = 'déposer argent',
+		['amount_of_withdrawal'] = 'montant du retrait',
+		['invalid_amount'] = 'montant invalide',
+		['amount_of_deposit'] = 'montant du dépôt',
+		['open_bossmenu'] = 'appuez sur ~INPUT_CONTEXT~ pour ouvrir le menu',
+	--Misc
+		['remove_object'] = 'appuez sur ~INPUT_CONTEXT~ pour enlever l\'objet',
+		['map_blip'] = 'Commissariat',
+	--Notifications
+		['from'] = '~s~ à ~b~',
+		['you_have_confinv'] = 'vous avez confisqué ~y~x',
+		['confinv'] = '~s~ vous a confisqué ~y~x',
+		['you_have_confdm'] = 'vous avez confisqué ~y~$',
+		['confdm'] = '~s~ vous a confisqué ~y~$',
+		['you_have_confweapon'] = 'vous avez confisqué ~y~x1 ',
+		['confweapon'] = '~s~ vous a confisqué ~y~x1 ',
+		['alert_police'] = 'alerte police',
+	--Authorized Vehicles
+		['police'] = 'véhicule de patrouille 1',
+		['police2'] = 'véhicule de patrouille 2',
+		['police3'] = 'véhicule de patrouille 3',
+		['police4'] = 'véhicule civil',
+		['policeb'] = 'moto',
+		['policet'] = 'van de transport',
+}
