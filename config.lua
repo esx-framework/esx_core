4201f4a7359baed13d1ee3e3c18a4279e66e54bd
nearbyplayer <nearbyplayer@users.noreply.github.com>
2017-08-15 22:39:52 -0400

    Multi-language support

--- config.lua
+++ config.lua
@@ -3,6 +3,7 @@ Config.DrawDistance           = 100.0
 Config.NPCJobEarnings         = {min = 15, max = 40}
 Config.MaxInService           = 2
 Config.EnablePlayerManagement = false
+Config.Locale = 'fr'
 
 Config.Zones = {
 
