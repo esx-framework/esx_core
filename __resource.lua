8b8fcb532f377bb7e1e5ce8560020221b4829145
nearbyplayer <nearbyplayer@users.noreply.github.com>
2017-08-15 21:22:19 -0400

    Multi-language support

--- __resource.lua
+++ __resource.lua
@@ -3,11 +3,17 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'ESX Basic Needs'
 
 server_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
 	'config.lua',
 	'client/main.lua'
 }
---
+++ locales/en.lua
@@ -0,0 +1,6 @@
+Locales['en'] = {
+
+	['used_bread'] = 'you have used 1x bread',
+	['used_water'] = 'you have used 1x water',
+
+}
