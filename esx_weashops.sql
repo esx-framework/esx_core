17d588c2e65530420f0652433b712127d6c4e7e6
Don <don@edge-gamers.com>
2017-08-17 00:10:07 -0700

    Fixed SQL, Fixed INPUT_CONTEXT
    Fixed SQL, Fixed INPUT_CONTEXT, Fixed Readme

--- esx_weashops.sql
+++ esx_weashops.sql
@@ -6,12 +6,12 @@ CREATE TABLE `weashops` (
   `name` varchar(255) NOT NULL,
   `item` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
+  `label` varchar(255) NOT NULL,
   
   PRIMARY KEY (`id`)
 );
 
-
-INSERT INTO `weashops` (name, item, price) VALUES
-	('GunShop','WEAPON_Pistol',300),
-	('blackweashop','WEAPON_Pistol',500)
+INSERT INTO `weashops` (name, item, price, label) VALUES
+	('GunShop','WEAPON_Pistol',300, 'pistol'),
+	('blackweashop','WEAPON_Pistol',500, 'pistol')
 ;
