b18a0cbd753d79fa80297d3caa6a42ab1a8ac3d1
TakumiBR <31086346+TakumiBR@users.noreply.github.com>
2017-08-16 22:52:56 -0700

    Add files via upload

---
+++ locales/br.lua
@@ -0,0 +1,9 @@
+Locales['br'] = {
+
+	['invoices'] = 'Faturas',
+	['received_invoice'] = 'Você ~r~recebeu~s~ uma fatura',
+	['paid_invoice'] = 'Você ~g~pagou~s~ uma fatura de ~r~$',
+	['received_payment'] = 'Você ~g~recebeu~s~ um pagamento de ~r~$',
+	['player_not_logged'] = 'O jogador não está logado',
+
+}
