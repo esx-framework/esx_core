9f380ad8d35a307c79a0886d5ff3c8d1989ddbd4
TakumiBR <31086346+TakumiBR@users.noreply.github.com>
2017-08-17 08:43:24 -0700

    Multi-language support
    Brazilian Portuguese translation.

---
+++ locales/br.lua
@@ -0,0 +1,7 @@
+Locales['br'] = {
+
+	['new_job'] = 'Você agora tem um novo emprego!',
+	['access_job_center'] = 'Pressione ~INPUT_PICKUP~ para acessar a ~b~Lista de Empregos~s~.',
+	['job_center'] = 'Agência de Empregos',
+
+}
