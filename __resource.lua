9d320fc84fd15a1a4b7637541e57ac7b32262265
TakumiBR <31086346+TakumiBR@users.noreply.github.com>
2017-08-17 09:01:34 -0700

    Multi-language support
    Brazilian Portuguese translation.

--- __resource.lua
+++ __resource.lua
@@ -6,6 +6,7 @@ server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'config.lua',
+	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
 	'server/main.lua'
@@ -14,6 +15,7 @@ server_scripts {
 client_scripts {
 	'@es_extended/locale.lua',
 	'config.lua',
+	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
 	'client/main.lua'
