668758fe422d9998c4c83030619ef4ff1eb20f8a
Allan <31086346+TakumiBR@users.noreply.github.com>
2017-08-17 12:09:33 -0700

    Multi-language support
    Brazilian Portuguese translation.

--- locales/br.lua
+++ locales/br.lua
@@ -1,7 +1,7 @@
 Locales['br'] = {
 
 	['skin_menu'] = 'Skin Menu',
-	['use_rotate_view'] = 'Use as setas ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para girar a visão.',
+	['use_rotate_view'] = 'Use ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ e ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para girar a visão.',
     ['skin'] = 'Trocar',
 	['saveskin'] = 'Salvar',
 
