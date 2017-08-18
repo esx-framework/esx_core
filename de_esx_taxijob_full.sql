2055280cc357d35f9a5859099b0d393c236c4a34
ultratm <renekiesewetter@msn.com>
2017-08-18 11:35:02 +0200

    add sql and config

---
+++ de_esx_taxijob_full.sql
@@ -0,0 +1,17 @@
+USE `essentialmode`;
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+  ('society_taxi','Taxi',1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+  ('taxi','Taxi')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+  ('taxi',0,'recrue','Rekrut',12,'{}','{}'),
+  ('taxi',1,'novice','Anfänger',24,'{}','{}'),
+  ('taxi',2,'experimente','Experte',36,'{}','{}'),
+  ('taxi',3,'uber','Schichtführer',48,'{}','{}'),
+  ('taxi',4,'boss','Chef',0,'{}','{}')
+;
