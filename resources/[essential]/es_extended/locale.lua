8c74011b397337e3c24bb716f133039ac32ad90a
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-08-19 11:28:19 +0200

    Add fallback cases

--- resources/[essential]/es_extended/locale.lua
+++ resources/[essential]/es_extended/locale.lua
@@ -1,7 +1,19 @@
 Locales = {}
 
 function _(str, ...)	-- Translate string
-	return string.format(Locales[Config.Locale][str], ...)
+
+	if Locales[Config.Locale] ~= nil then
+
+		if Locales[Config.Locale][str] ~= nil then
+			return string.format(Locales[Config.Locale][str], ...)
+		else
+			return 'Translation [' .. Config.Locale .. '][' .. str .. '] does not exists'
+		end
+
+	else
+		return 'Locale [' .. Config.Locale .. '] does not exists'
+	end
+
 end
 
 function _U(str, ...) -- Translate string first char uppercase
