dd53565a9938b0de6e4400fc6b4cdafb7fddb249
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-08-22 01:22:37 +0200

    Fix typo

--- locales/en.lua
+++ locales/en.lua
@@ -4,7 +4,7 @@ Locales['en'] = {
     ['yes'] = 'yes',
     ['no'] = 'no',
     ['not_enough_money'] = 'you do not have enough money',
-    ['press_access'] = 'press ~INPUT_CONTENT~ to access the menu',
+    ['press_access'] = 'press ~INPUT_CONTEXT~ to access the menu',
     ['barber_blip'] = 'barber shop',
     ['you_paid'] = 'you paid ',
 }
