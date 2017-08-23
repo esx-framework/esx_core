61e7f37c7b3d0dd12cc0168c14aecfb75effc9a3
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-08-23 10:38:48 +0200

    Add new __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -3,6 +3,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'ESX Banker job'
 
 server_scripts {
+	'@async/async.lua',
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'config.lua',
