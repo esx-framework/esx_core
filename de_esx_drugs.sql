18aab6e401e50fc4d3c15ba10cf886297a8d40f3
ultratm <renekiesewetter@msn.com>
2017-08-26 11:19:25 +0200

    Add German Language

---
+++ de_esx_drugs.sql
@@ -0,0 +1,10 @@
+INSERT INTO `items` (name, label) VALUES
+	('weed', 'Grass'),
+	('weed_pooch', 'Portion Grass'),
+	('coke', 'Koks'),
+	('coke_pooch', 'Portion Koks'),
+	('meth', 'Meth'),
+	('meth_pooch', 'Portion Meth'),
+	('opium', 'Opium'),
+	('opium_pooch', 'Portion Opium')
+;
