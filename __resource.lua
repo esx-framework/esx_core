645ab7583fa7c1bfcc022a1efc6fe1a56643e1e1
ultratm <renekiesewetter@msn.com>
2017-08-26 10:57:48 +0200

    Add German Language

--- __resource.lua
+++ __resource.lua
@@ -7,6 +7,7 @@ server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'config.lua',
+	'locales/de.lua',
 	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
@@ -16,6 +17,7 @@ server_scripts {
 client_scripts {
 	'@es_extended/locale.lua',
 	'config.lua',
+	'locales/de.lua',
 	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
