017a2d8733d5ae09218bbc02ffdd88e72cae9f63
ultratm <renekiesewetter@msn.com>
2017-08-26 12:50:44 +0200

    Add German Language

---
+++ locales/de.lua
@@ -0,0 +1,10 @@
+Locales['de'] = {
+
+    ['valid_purchase'] = 'Einkauf bestätigen?',
+    ['yes'] = 'ja',
+    ['no'] = 'nein',
+    ['not_enough_money'] = 'du hast nicht genug Geld',
+    ['press_access'] = 'Drücke ~INPUT_CONTEXT~ um das Menü zu öffnen',
+    ['barber_blip'] = 'Friseur',
+    ['you_paid'] = 'du bezahlst ',
+}
