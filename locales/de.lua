a52c27eb8b88c31eb4e7ef45de37019aecb28fb2
ultratm <renekiesewetter@msn.com>
2017-08-26 13:04:20 +0200

    Add German Language

---
+++ locales/de.lua
@@ -0,0 +1,10 @@
+Locales ['de'] = {
+
+    ['buy'] = 'du kaufst',
+    ['not_enough_black'] = 'du hast nicht genug Schwarzgeld',
+    ['not_enough'] = 'du hast nicht genug Geld',
+    ['shop'] = 'Geschäft',
+    ['shop_menu'] = 'Drücke ~INPUT_CONTEXT~ um auf das Geschäft zuzugreifen.',
+    ['map_blip'] = 'Waffenladen',
+
+}
