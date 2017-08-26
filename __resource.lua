bc8d30e6586ca817c19b2dd790442e32e11f5f13
ultratm <renekiesewetter@msn.com>
2017-08-26 12:25:12 +0200

    Add German Language

--- __resource.lua
+++ __resource.lua
@@ -6,6 +6,8 @@ server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'config.lua',
 	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
 	'server/main.lua'
@@ -14,6 +16,8 @@ server_scripts {
 client_scripts {
 	'@es_extended/locale.lua',
 	'config.lua',
+	'locales/de.lua',
+	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
 	'client/main.lua'
---
+++ locales/de.lua
@@ -0,0 +1,13 @@
+Locales['de'] = {
+
+	['new_message'] = '~b~Neue Nachricht',
+	['press_take_call'] = ' - Drücke ~INPUT_CONTEXT~ um den Anruf anzunehmen',
+	['taken_call'] = ' hat den Anruf angenommen',
+	['gps_position'] = 'Ziel in GPS eingegeben',
+	['message_sent'] = 'nachricht gesendet',
+	['cannot_add_self'] = 'du kannst dich nicht selbst hinzufügen',
+	['number_in_contacts'] = 'diese Nummer ist bereits in deinen Kontakten',
+	['contact_added'] = 'Kontakt hinzugefügt',
+	['number_not_assigned'] = 'diese Nummer ist nicht vergeben...',
+
+}
