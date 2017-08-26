081e07887b27dbb5de5dc8cee8e8ee668f001921
ultratm <renekiesewetter@msn.com>
2017-08-26 12:43:20 +0200

    Add German Language

--- __resource.lua
+++ __resource.lua
@@ -1,5 +1,6 @@
 server_scripts {
     '@es_extended/locale.lua',
+	'locales/de.lua',
     'locales/br.lua',
     'locales/en.lua',
     'locales/fr.lua',
@@ -9,6 +10,7 @@ server_scripts {
 
 client_scripts {
     '@es_extended/locale.lua',
+	'locales/de.lua',
     'locales/br.lua',
     'locales/en.lua',
     'locales/fr.lua',
