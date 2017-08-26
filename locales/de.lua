1090d0f663654f84a42de3a6bbcec6f8c19171ec
ultratm <renekiesewetter@msn.com>
2017-08-26 11:29:18 +0200

    Add German Language

---
+++ locales/de.lua
@@ -0,0 +1,91 @@
+Locales['de'] = {
+	--Cloackroom
+		['cloakroom'] = 'Garderobe',
+		['citizen_wear'] = 'Zivilkleidung',
+		['police_wear'] = 'Arbeitskleidung',
+		['open_cloackroom'] = 'Drücke ~INPUT_CONTEXT~ um dich umzuziehen',
+	--Armory
+		['get_weapon'] = 'Waffen holen',
+		['put_weapon'] = 'Waffen bringen',
+		['buy_weapons'] = 'Waffen kaufen',
+		['armory'] = 'Waffenkammer',
+		['open_armory'] = 'Drücke ~INPUT_CONTEXT~ um die Waffenkammer zu öffnen',
+	--Vehicles
+		['vehicle_menu'] = 'Fahrzeug',
+		['vehicle_out'] = 'es ist bereits ein Fahrzeug draussen',
+		['vehicle_spawner'] = 'Drücke ~INPUT_CONTEXT~ um ein Fahrzeug zu holen',
+		['store_vehicle'] = 'Drücke ~INPUT_CONTEXT~ um das Fahrzeug zu parken',
+		['service_max'] = 'Max Polizisten im Dienst : ',
+	--Action Menu
+		['citizen_interaction'] = 'Zivilistenaktionen',
+		['vehicle_interaction'] = 'Fahrzeuginteraktionen',
+		['object_spawner'] = 'Objekt Spawner',
+
+		['id_card'] = 'ID Karte',
+		['search'] = 'Suche',
+		['handcuff'] = 'Festnehmen / Freilassen',
+		['put_in_vehicle'] = 'In Fahrzeug setzen',
+		['fine'] = 'Strafe',
+		['no_players_nearby'] = 'Keine Spieler in der Nähe',
+
+		['vehicle_info'] = 'Fahrzeug Info',
+		['pick_lock'] = 'Fahrzeug öffnen',
+		['vehicle_unlocked'] = 'Fahrzeug ~g~offen~s~',
+		['no_vehicles_nearby'] = 'Keine Fahrzeuge in der Nähe',
+		['traffic_interaction'] = 'Straßeninteraktionen',
+		['cone'] = 'Hütchen',
+		['barrier'] = 'Barriere',
+		['spikestrips'] = 'Nagelband',
+		['box'] = 'Box',
+		['cash'] = 'Box mit Geld',
+	--ID Card Menu
+		['name'] = 'name : ',
+		['bac'] = 'bAC : ',
+	--Body Search Menu
+		['confiscate_dirty'] = 'Schwarzgeld konfesziert : $',
+		['guns_label'] = '--- Waffen ---',
+		['confiscate'] = 'konfeszieren ',
+		['inventory_label'] = '--- Inventar ---',
+		['confiscate_inv'] = 'konfeziere x',
+
+		['traffic_offense'] = 'Verkehrs vergehen',
+		['minor_offense'] = 'Geringes vergehen',
+		['average_offense'] = 'Normales vergehen',
+		['major_offense'] = 'Hohes vergehen',
+		['fine_total'] = 'Strafe : ',
+	--Vehicle Info Menu
+		['plate'] = 'Plate: ',
+		['owner_unknown'] = 'Besitzer : Unbekannt',
+		['owner'] = 'Besitzer : ',
+	--Weapons Menus
+		['get_weapon_menu'] = 'Waffenkammer - Waffen nehmen',
+		['put_weapon_menu'] = 'Waffenkammer - Waffen bringen',
+		['buy_weapon_menu'] = 'Waffenkammer - Waffen kaufen',
+		['not_enough_money'] = 'Du hast nicht genug Geld',
+	--Boss Menu
+		['take_company_money'] = 'Firmengeld abheben',
+		['deposit_money'] = 'Geld einzahlen',
+		['amount_of_withdrawal'] = 'Betrag zum abheben',
+		['invalid_amount'] = 'ungültiger Betrag',
+		['amount_of_deposit'] = 'Betrag zum einzahlen',
+		['open_bossmenu'] = 'Drücke ~INPUT_CONTEXT~ um das Menü zu öffnen',
+	--Misc
+		['remove_object'] = 'Drücke ~INPUT_CONTEXT~ um das Objekt zu entfernen',
+		['map_blip'] = 'Polizeistation',
+	--Notifications
+		['from'] = '~s~ von ~b~',
+		['you_have_confinv'] = 'du hast ~y~x konfesziert',
+		['confinv'] = '~s~ hat von dir ~y~x konfesziert',
+		['you_have_confdm'] = 'du hast ~y~$ konfesziert',
+		['confdm'] = '~s~ hat von dir ~y~$ konfesziert',
+		['you_have_confweapon'] = 'du hast ~y~x1 konfesziert',
+		['confweapon'] = '~s~ hat von dir ~y~x1 konfesziert',
+		['alert_police'] = 'Polizei alamieren',
+	--Authorized Vehicles
+		['police'] = 'Streifenfahrzeug 1',
+		['police2'] = 'Streifenfahrzeug 2',
+		['police3'] = 'Streifenfahrzeug 3',
+		['police4'] = 'Zivilfahrzeug',
+		['policeb'] = 'Motorrad',
+		['policet'] = 'Transporter',
+}
