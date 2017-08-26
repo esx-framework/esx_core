9ed46165f5e18ec06031bec68c10e335f8d7e624
ultratm <renekiesewetter@msn.com>
2017-08-26 11:31:41 +0200

    Add German Language

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,7 @@ server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'config.lua',
+	'locales/de.lua',
 	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
@@ -11,6 +12,7 @@ server_scripts {
 client_scripts {
 	'@es_extended/locale.lua',
 	'config.lua',
+	'locales/de.lua',
 	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
---
+++ locales/de.lua
@@ -0,0 +1,7 @@
+Locales['de'] = {
+
+	['new_job'] = 'Du hast einen neuen Job!',
+	['access_job_center'] = 'Drücke ~INPUT_PICKUP~ um das ~b~Arbeitsamt~s~ zu öffnen.',
+	['job_center'] = 'Arbeitsamt',
+
+}
