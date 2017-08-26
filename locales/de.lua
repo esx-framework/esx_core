cca9c70de6fbfd18109c1b22898f32f6d930e34b
ultratm <renekiesewetter@msn.com>
2017-08-26 12:55:32 +0200

    Add German Language

---
+++ locales/de.lua
@@ -0,0 +1,23 @@
+Locales['de'] = {
+
+	['robbery_cancelled'] = 'der Raub wurde abgebrochen, du bekommst nichts!',
+	['robbery_successful'] = 'Raub erfolgreich du erbeutest ~g~$',
+	['shop_robbery'] = 'Ladenraub',
+	['press_to_rob'] = 'Drücke ~INPUT_CONTEXT~ zum ausrauben ~b~',
+	['robbery_of'] = 'Ladenraub: ~r~',
+	['seconds_remaining'] = '~w~ sekunden übrig',
+	['robbery_cancelled_at'] = '~r~ Raub abgebrochen bei: ~b~',
+	['robbery_has_cancelled'] = '~r~ Raub wurde abgebrochen: ~b~',
+	['already_robbed'] = 'dieser Laden wurde bereits ausgeraubt. Bitte warte: ',
+	['seconds'] = 'sekunden.',
+	['rob_in_prog'] = '~r~ Raub im gange bei: ~b~',
+	['started_to_rob'] = 'du hast einen Raub gestartet ',
+	['do_not_move'] = ', laufe nicht weg!',
+	['alarm_triggered'] = 'der Alarm wurde ausgelöst',
+	['hold_pos'] = 'halte 5 Minuten lang durch, und die Beute gehört dir!',
+	['robbery_complete'] = '~r~ Raub erfolgreich.~s~ ~h~ LAUF!',
+	['robbery_complete_at'] = '~r~ Raub erfolgreich bei: ~b~',
+	['min_two_police'] = 'es müssen mindestens ~b~2 Polizisten~s~ im Dienst sein um einen Raub starten zu können.',
+	['robbery_already'] = '~r~Ein Raub ist bereits im gange.',
+
+}
