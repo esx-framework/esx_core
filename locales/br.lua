93f49924b62127e7e9bab4f459028652ff27c28e
Wesley Moura <14825927+wesleymoura@users.noreply.github.com>
2017-08-27 19:07:59 -0300

    Add Brazilian Language

---
+++ locales/br.lua
@@ -0,0 +1,10 @@
+Locales['br'] = {
+
+    ['valid_purchase'] = 'validar esta compra?',
+    ['yes'] = 'sim',
+    ['no'] = 'não',
+    ['not_enough_money'] = 'você não tem dinheiro suficiente',
+    ['press_access'] = 'pressione ~INPUT_CONTEXT~ para acessar o menu',
+    ['barber_blip'] = 'Barbearia',
+    ['you_paid'] = 'você pagou ',
+}
