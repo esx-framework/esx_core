152c447325185be3d5caf8e50a124b0697504a16
Pgmbru <pgmbru@hotmail.fr>
2017-08-27 23:07:38 +0200

    Fix trad

--- locales/en.lua
+++ locales/en.lua
@@ -8,5 +8,6 @@ Locales['en'] = {
 	['press_menu'] = 'press ~INPUT_CONTEXT~ to access the menu',
 	['clothes'] = 'clothes',
 	['you_paid'] = 'you paid $',
+	['save_in_dressing'] = 'Do you want to give a name to your outfit ?',
 
 }
