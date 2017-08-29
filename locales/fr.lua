ee242c0b92d140c5e18f1e569a844298e69f649f
Aleweadan <alexbox369@gmail.com>
2017-08-30 00:27:57 +0200

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,6 +1,6 @@
 Locales['fr'] = {
 
-	['new_job'] = 'vous avez nouveau job !',
+	['new_job'] = 'vous avez un nouveau job !',
 	['access_job_center'] = 'appuyez sur ~INPUT_PICKUP~ pour \naccéder au ~b~Pôle Emploi~s~.',
 	['job_center'] = 'pôle-Emploi',
 
