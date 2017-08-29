e58dc08318bd21ae3ad1e71a5888e6a8cb66bf26
Aleweadan <alexbox369@gmail.com>
2017-08-30 00:26:24 +0200

    Update esx_realestateagentjob.sql

--- esx_realestateagentjob.sql
+++ esx_realestateagentjob.sql
@@ -1,16 +1,16 @@
 USE `essentialmode`;
 
 INSERT INTO `addon_account` (name, label, shared) VALUES
-  ('society_realestateagent','Immobilienverkauf',1)
+  ('society_realestateagent','Agent immobilier',1)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-  ('realestateagent','Immobilienverkauf')
+  ('realestateagent','Agent immobilier')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-  ('realestateagent',0,'location','Vermieter',10,'{}','{}'),
-  ('realestateagent',1,'vendeur','Verkäufer',25,'{}','{}'),
-  ('realestateagent',2,'gestion','Manager',40,'{}','{}'),
-  ('realestateagent',3,'boss','Boss',0,'{}','{}')
+  ('realestateagent',0,'location','Location',10,'{}','{}'),
+  ('realestateagent',1,'vendeur','Vendeur',25,'{}','{}'),
+  ('realestateagent',2,'gestion','Gestion',40,'{}','{}'),
+  ('realestateagent',3,'boss','Patron',0,'{}','{}')
 ;
