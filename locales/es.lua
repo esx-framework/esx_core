5680af3c23c26122fa14e140a5751362fef44e55
o0Coro0o <david_el_pitu@hotmail.com>
2017-09-11 21:52:01 +0200

    Create es.lua
    Added Spanish language

---
+++ locales/es.lua
@@ -0,0 +1,8 @@
+Locales['es'] = {
+	['invalid_amount']    = '~r~cantidad no válida',
+	['deposit_money']          = 'has depositado ~g~€',
+	['withdraw_money']         = 'has retirado ~g~€',
+	['press_e_atm'] = 'Presiona ~INPUT_PICKUP~ para depositar o retirar ~g~cash~s~.',  
+	['transf_money_ok'] = 'Transferencia correctamente ~g~€',
+	['transf_money_error'] = '~r~ Error en la transferencia'
+}
