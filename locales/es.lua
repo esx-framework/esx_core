5ce1295fbd9cf1426a9b7c68bda140e6c3f7a361
Coro Veneno <david_el_pitu@hotmail.com>
2017-09-12 22:12:52 +0200

    Create es.lua
    added spanish language Multi-language

---
+++ locales/es.lua
@@ -0,0 +1,10 @@
+Locales['es'] = {
+
+    ['valid_purchase'] = '¿validar esta compra?',
+    ['yes'] = 'Si',
+    ['no'] = 'No',
+    ['not_enough_money'] = 'No tienes suficente dinero',
+    ['press_access'] = 'Presiona ~INPUT_CONTEXT~ para acceder al menú',
+    ['barber_blip'] = 'Barbero',
+    ['you_paid'] = 'Has pagado ',
+}
