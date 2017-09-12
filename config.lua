0462fa7f639fd84a7b85481733ecef7752ebdba2
ArkSeyonet <31637812+ArkSeyonet@users.noreply.github.com>
2017-09-12 09:01:22 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ Config.MarkerSize                 = {x = 1.5, y = 1.5, z = 1.0}
 Config.MarkerColor                = {r = 50, g = 50, b = 204}
 Config.EnablePlayerManagement     = false
 Config.EnableArmoryManagement     = false
-Config.EnableESXIdentity          = false
+Config.EnableESXIdentity          = false -- only turn this on if you are using esx_identity
 Config.EnableNonFreemodePeds      = false -- turn this on if you want custom peds
 Config.EnableSocietyOwnedVehicles = false
 Config.MaxInService               = -1
