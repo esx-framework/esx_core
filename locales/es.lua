fcda14fe191c4e3bea8cd420ecadfa9e9eff38da
Coro Veneno <david_el_pitu@hotmail.com>
2017-09-12 22:38:54 +0200

    Create es.lua
    Added Spanish Language Multi-Language

---
+++ locales/es.lua
@@ -0,0 +1,7 @@
+Locales['es'] = {
+
+	['new_job'] = '¡Tienes un trabajo nuevo!',
+	['access_job_center'] = 'Pulsa ~INPUT_PICKUP~ para entrar a la ~b~Oficina de Empleo~s~.',
+	['job_center'] = 'Oficina de Empleo',
+
+}
