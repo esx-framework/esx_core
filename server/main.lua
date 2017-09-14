e66b7acb17af37596280e707596cddec426f13f6
Romain Lanz <romain.lanz@slynova.ch>
2017-09-14 17:55:37 +0200

    chore(coding-style): correct indentation

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,7 @@
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj)
-	ESX = obj
+  ESX = obj
 end)
 
 TriggerEvent('esx_phone:registerNumber', 'realestateagent', _U('client'), false, false)
@@ -9,58 +9,58 @@ TriggerEvent('esx_society:registerSociety', 'realestateagent', 'Agent immobilier
 
 RegisterServerEvent('esx_realestateagentjob:revoke')
 AddEventHandler('esx_realestateagentjob:revoke', function(property, owner)
-	TriggerEvent('esx_property:removeOwnedPropertyIdentifier', property, owner)
+  TriggerEvent('esx_property:removeOwnedPropertyIdentifier', property, owner)
 end)
 
 RegisterServerEvent('esx_realestateagentjob:sell')
 AddEventHandler('esx_realestateagentjob:sell', function(target, property, price)
-	
-	local xPlayer = ESX.GetPlayerFromId(target)
 
-	xPlayer.removeMoney(price)
+  local xPlayer = ESX.GetPlayerFromId(target)
 
-	TriggerEvent('esx_addonaccount:getSharedAccount', 'society_realestateagent', function(account)
-		account.addMoney(price)
-	end)
+  xPlayer.removeMoney(price)
 
-	TriggerEvent('esx_property:setPropertyOwned', property, price, false, xPlayer.identifier)
+  TriggerEvent('esx_addonaccount:getSharedAccount', 'society_realestateagent', function(account)
+    account.addMoney(price)
+  end)
+
+  TriggerEvent('esx_property:setPropertyOwned', property, price, false, xPlayer.identifier)
 end)
 
 RegisterServerEvent('esx_realestateagentjob:rent')
 AddEventHandler('esx_realestateagentjob:rent', function(target, property, price)
-	
-	local xPlayer = ESX.GetPlayerFromId(target)
 
-	TriggerEvent('esx_property:setPropertyOwned', property, price, true, xPlayer.identifier)
+  local xPlayer = ESX.GetPlayerFromId(target)
+
+  TriggerEvent('esx_property:setPropertyOwned', property, price, true, xPlayer.identifier)
 end)
 
 ESX.RegisterServerCallback('esx_realestateagentjob:getCustomers', function(source, cb)
 
-	TriggerEvent('esx_ownedproperty:getOwnedProperties', function(properties)
-		
-		local xPlayers  = ESX.GetPlayers()
-		local customers = {}
+  TriggerEvent('esx_ownedproperty:getOwnedProperties', function(properties)
 
-		for i=1, #properties, 1 do
-			for j=1, #xPlayers, 1 do
+    local xPlayers  = ESX.GetPlayers()
+    local customers = {}
 
-				local xPlayer = ESX.GetPlayerFromId(xPlayers[j])
-				
-				if xPlayer.identifier == properties[i].owner then
-					table.insert(customers, {
-						name           = xPlayer.name,
-						propertyOwner  = properties[i].owner,
-						propertyRented = properties[i].rented,
-						propertyId     = properties[i].id,
-						propertyPrice  = properties[i].price,
-						propertyName   = properties[i].name
-					})
-				end
-			end
-		end
+    for i=1, #properties, 1 do
+      for j=1, #xPlayers, 1 do
 
-		cb(customers)
+        local xPlayer = ESX.GetPlayerFromId(xPlayers[j])
 
-	end)
+        if xPlayer.identifier == properties[i].owner then
+          table.insert(customers, {
+            name           = xPlayer.name,
+            propertyOwner  = properties[i].owner,
+            propertyRented = properties[i].rented,
+            propertyId     = properties[i].id,
+            propertyPrice  = properties[i].price,
+            propertyName   = properties[i].name
+          })
+        end
+      end
+    end
+
+    cb(customers)
+
+  end)
 
 end)
