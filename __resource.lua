c041e1834ffdb0be7f7948292d108226595148dc
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-09-15 15:30:12 +0200

    chore(resource): Add version

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,8 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Mecano Job'
 
+version '1.0.0'
+
 client_scripts {
   '@es_extended/locale.lua',
   'locales/en.lua',
