9f3064199cc9f7086f0a3830d5c754705d68260d
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-09-15 15:24:32 +0200

    chore(resource): Add version

--- __resource.lua
+++ __resource.lua
@@ -1,27 +1,29 @@
 description 'esx_drugs'
 
+version '1.0.0'
+
 server_scripts {
 
-    '@es_extended/locale.lua',
+  '@es_extended/locale.lua',
 	'locales/de.lua',
 	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
 	'locales/es.lua',
-    'server/esx_drugs_sv.lua',
-    'config.lua'
+  'server/esx_drugs_sv.lua',
+  'config.lua'
 
 }
 
 client_scripts {
 
-    '@es_extended/locale.lua',
+  '@es_extended/locale.lua',
 	'locales/de.lua',
 	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
 	'locales/es.lua',
-    'config.lua',
-    'client/esx_drugs_cl.lua'
+  'config.lua',
+  'client/esx_drugs_cl.lua'
 
 }
