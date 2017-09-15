10c6203565a0e2cbb13c3267fb8469ef08ab46ef
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-09-15 15:40:03 +0200

    chore(resource): Add version

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,8 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX ClotheShop'
 
+version '1.0.0'
+
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
     '@es_extended/locale.lua',
