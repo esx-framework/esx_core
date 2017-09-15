2b972fb3df4731e22bbfb9d8f4cc4fe889031aac
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-09-15 15:37:32 +0200

    chore(resource): Add version

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,8 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Optional Needs'
 
+version '1.0.0'
+
 server_scripts {
 	'@es_extended/locale.lua',
 	'locales/en.lua',
