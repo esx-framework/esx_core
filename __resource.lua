a99c34cab20094db799bbe25dfaf4625bd155468
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-09-15 15:14:08 +0200

    chore(resource): Add version

--- __resource.lua
+++ __resource.lua
@@ -2,6 +2,8 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ES Extended'
 
+version '1.0.0'
+
 server_scripts {
   '@async/async.lua',
   '@mysql-async/lib/MySQL.lua',
