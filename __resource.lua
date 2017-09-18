ab51d046f787d3516ab52ffb5cd22f52caad99d1
nearbyplayer <aradz1299@gmail.com>
2017-09-17 20:55:01 -0400

    feat(locale): English Translation

--- __resource.lua
+++ __resource.lua
@@ -1,12 +1,13 @@
 description 'ESX Whitelist'
 
-version '1.0.0'
+version '1.0.1'
 
 server_scripts {
   '@mysql-async/lib/MySQL.lua',
   '@es_extended/locale.lua',
-  'Config.lua',
+  'config.lua',
+  'locales/en.lua',
   'locales/fr.lua',
   'server/main.lua',
-  'server/commands.lua',
+  'server/commands.lua'
 }
---
+++ locales/en.lua
@@ -0,0 +1,6 @@
+Locales['en'] = {
+  ['not_whitelisted']     = 'you are not whitelisted on this server',
+  ['steamid_error']       = 'there seems to be a problem with your SteamID',
+  ['help_whitelist_add']  = 'add someone to the whitelist',
+  ['help_whitelist_load'] = 'reload the whitelist',
+}
