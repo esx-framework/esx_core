072ef503b21091a960568022950b525eadeee97b
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-09-19 15:18:42 +0200

    fix(sql): Add dmv

--- esx_dmvschool.sql
+++ esx_dmvschool.sql
@@ -1,6 +1,7 @@
 USE `essentialmode`;
 
 INSERT INTO `licenses` (`type`, `label`) VALUES
+	('dmv', 'Code de la route'),
 	('drive', 'Permis de conduire'),
 	('drive_bike', 'Permis moto'),
 	('drive_truck', 'Permis camion')
