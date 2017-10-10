3a2e5e7eb46453fdfbc0b7d22dc16890711e2c52
ArkSeyonet <31637812+ArkSeyonet@users.noreply.github.com>
2017-10-10 06:25:27 -0500

    Update esx_identity.sql

--- esx_identity.sql
+++ esx_identity.sql
@@ -7,3 +7,13 @@ ALTER TABLE `users`
   ADD COLUMN `sex` VARCHAR(10) NULL DEFAULT '',
   ADD COLUMN `height` VARCHAR(5) NULL DEFAULT ''
 ;
+
+CREATE TABLE `characters` (
+
+	`identifier` varchar(255) NOT NULL,
+	`firstname` varchar(255) NOT NULL,
+	`lastname` varchar(255) NOT NULL,
+	`dateofbirth` varchar(255) NOT NULL,
+	`sex` varchar(1) NOT NULL DEFAULT 'f',
+	`height` varchar(128) NOT NULL
+);
