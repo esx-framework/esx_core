95e35e4e489ea86aac348473c6e280780333698a
Romain Lanz <romain.lanz@slynova.ch>
2017-10-14 23:56:54 +0200

    feat(sql): update SQL script

--- esx_weashops.sql
+++ esx_weashops.sql
@@ -1,14 +1,18 @@
 USE `essentialmode`;
 
 CREATE TABLE `weashops` (
-  
+
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `item` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
-  
+
   PRIMARY KEY (`id`)
 );
+
+INSERT INTO `licenses` (type, label) VALUES
+  ('weapon', "Permis de port d'arme")
+;
 
 INSERT INTO `weashops` (name, item, price) VALUES
 	('GunShop','WEAPON_PISTOL',300),
