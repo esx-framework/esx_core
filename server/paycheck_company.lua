90018baac608d1cd5162eb7f77ae9960ca0941db
KandaSoranyan <32266067+KandaSoranyan@users.noreply.github.com>
2017-12-03 21:37:09 +0100

    Update paycheck_company.lua
    No more cunt who are giving them 1 million with society paycheck.
    No money in society account, no pay. That's all

--- server/paycheck_company.lua
+++ server/paycheck_company.lua
@@ -1,29 +1,30 @@
 ESX.StartPayCheck = function ()
+
   function payCheck ()
     local xPlayers = ESX.GetPlayers()
-
     for i=1, #xPlayers, 1 do
       local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-
-      if xPlayer.job.grade_salary > 0 then
-        xPlayer.addMoney(xPlayer.job.grade_salary)
-
-        if xPlayer.job.grade_name == 'interim' then
-          TriggerClientEvent('esx:showNotification', xPlayer.source, _U('rec_help') .. '~g~$' .. xPlayer.job.grade_salary)
-        else
-          TriggerEvent('esx_society:getSociety', xPlayer.job.name, function (society)
-            TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function (account)
-              account.removeMoney(xPlayer.job.grade_salary)
-            end)
-          end)
-
-          TriggerClientEvent('esx:showNotification', xPlayer.source, _U('rec_salary') .. '~g~$' .. xPlayer.job.grade_salary)
+      if xPlayer.job.grade_name == 'interim' or xPlayer.job.grade_name == 'rsa' or xPlayer.job.grade_name == 'employee' then -- Si il n'est pas dans une société, je prends chez l'état
+        if xPlayer.job.grade_salary > 0 then
+          xPlayer.addAccountMoney('bank',xPlayer.job.grade_salary)
+        TriggerClientEvent('esx:showNotification', xPlayer.source, ('L\'état vous a payé ') .. '~g~$' .. xPlayer.job.grade_salary)
         end
-      end
+      else -- Sinon je prends l'argent dans la société
+        TriggerEvent('esx_society:getSociety', xPlayer.job.name, function (society)
+          TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function (account)
+        if account.money >= xPlayer.job.grade_salary then
+          xPlayer.addAccountMoney('bank',xPlayer.job.grade_salary)
+          account.removeMoney(xPlayer.job.grade_salary)
+         TriggerClientEvent('esx:showNotification', xPlayer.source, ('Votre entreprise vous a payé ') .. '~g~$' .. xPlayer.job.grade_salary)
+        else
+          TriggerClientEvent('esx:showNotification', xPlayer.source, 'Votre entreprise n\'a plus d\'argent pour vous payer !')
+        end
+      end)
+      end)
+    end
     end
 
     SetTimeout(Config.PaycheckInterval, payCheck)
   end
-
   SetTimeout(Config.PaycheckInterval, payCheck)
 end
