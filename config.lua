f3dc5d9d60df1713cd27e22f03594978d10acc34
KandaSoranyan <32266067+KandaSoranyan@users.noreply.github.com>
2018-02-19 03:31:41 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,12 @@
 Config                          = {}
-Config.MaxPlayers               = 4
+Config.MaxPlayers               = 32
 Config.Accounts                 = { 'bank', 'black_money' }
 Config.AccountLabels            = { bank = 'Banque', black_money = 'Argent Sale' } -- French
 -- Config.AccountLabels            = { bank = 'Bank', black_money = 'Dirty Money' } -- English
-Config.PaycheckInterval         = 2 * 60000
-Config.ShowDotAbovePlayer       = true
+Config.PaycheckInterval         = 7 * 60000
+Config.ShowDotAbovePlayer       = false
 Config.DisableWantedLevel       = true
-Config.RemoveInventoryItemDelay = 1000
+Config.RemoveInventoryItemDelay = 5 * 60000
 Config.EnableWeaponPickup       = true
 Config.Locale                   = 'fr'
-Config.EnableDebug              = true
-
+Config.EnableDebug              = false
