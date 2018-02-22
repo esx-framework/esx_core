7aadbaa58ffa863ad03380fc8a281a8385d7fac9
Unknown <stephane.dos96@gmail.com>
2018-02-22 01:50:38 +0100

    Fix message pack Error

--- server/main.lua
+++ server/main.lua
@@ -5,7 +5,8 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterServerEvent('esx_barbershop:pay')
 AddEventHandler('esx_barbershop:pay', function()
 
-	local xPlayer = ESX.GetPlayerFromId(source)
+	local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
 
 	xPlayer.removeMoney(Config.Price)
 
@@ -15,7 +16,8 @@ end)
 
 ESX.RegisterServerCallback('esx_barbershop:checkMoney', function(source, cb)
 
-	local xPlayer = ESX.GetPlayerFromId(source)
+	local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
 
 	if xPlayer.get('money') >= Config.Price then
 		cb(true)
