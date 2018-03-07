d0e71684842b9098ea317cfc11d57fbf7f17d299
Vanheden <33155577+Vanheden@users.noreply.github.com>
2018-03-07 14:59:15 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -8,6 +8,7 @@ client_scripts {
   '@es_extended/locale.lua',
   'locales/en.lua',
   'locales/fr.lua',
+  'locales/sv.lua',
   'config.lua',
   'client/main.lua'
 }
@@ -16,6 +17,7 @@ server_scripts {
   '@es_extended/locale.lua',
   'locales/en.lua',
   'locales/fr.lua',
+  'locales/sv.lua',
   'config.lua',
   'server/main.lua'
 }
