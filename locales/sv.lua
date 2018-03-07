e743fc872d39fab4cd8c224a4e7a6e35352b292b
Vanheden <33155577+Vanheden@users.noreply.github.com>
2018-03-07 14:24:52 +0100

    Create sv.lua

---
+++ locales/sv.lua
@@ -0,0 +1,23 @@
+Locales['sv'] = {
+
+	['robbery_cancelled'] = 'rånet kommer att avbrytas, du får ingenting!',
+	['robbery_successful'] = 'du lyckades råna och har nu fått ~g~$',
+	['shop_robbery'] = 'Råna Affär',
+	['press_to_rob'] = 'tryck ~INPUT_CONTEXT~ för att råna ~b~',
+	['robbery_of'] = 'store robbery: ~r~',
+	['seconds_remaining'] = '~w~ sekunder kvar',
+	['robbery_cancelled_at'] = '~r~ Rånet avbryt: ~b~',
+	['robbery_has_cancelled'] = '~r~ rånet har blivit avbrutet: ~b~',
+	['already_robbed'] = 'denna affär har redan blivit råna, Vänta lite: ',
+	['seconds'] = 'sekunder.',
+	['rob_in_prog'] = '~r~ pågående rån vid: ~b~',
+	['started_to_rob'] = 'du började att råna ',
+	['do_not_move'] = ', Rör dig inte!',
+	['alarm_triggered'] = 'larmet har utlösts',
+	['hold_pos'] = 'stanna i 5 minuter och pengarna är din!',
+	['robbery_complete'] = '~r~ Rånet avklarat.~s~ ~h~ Spring!',
+	['robbery_complete_at'] = '~r~ Rånet har blivit avslutat och dom har fått pengar: ~b~',
+	['min_two_police'] = 'det måste vara minst~b~2 poliser~s~ för att råna.',
+	['robbery_already'] = '~r~Ett rån pågår redan.',
+
+}
