5ca48a671fcdbde55bf035913daa21e70d601d56
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2018-03-15 12:40:37 +0100

    bump version

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Menu Default'
 
-version '1.0.2'
+version '1.0.3'
 
 client_scripts {
 	'@es_extended/client/wrapper.lua',
