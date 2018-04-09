a86f4232d1717d6a1806703e54343356d995d6af
ElPumpo <samo_kol@hotmail.com>
2018-04-09 12:33:27 +0200

    Added missing manifest info, Swedish language

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,8 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
 description 'ESX Shops'
 
-version '1.0.0'
+version '1.1.0'
 
 client_scripts {
 	'@es_extended/locale.lua',
@@ -9,6 +11,7 @@ client_scripts {
 	'locales/en.lua',
 	'locales/fr.lua',
 	'locales/es.lua',
+	'locales/sv.lua',
 	'config.lua',
 	'client/main.lua'
 }
@@ -20,6 +23,7 @@ server_scripts {
 	'locales/en.lua',
 	'locales/fr.lua',
 	'locales/es.lua',
+	'locales/sv.lua',
 	'@mysql-async/lib/MySQL.lua',
 	'config.lua',
 	'server/main.lua'
---
+++ locales/sv.lua
@@ -0,0 +1,8 @@
+Locales['sv'] = {
+	['shop'] = 'affär',
+	['shops'] = 'affärer',
+	['press_menu'] = 'tryck ~INPUT_CONTEXT~ för att ~y~handla prylar~w~.',
+	['bought'] = 'du har köpt ~y~1x~w~ ~b~%s~w~ för ~r~$%s~w~',
+	['not_enough'] = 'du har inte ~r~tillräckligt~s~ med pengar, det ~y~saknas~w~ ~r~$%s~w~!',
+	['player_cannot_hold'] = 'du har ~r~inte~w~ tillräckligt med ~y~utrymme~w~ för att hålla det!',
+}
