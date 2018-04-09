d673b3f19889dd185a4d1770a26910af6baefc75
ElPumpo <samo_kol@hotmail.com>
2018-04-09 11:56:06 +0200

    Removed unneeded id

--- esx_license.sql
+++ esx_license.sql
@@ -2,11 +2,10 @@ USE `essentialmode`;
 
 CREATE TABLE `licenses` (
   
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
   `label` varchar(255) NOT NULL,
   
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`type`)
 );
 
 CREATE TABLE `user_licenses` (
