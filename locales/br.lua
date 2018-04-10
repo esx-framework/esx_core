a96e5b9c0ada3cfd257dd8c5b6b2acf339a8f17a
renildomarcio <krsolucoesweb@gmail.com>
2018-04-10 03:08:40 -0300

    Update br.lua

--- locales/br.lua
+++ locales/br.lua
@@ -5,5 +5,6 @@ Locales['br'] = {
 	['press_menu'] = 'Pressione ~INPUT_CONTEXT~ para acessar a loja.',
 	['bought'] = 'você comprou ~b~1x ',
 	['not_enough'] = 'você não tem ~r~dinheiro suficiente~s~.'
+        ['player_cannot_hold'] = 'Você ~r~não~s~ tem bastante ~y~espaço livre~s~ no seu inventário!',
 
 }
