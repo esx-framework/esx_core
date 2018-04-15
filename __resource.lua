066bce056131266afd9c073d7a5f6c4a0223f851
LuaDeldu <luadelduwaht@outlook.fr>
2018-04-15 02:06:35 +0200

    Code cleanup
    + Tabs indents (new try),
    + Correct Citizen.Wait(),
    + esx:onPlayerDeath used,
    + Fix nil value when player press E again without leaving the marker.

--- __resource.lua
+++ __resource.lua
@@ -5,17 +5,17 @@ description 'ESX Accessories'
 version '1.0.1'
 
 server_scripts {
-    '@es_extended/locale.lua',
-    'locales/en.lua',
-    'locales/fr.lua',
-    'config.lua',
-    'server/main.lua'
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'server/main.lua'
 }
 
 client_scripts {
-    '@es_extended/locale.lua',
-    'locales/en.lua',
-    'locales/fr.lua',
-    'config.lua',
-    'client/main.lua'
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'client/main.lua'
 }
