0ddf00cf9239e11954bbdc757730baf10629edf3
MHL1337 <mackanlundberg01@gmail.com>
2018-04-15 20:42:36 +0200

    Removed plate

--- esx_garage.sql
+++ esx_garage.sql
@@ -5,7 +5,6 @@ CREATE TABLE `user_parkings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` varchar(60) DEFAULT NULL,
   `garage` varchar(60) DEFAULT NULL,
-  `plate` varchar(60) DEFAULT NULL,
   `zone` int(11) NOT NULL,
   `vehicle` longtext,
 
