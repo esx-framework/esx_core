4dcaf6b9ba25d02fc86bd3389171ff01af58eae0
ElPumpo <samo_kol@hotmail.com>
2018-04-30 15:37:28 +0200

    Fixed vehicles randomly spawning

--- client/main.lua
+++ client/main.lua
@@ -20,6 +20,7 @@ local InsideInstance          = false
 ESX                           = nil
 
 Citizen.CreateThread(function()
+	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
 	end
