934ca309ff97fbd47b408274aa9c62ae06a77a34
ElPumpo <samo_kol@hotmail.com>
2018-06-05 20:16:29 +0200

    Fixed broken sql file

--- esx_identity.sql
+++ esx_identity.sql
@@ -1,12 +1,12 @@
 USE `essentialmode`;
 
-ALTER TABLE `users` (
+ALTER TABLE `users`
 	ADD COLUMN `firstname` VARCHAR(50) NULL DEFAULT '',
 	ADD COLUMN `lastname` VARCHAR(50) NULL DEFAULT '',
 	ADD COLUMN `dateofbirth` VARCHAR(25) NULL DEFAULT '',
 	ADD COLUMN `sex` VARCHAR(10) NULL DEFAULT '',
 	ADD COLUMN `height` VARCHAR(5) NULL DEFAULT ''
-);
+;
 
 CREATE TABLE `characters` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
