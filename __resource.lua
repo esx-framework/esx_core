09cb574992872c1a8a5b01baf6459f2b088a34ce
ElPumpo <samo_kol@hotmail.com>
2018-06-09 13:26:35 +0200

    Added readme, minor cleanup

--- __resource.lua
+++ __resource.lua
@@ -1,26 +1,26 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Menu Default'
+description 'ESX Menu List'
 
-version '1.0.1'
+version '1.0.2'
 
 client_scripts {
 	'@es_extended/client/wrapper.lua',
 	'client/main.lua'
 }
 
-ui_page {
-	'html/ui.html'
-}
+ui_page 'html/ui.html'
 
 files {
 	'html/ui.html',
+
 	'html/css/app.css',
+
 	'html/js/mustache.min.js',
 	'html/js/app.js',
+
 	'html/fonts/pdown.ttf',
-	'html/fonts/bankgothic.ttf',
-	'html/img/cursor.png',
-	'html/img/keys/enter.png',
-	'html/img/keys/return.png',
+	'html/fonts/bankgothic.ttf'
 }
+
+dependency 'es_extended'
