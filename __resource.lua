323452cc55e829581bd72438d714e424f457e26d
ElPumpo <samo_kol@hotmail.com>
2018-06-10 11:07:47 +0200

    Cleanup, added locale system

--- __resource.lua
+++ __resource.lua
@@ -1,15 +1,14 @@
---[[
-
--- ESX Voice
--- Original Author of Script: github.com/aabbfive
--- Edited by: nearbyplayer
-
---]]
-
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Voice Controller'
 
 version '1.0.0'
+version '1.1.0'
 
-client_script	'client/main.lua'
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/sv.lua',
+	'config.lua',
+	'client/main.lua'
+}
---
+++ locales/sv.lua
@@ -0,0 +1,6 @@
+Locales ['sv'] = {
+	['voice']   = '~y~Röst: ~s~%s',
+	['normal']  = 'prata',
+	['shout']   = 'skrik',
+	['whisper'] = 'viska',
+}
