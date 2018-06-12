99623ef6e89375a35a7fac2e144815dceb6e5bcb
AdrineX <toni_pietila@hotmail.com>
2018-06-12 21:49:30 +0300

    Add Finnish lang...

---
+++ locales/fi.lua
@@ -0,0 +1,13 @@
+Locales ['fi'] = {
+
+  ['buy_license'] = 'osta lisenssi?',
+  ['yes'] = 'kyllä',
+  ['no'] = 'ei',
+  ['buy'] = 'sinä ostit',
+  ['not_enough_black'] = 'ei tarpeeksi likaista rahaa',
+  ['not_enough'] = 'sinulla ei ole tarpeeksi rahaa',
+  ['shop'] = 'kauppa',
+  ['shop_menu'] = 'paina ~INPUT_CONTEXT~ avataksesi kauppa ikkuna.',
+  ['map_blip'] = 'asekauppa',
+
+}
