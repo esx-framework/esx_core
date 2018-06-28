d484bda786bde2540d95aaee5484e76a34d8cbd5
MajorMarcin <zelkowski.m@gmail.com>
2018-06-28 19:41:38 +0200

    added Polish language

---
+++ locales/pl.lua
@@ -0,0 +1,63 @@
+Locales['pl'] = {
+  -- Cloakroom
+  ['cloakroom'] = 'szatnia',
+  ['ems_clothes_civil'] = 'ubrania cywilne',
+  ['ems_clothes_ems'] = 'Ubrania EMS',
+  -- Vehicles
+  ['veh_menu'] = 'pojazdy',
+  ['veh_spawn'] = 'Naciśnij ~INPUT_CONTEXT~ żeby wziąść Pojazd lub Helikopter',
+  ['store_veh'] = 'Naciśnij ~INPUT_CONTEXT~ żeby przechować pojazd',
+  ['ambulance'] = 'ambulans',
+  ['helicopter'] = 'helikopter',
+  -- Action Menu
+  ['hospital'] = 'szpital',
+  ['revive_inprogress'] = 'trwa wskrzeszenie',
+  ['revive_complete'] = 'Zostałeś wskrzeszony ~y~%s~s~',
+  ['revive_complete_award'] = 'Zostałeś wskrzeszony ~y~%s~s~ i zarobiono ~g~$%s~s~!',
+  ['heal_inprogress'] = 'Leczysz!',
+  ['heal_complete'] = 'Zostałeś uleczony ~y~%s~s~',
+  ['no_players'] = 'Brak graczy w pobliżu',
+  ['no_vehicles'] = 'Brak pojazdów w pobliżu',
+  ['player_not_unconscious'] = 'Ten gracz nie jest nieprzytomny!',
+  ['player_not_conscious'] = 'Ten gracz nie jest świadomy!',
+  -- Boss Menu
+  ['deposit_society'] = 'Zdeponuj do EMS',
+  ['withdraw_society'] = 'Wyjmij z depozytu EMS',
+  ['boss_actions'] = 'Akcje szefa',
+  -- Misc
+  ['invalid_amount'] = '~r~nieprawidłowa kwota',
+  ['open_menu'] = 'Naciśnij ~INPUT_CONTEXT~ aby otworzyc menu',
+  ['deposit_amount'] = 'kwota depozytu',
+  ['money_withdraw'] = 'wycofana kwota',
+  ['fast_travel'] = 'Naciśnij na ~INPUT_CONTEXT~ do szybkiej podróży.',
+  ['open_pharmacy'] = 'Naciśnij na ~INPUT_CONTEXT~ aby otworzyć aptekę',
+  ['pharmacy_menu_title'] = 'Apteka',
+  ['pharmacy_take'] = 'Weź',
+  ['medikit'] = 'apteczka',
+  ['bandage'] = 'bandaż',
+  ['max_item'] = 'Już masz na sobie dość.',
+  -- F6 Menu
+  ['ems_menu'] = 'Menu EMS',
+  ['ems_menu_title'] = 'EMS Menu - Ambulans',
+  ['ems_menu_revive'] = 'Ożyw gracza',
+  ['ems_menu_putincar'] = 'umieścić w pojeździe',
+  ['ems_menu_small'] = 'Ulecz małe rany',
+  ['ems_menu_big'] = 'Ulecz poważne obrażenia',
+  -- Phone
+  ['alert_ambulance'] = 'Wezwij ambulans',
+  -- Death
+  ['respawn_at_hospital'] = 'Czy chcesz odrodzić się w szpitalu?',
+  ['yes'] = 'tak',
+  ['please_wait'] = 'Jesteś nieprzytomny i będziesz się wykrwawiał ~b~%s minut i %s sekund~s~',
+  ['press_respawn'] = 'Naciśnij [~b~E~w~] aby się odrodzić.',
+  ['respawn_fine'] = 'Zapłaciłeś ~r~$%s~s~ za odrodzenie.',
+  -- Revive
+  ['revive_help'] = 'Ożyw gracza',
+  -- Item
+  ['used_medikit'] = 'Użyłeś 1x Apteczki',
+  ['used_bandage'] = 'Użyłeś 1x Bandaża',
+  ['not_enough_medikit'] = 'Nie posiadasz ~b~apteczki~w~.',
+  ['not_enough_bandage'] = 'Nie posiadasz ~b~bandaża~w~.',
+  ['healed'] = 'zostałeś potraktowany',
+  -- Misc
+}
