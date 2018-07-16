3e855c0f0e049903f602bd353e92b63271d25fd1
MajorMarcin <zelkowski.m@gmail.com>
2018-07-16 11:37:06 +0200

    add pl

---
+++ locales/pl.lua
@@ -0,0 +1,11 @@
+Locales['pl'] = {
+	['shop'] = 'sklep',
+	['shops'] = 'sklepy',
+	['press_menu'] = 'naciśnij ~INPUT_CONTEXT~ żeby wejść do ~y~sklepu~s~.',
+	['bought'] = 'właśnie zakupiłeś ~y~%s~s~ x ~b~%s~s~ za ~g~%s $~s~',
+	['not_enough'] = 'nie masz ~r~wystarczjąco~s~ pięniędzy, ~y~Brakuje Ci~s~ ~r~$%s~s~!',
+	['player_cannot_hold'] = '~r~Nie~s~ masz wystarczająco ~y~wolnego miejsca~s~ w swoim ekwipunku!',
+	['shop_confirm'] = 'chcesz kupić %sx %s za $%s?',
+	['no'] = 'nie',
+	['yes'] = 'tak',
+}
