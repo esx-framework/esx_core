a162cb9b2b75aace17b40c4f2f5f897e5897b388
ElPumpo <samo_kol@hotmail.com>
2018-07-16 14:55:04 +0200

    Fixed undrag when put in vehicle

--- client/main.lua
+++ client/main.lua
@@ -1488,7 +1488,7 @@ AddEventHandler('esx_policejob:putInVehicle', function()
 
       if freeSeat ~= nil then
         TaskWarpPedIntoVehicle(playerPed, vehicle, freeSeat)
-        IsDragged = false
+        DragStatus.IsDragged = false
       end
 
     end
