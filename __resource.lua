a00b76e3b762e76f7711c2969c8d5a693e3abb79
deviljin112 <39461646+deviljin112@users.noreply.github.com>
2018-07-23 23:22:52 +0100

    Polish Translation

--- __resource.lua
+++ __resource.lua
@@ -12,6 +12,7 @@ client_scripts {
 	'locales/fi.lua',
 	'locales/fr.lua',
 	'locales/sv.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'client/main.lua'
 }
@@ -24,6 +25,7 @@ server_scripts {
 	'locales/fi.lua',
 	'locales/fr.lua',
 	'locales/sv.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'server/main.lua'
 }
