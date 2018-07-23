3784200c2c34e911077935d7e79ec903588b265d
MajorMarcin <zelkowski.m@gmail.com>
2018-07-23 18:05:02 +0200

    add pl

--- __resource.lua
+++ __resource.lua
@@ -14,6 +14,7 @@ server_scripts {
 	'locales/fr.lua',
 	'locales/es.lua',
 	'locales/sv.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -27,6 +28,7 @@ client_scripts {
 	'locales/fr.lua',
 	'locales/es.lua',
 	'locales/sv.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'client/main.lua'
 }
