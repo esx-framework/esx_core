4a7065f16671a831e9ed16a7828269f4e083f999
MajorMarcin <zelkowski.m@gmail.com>
2018-07-29 20:46:45 +0200

    blips option and add pl
    added to the config option to hide markers on the map
    add pl translation

---
+++ localization/pl_esx_drugs.sql
@@ -0,0 +1,10 @@
+INSERT INTO `items` (name, label, `limit`) VALUES
+	('weed', 'Topy', 50),
+	('weed_pooch', 'Marihuana', 10),
+	('coke', 'Liście kokainy', 50),
+	('coke_pooch', 'Kokaina', 10),
+	('meth', 'Odlew', 50),
+	('meth_pooch', 'Metafetamina', 10),
+	('opium', 'Opium', 50),
+	('opium_pooch', 'Torebeczka opium', 10)
+;
