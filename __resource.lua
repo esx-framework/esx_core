188a0943d74612f23167a16ead7f8a6c874386e7
ElPumpo <samo_kol@hotmail.com>
2018-08-09 20:06:08 +0200

    Added Swedish locale

--- __resource.lua
+++ __resource.lua
@@ -8,6 +8,7 @@ server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'locales/en.lua',
+	'locales/sv.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -15,6 +16,7 @@ server_scripts {
 client_scripts {
 	'@es_extended/locale.lua',
 	'locales/en.lua',
+	'locales/sv.lua',
 	'config.lua',
 	'client/main.lua',
 	'client/marker.lua'
