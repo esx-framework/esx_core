f02711422f0f06f0a84eda876f7b202f4808f8af
ElPumpo <samo_kol@hotmail.com>
2018-08-13 20:46:05 +0200

    Corrected Swedish locale

--- locales/sv.lua
+++ locales/sv.lua
@@ -1,8 +1,8 @@
 Locales['sv'] = {
 	['invoices'] = 'räkningar',
 	['received_invoice'] = 'du har ~y~mottagit~s~ en räkning',
-	['paid_invoice'] = 'du ~y~betalade~s~ precis en räkning på ~g~$%s~s~',
-	['received_payment'] = 'du har ~y~mottagit~s~ en räkning på ~g~$%s~s~',
+	['paid_invoice'] = 'du ~y~betalade~s~ precis en räkning på ~g~%s SEK~s~',
+	['received_payment'] = 'du har ~y~mottagit~s~ en räkning på ~g~%s SEK~s~',
 	['player_not_online'] = 'personen är inte online',
 	['no_money'] = 'du har ~r~inte råd~s~ för att kunna betala räkningen',
 	['target_no_money'] = 'personen har ~r~inte råd~s~ för att betala räkningen',
