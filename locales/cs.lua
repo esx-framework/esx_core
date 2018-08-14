fe242059a94b77ba5b3dd802ef0f876a08f44416
David Rychlý <davidrychly2@gmail.com>
2018-08-14 23:09:12 +0200

    Czech translate

---
+++ locales/cs.lua
@@ -0,0 +1,15 @@
+Locales['cs'] = {
+	['ooc_help'] = 'poslat out of character zprávu',
+	['ooc_prefix'] = 'OOC | %s',
+	['twt_help'] = 'odeslat tweet',
+	['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+	['me_help'] = 'osobní akce',
+	['me_prefix'] = 'me | %s',
+	['do_help'] = 'rP informace',
+	['do_prefix'] = 'do | %s',
+	['news_help'] = 'oznámit zprávy (nezneužívat)',
+	['news_prefix'] = 'news | %s',
+	['ooc_argument_name'] = 'zpráva',
+	['ooc_argument_help'] = 'zpráva',
+	['ooc_unknown_command'] = '^3%s^0 není platný příkaz!',
+}
