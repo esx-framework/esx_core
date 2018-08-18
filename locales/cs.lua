d131442fc3e4c68149d653c9c1d3b7e0702bc31b
David Rychlý <davidrychly2@gmail.com>
2018-08-18 09:03:26 +0200

    Czech translate typo fix

--- locales/cs.lua
+++ locales/cs.lua
@@ -1,7 +1,7 @@
 Locales['cs'] = {
 
 	['new_job'] = 'máte novou práci!',
-	['access_job_center'] = 'zmáčkněte ~INPUT_PICKUP~ pro přísutp na ~b~Úřad práce~s~.',
+	['access_job_center'] = 'zmáčkněte ~INPUT_PICKUP~ pro přístup na ~b~Úřad práce~s~.',
 	['job_center'] = 'úřad práce',
 
 }
