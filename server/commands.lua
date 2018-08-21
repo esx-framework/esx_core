0cac67ef863d63b135e1a56afe3e86425c2b8513
ElPumpo <samo_kol@hotmail.com>
2018-08-21 21:56:35 +0200

    Renamed new command

--- server/commands.lua
+++ server/commands.lua
@@ -61,7 +61,7 @@ end, function(source, args, user)
   TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
 end, {help = _U('spawn_car'), params = {{name = "car", help = _U('spawn_car_param')}}})
 
-TriggerEvent('es:addGroupCommand', 'cardelete', 'admin', function(source, args, user)
+TriggerEvent('es:addGroupCommand', 'cardel', 'admin', function(source, args, user)
 	TriggerClientEvent('esx:deleteVehicle', source)
 end, function(source, args, user)
 	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
