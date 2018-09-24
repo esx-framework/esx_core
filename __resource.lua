4f6a40199d8c59d8d401826e739ed43de11d8947
Delduwaht <33182571+LuaDeldu@users.noreply.github.com>
2018-09-24 17:50:00 +0200

    Added more ped components (#23)
    - 4 tabs indent
    - add Polish translation (from deviljin112)
    - add more components (eyes color / arms 2 / watches / bracelets / blemishes / ageing / blush / complexion / sun damages / freckles / chest hair / body blemishes)
    - increment version

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'Skin Changer'
 
-version '1.0.2'
+version '1.0.3'
 
 client_scripts {
 	'locale.lua',
@@ -11,6 +11,7 @@ client_scripts {
 	'locales/en.lua',
 	'locales/fi.lua',
 	'locales/fr.lua',
+	'locales/pl.lua',
 	'locales/sv.lua',
 	'config.lua',
 	'client/main.lua'
