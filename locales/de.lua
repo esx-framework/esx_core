d65947e61b650c0d17c5b50d3cf013840aff01e8
ElPumpo <samo_kol@hotmail.com>
2018-10-31 15:25:08 +0100

    Locale cleanup

--- locales/de.lua
+++ locales/de.lua
@@ -1,15 +1,10 @@
-Locales['en'] = {
+Locales['de'] = {
   ['properties'] = 'Immobilien',
   ['property'] = 'Immobilie',
   ['clients'] = 'Kunden',
-  ['remove_comp_money'] = 'Firmengeld abheben',
-  ['dep_money'] = 'Geld einzahlen',
-  ['wash_money'] = 'Geld waschen',
   ['realtor'] = 'Makler',
   ['amount_withdraw'] = 'Betrag zun abheben',
   ['invalid_amount'] = 'ungültiger Betrag',
-  ['amount_deposit'] = 'Betrag zum einzahlen',
-  ['amount_launder'] = 'Betrag zum waschen',
   ['press_to_access'] = 'Drücke ~INPUT_CONTEXT~ um das Menü zu öffnen',
   ['property_actions'] = '{{Zuweisen [Verkaufen]|sell}} {{Zuweisen [Vermieten]|rent}} {{GPS|gps}}',
   ['amount'] = 'Betrag',
@@ -18,4 +13,5 @@ Locales['en'] = {
   ['rent'] = 'Mieten',
   ['sell'] = 'Verkaufen',
   ['realtors'] = 'Makler',
+  ['boss_action'] = 'boss Actions',
 }
--- localization/de_esx_realestateagentjob.sql
+++ localization/de_esx_realestateagentjob.sql
@@ -1,16 +1,16 @@
 USE `essentialmode`;
 
 INSERT INTO `addon_account` (name, label, shared) VALUES
-  ('society_realestateagent','Immobilienverkauf',1)
+	('society_realestateagent','Immobilienverkauf',1)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-  ('realestateagent','Immobilienverkauf')
+	('realestateagent','Immobilienverkauf')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-  ('realestateagent',0,'location','Vermieter',10,'{}','{}'),
-  ('realestateagent',1,'vendeur','Verkäufer',25,'{}','{}'),
-  ('realestateagent',2,'gestion','Manager',40,'{}','{}'),
-  ('realestateagent',3,'boss','Boss',0,'{}','{}')
+	('realestateagent',0,'location','Vermieter',10,'{}','{}'),
+	('realestateagent',1,'vendeur','Verkäufer',25,'{}','{}'),
+	('realestateagent',2,'gestion','Manager',40,'{}','{}'),
+	('realestateagent',3,'boss','Boss',0,'{}','{}')
 ;
