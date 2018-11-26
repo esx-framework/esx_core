a1862bdc4820ff488585da9e56e3f8560f04db5f
David Rychlý <davidrychly2@gmail.com>
2018-11-26 09:11:54 +0100

    Add of Czech translate (#91)

---
+++ locales/cs.lua
@@ -0,0 +1,64 @@
+Locales['cs'] = {
+  -- Cloakroom
+  ['cloakroom'] = 'šatna',
+  ['ems_clothes_civil'] = 'civilní oblečení',
+  ['ems_clothes_ems'] = 'zdravotnické oblečení',
+  -- Vehicles
+  ['veh_menu'] = 'vozidlo',
+  ['veh_spawn'] = 'stiskněte ~INPUT_CONTEXT~ k vyzvednutí vozidla nebo helikoptéry',
+  ['store_veh'] = 'stiskněte ~INPUT_CONTEXT~ k vrácení vozidla',
+  ['ambulance'] = 'sanitka',
+  ['helicopter'] = 'helikoptéra',
+  -- Action Menu
+  ['hospital'] = 'nemocnice',
+  ['revive_inprogress'] = 'probíhá oživení!',
+  ['revive_complete'] = 'oživili jste ~y~%s~s~',
+  ['revive_complete_award'] = 'oživili jste ~y~%s~s~ a dostali jste ~g~$%s~s~!',
+  ['heal_inprogress'] = 'uzdravujete!',
+  ['heal_complete'] = 'uzdravili jste ~y~%s~s~',
+  ['no_players'] = 'v okolí není žádný hráč!',
+  ['no_vehicles'] = 'v okolí není žádné vozidlo',
+  ['player_not_unconscious'] = 'hráč není v bezvědomí!',
+  ['player_not_conscious'] = 'hráč je v bezvědomí!',
+  -- Boss Menu
+  ['boss_actions'] = 'akce šéfa',
+  -- Misc
+  ['invalid_amount'] = '~r~neplatná částka',
+  ['open_menu'] = 'stiskněte ~INPUT_CONTEXT~ k otevření menu',
+  ['deposit_amount'] = 'částka vkladu',
+  ['money_withdraw'] = 'částka výběru',
+  ['fast_travel'] = 'stiskněte ~INPUT_CONTEXT~ k rychlému odcestování.',
+  ['open_pharmacy'] = 'stiskněte ~INPUT_CONTEXT~ k otevření lékárny.',
+  ['pharmacy_menu_title'] = 'lékárna',
+  ['pharmacy_take'] = 'vzít ~y~%s~s~',
+  ['medikit'] = 'lékarnička',
+  ['bandage'] = 'obvaz',
+  ['max_item'] = 'již nesete dostatek věcí.',
+  -- F6 Menu
+  ['ems_menu'] = 'Zdravotnické menu',
+  ['ems_menu_title'] = 'záchranka - Zdravotnické menu',
+  ['ems_menu_revive'] = 'oživit hráče',
+  ['ems_menu_putincar'] = 'naložit do vozidla',
+  ['ems_menu_small'] = 'ošetřit malé zranění',
+  ['ems_menu_big'] = 'ošetřit vážné zranění',
+  -- Phone
+  ['alert_ambulance'] = 'zdravotnický poplach',
+  -- Death
+  ['respawn_available_in'] = 'oživení dostupné za ~b~%s minut a %s sekund~s~\n',
+  ['respawn_bleedout_in'] = 'vykrvácíte za ~b~%s minut a %s sekund~s~\n',
+  ['respawn_bleedout_prompt'] = 'držte [~b~E~s~] pro respawn.',
+  ['respawn_now_fine'] = 'držte [~b~E~s~] pro oživení za ~g~$%s~s~',
+  ['respawn_bleedout_fine_msg'] = 'zaplatili jste ~r~$%s~s~ za respawn.',
+  ['distress_send'] = 'stiskněte [~b~G~s~] pro vyslání tísňového signálu',
+  ['distress_sent'] = 'tísňový signál byl vyslán dostupným jednotkám!',
+  ['distress_message'] = 'nutná lékařská péče: občan v bezvědomí!',
+  ['combatlog_message'] = 'byli jste násilně oživeni, protože jste předtím opustili server, když jste byli mrtví.',
+  -- Revive
+  ['revive_help'] = 'oživit hráče',
+  -- Item
+  ['used_medikit'] = 'použili jste ~y~1x~s~ lékarničku',
+  ['used_bandage'] = 'použili jste ~y~1x~s~ obvaz',
+  ['not_enough_medikit'] = 'nemáte ~b~medikit~s~.',
+  ['not_enough_bandage'] = 'nemáte~b~bandage~s~.',
+  ['healed'] = 'byli jste ošetřeni.',
+}
