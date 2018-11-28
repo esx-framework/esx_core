fec3244e86f84b1e1936c47b1451b9f4c81c5576
D3v <39461646+deviljin112@users.noreply.github.com>
2018-11-28 20:37:06 +0000

    Polish Translation (#17)
    * Polish Translation
    
    * Update pl.lua
    
    quick fix
    
    * Update pl.lua
    
    quick fix

---
+++ locales/pl.lua
@@ -0,0 +1,9 @@
+Locales['pl'] = {
+	['invoices'] = 'faktury',
+	['received_invoice'] = 'właśnie ~r~dostałeś/aś~s~ fakture',
+	['paid_invoice'] = '~g~Zapłaciłeś/aś~s~ fakture: ~r~$%s~s~',
+	['received_payment'] = '~g~Otrzymałeś/aś~s~ zapłate: ~r~$%s~s~',
+	['player_not_online'] = 'gracz nie jest zalogowany',
+	['no_money'] = 'nie masz wystarczająco pieniędzy aby zapłacić tą fakture',
+	['target_no_money'] = 'gracz ~r~nie posiada~s~ wystarczająco pieniędzy aby zapłacić tą fakture!',
+}
