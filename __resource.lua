b24629249edc234aa5f9e67df1205828ab2a29a3
renildomarcio <krsolucoesweb@gmail.com>
2018-12-02 18:09:43 -0200

    Brazilian translation (#6)
    * Create br.lua
    
    Up
    
    * Proper indent and resource manifest

--- __resource.lua
+++ __resource.lua
@@ -6,11 +6,12 @@ version '1.1.0'
 
 client_scripts {
 	'@es_extended/locale.lua',
+	'locales/br.lua',
 	'locales/en.lua',
 	'locales/fi.lua',
+  'locales/fr.lua',
 	'locales/pl.lua',
 	'locales/sv.lua',
-	'locales/fr.lua',
 	'config.lua',
 	'client/main.lua'
 }
