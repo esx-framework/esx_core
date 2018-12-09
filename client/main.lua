4b62ed9cba0078f64cd20cc19e4209f5ee20a6d5
ElPumpo <samo_kol@hotmail.com>
2018-12-09 16:12:49 +0100

    Removed chat suggestions for old commands

--- client/main.lua
+++ client/main.lua
@@ -14,19 +14,15 @@ AddEventHandler('esx_rpchat:sendProximityMessage', function(playerId, title, mes
 end)
 
 Citizen.CreateThread(function()
-	TriggerEvent('chat:addSuggestion', '/ooc',  _U('ooc_help'),  { { name = _U('ooc_argument_name'), help = _U('ooc_argument_help') } } )
-	TriggerEvent('chat:addSuggestion', '/twt',  _U('twt_help'),  { { name = _U('ooc_argument_name'), help = _U('ooc_argument_help') } } )
-	TriggerEvent('chat:addSuggestion', '/me',   _U('me_help'),   { { name = _U('ooc_argument_name'), help = _U('ooc_argument_help') } } )
-	TriggerEvent('chat:addSuggestion', '/do',   _U('do_help'),   { { name = _U('ooc_argument_name'), help = _U('ooc_argument_help') } } )
-	TriggerEvent('chat:addSuggestion', '/news', _U('news_help'), { { name = _U('ooc_argument_name'), help = _U('ooc_argument_help') } } )
+	TriggerEvent('chat:addSuggestion', '/twt',  _U('twt_help'),  { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
+	TriggerEvent('chat:addSuggestion', '/me',   _U('me_help'),   { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
+	TriggerEvent('chat:addSuggestion', '/do',   _U('do_help'),   { { name = _U('generic_argument_name'), help = _U('generic_argument_help') } } )
 end)
 
 AddEventHandler('onResourceStop', function(resource)
 	if resource == GetCurrentResourceName() then
-		TriggerEvent('chat:removeSuggestion', '/ooc')
 		TriggerEvent('chat:removeSuggestion', '/twt')
 		TriggerEvent('chat:removeSuggestion', '/me')
 		TriggerEvent('chat:removeSuggestion', '/do')
-		TriggerEvent('chat:removeSuggestion', '/news')
 	end
 end)
