e6f07a6f805d99b9da315b805ef88377bdcaefdd
ElPumpo <samo_kol@hotmail.com>
2018-12-29 13:48:41 +0100

    Respect sv_maxclients

--- config.lua
+++ config.lua
@@ -1,15 +1,15 @@
-Config                          = {}
-Config.Locale                   = 'fr'
+Config                      = {}
+Config.Locale               = 'fr'
 
-Config.Accounts                 = { 'bank', 'black_money' }
-Config.AccountLabels            = { bank = _U('bank'), black_money = _U('black_money') }
+Config.Accounts             = { 'bank', 'black_money' }
+Config.AccountLabels        = { bank = _U('bank'), black_money = _U('black_money') }
 
-Config.EnableSocietyPayouts     = false -- pay from the society account that the player is employed at? Requirement: esx_society
-Config.ShowDotAbovePlayer       = false
-Config.DisableWantedLevel       = true
-Config.EnableHud                = true -- enable the default hud? Display current job and accounts (black, bank & cash)
+Config.EnableSocietyPayouts = false -- pay from the society account that the player is employed at? Requirement: esx_society
+Config.ShowDotAbovePlayer   = false
+Config.DisableWantedLevel   = true
+Config.EnableHud            = true -- enable the default hud? Display current job and accounts (black, bank & cash)
 
-Config.PaycheckInterval         = 7 * 60000
-Config.MaxPlayers               = 32
+Config.PaycheckInterval     = 7 * 60000
+Config.MaxPlayers           = GetConvarInt('sv_maxclients', 32)
 
-Config.EnableDebug              = false
+Config.EnableDebug          = false
