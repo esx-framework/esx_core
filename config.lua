bdef54f945c63d68489e63eae08e8edb702cee73
ElPumpo <samo_kol@hotmail.com>
2019-01-01 11:53:46 +0100

    Fixed missing alpha

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config                            = {}
 
 Config.DrawDistance               = 100.0
 
-Config.Marker                     = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, rotate = false }
+Config.Marker                     = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
 
 Config.ReviveReward               = 700  -- revive reward, set to 0 if you don't want it enabled
 Config.AntiCombatLog              = true -- enable anti-combat logging?
@@ -27,7 +27,7 @@ Config.RemoveItemsAfterRPDeath    = true
 Config.EarlyRespawnFine           = false
 Config.EarlyRespawnFineAmount     = 5000
 
-Config.RespawnPoint = { coords = vector3(341.07, -1397.31, 32.51), heading = 48.5}
+Config.RespawnPoint = { coords = vector3(341.07, -1397.31, 32.51), heading = 48.5 }
 
 Config.Hospitals = {
 
@@ -41,7 +41,7 @@ Config.Hospitals = {
 		},
 
 		AmbulanceActions = {
-			vector3(268.4, -1363.3, 23.5)
+			vector3(270.5, -1363.0, 23.5)
 		},
 
 		Pharmacies = {
@@ -77,37 +77,37 @@ Config.Hospitals = {
 			{
 				From = vector3(294.7, -1448.1, 29.0),
 				To = { coords = vector3(272.8, -1358.8, 23.5), heading = 0.0 },
-				Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, rotate = false }
+				Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
 			},
 
 			{
 				From = vector3(275.3, -1361, 23.5),
 				To = { coords = vector3(295.8, -1446.5, 28.9), heading = 0.0 },
-				Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, rotate = false }
+				Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
 			},
 
 			{
 				From = vector3(247.3, -1371.5, 23.5),
 				To = { coords = vector3(333.1, -1434.9, 45.5), heading = 138.6 },
-				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, rotate = false }
+				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
 			},
 
 			{
 				From = vector3(335.5, -1432.0, 45.50),
 				To = { coords = vector3(249.1, -1369.6, 23.5), heading = 0.0 },
-				Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, rotate = false }
+				Marker = { type = 1, x = 2.0, y = 2.0, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false }
 			},
 
 			{
 				From = vector3(234.5, -1373.7, 20.9),
 				To = { coords = vector3(320.9, -1478.6, 28.8), heading = 0.0 },
-				Marker = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, rotate = false }
+				Marker = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, a = 100, rotate = false }
 			},
 
 			{
 				From = vector3(317.9, -1476.1, 28.9),
 				To = { coords = vector3(238.6, -1368.4, 23.5), heading = 0.0 },
-				Marker = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, rotate = false }
+				Marker = { type = 1, x = 1.5, y = 1.5, z = 1.0, r = 102, g = 0, b = 102, a = 100, rotate = false }
 			}
 		},
 
@@ -115,14 +115,14 @@ Config.Hospitals = {
 			{
 				From = vector3(237.4, -1373.8, 26.0),
 				To = { coords = vector3(251.9, -1363.3, 38.5), heading = 0.0 },
-				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, rotate = false },
+				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false },
 				Prompt = _U('fast_travel')
 			},
 
 			{
 				From = vector3(256.5, -1357.7, 36.0),
 				To = { coords = vector3(235.4, -1372.8, 26.3), heading = 0.0 },
-				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, rotate = false },
+				Marker = { type = 1, x = 1.5, y = 1.5, z = 0.5, r = 102, g = 0, b = 102, a = 100, rotate = false },
 				Prompt = _U('fast_travel')
 			}
 		}
