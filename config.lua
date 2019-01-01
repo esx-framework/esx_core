179b0b1e59674acc3ec4554dcb9615174cf7173b
ElPumpo <samo_kol@hotmail.com>
2019-01-01 16:32:08 +0100

    Corrected default config

--- config.lua
+++ config.lua
@@ -5,20 +5,20 @@ Config.MarkerType                 = 1
 Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 0.5 }
 Config.MarkerColor                = { r = 50, g = 50, b = 204 }
 
-Config.EnablePlayerManagement     = true
-Config.EnableArmoryManagement     = true
-Config.EnableESXIdentity          = true -- enable if you're using esx_identity
+Config.EnablePlayerManagement     = false
+Config.EnableArmoryManagement     = false
+Config.EnableESXIdentity          = false -- enable if you're using esx_identity
 Config.EnableNonFreemodePeds      = false -- turn this on if you want custom peds
 Config.EnableSocietyOwnedVehicles = false
-Config.EnableLicenses             = true -- enable if you're using esx_license
+Config.EnableLicenses             = false -- enable if you're using esx_license
 
 Config.EnableHandcuffTimer        = true -- enable handcuff timer? will unrestrain player after the time ends
 Config.HandcuffTimer              = 10 * 60000 -- 10 mins
 
-Config.EnableJobBlip              = true -- enable blips for colleagues, requires esx_society
+Config.EnableJobBlip              = false -- enable blips for colleagues, requires esx_society
 
 Config.MaxInService               = -1
-Config.Locale                     = 'en'
+Config.Locale                     = 'fr'
 
 Config.PoliceStations = {
 
