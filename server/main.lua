cc371bad9f2eac8d4d7045c734011c4f8d09f660
ElPumpo <samo_kol@hotmail.com>
2019-01-04 13:26:07 +0100

    Only surround reserved with backtick

--- server/main.lua
+++ server/main.lua
@@ -438,7 +438,7 @@ ESX.RegisterServerCallback('esx_policejob:buyJobVehicle', function(source, cb, v
 	if xPlayer.getMoney() >= price then
 		xPlayer.removeMoney(price)
 
-		MySQL.Async.execute('INSERT INTO owned_vehicles (`owner`, `vehicle`, `plate`, `type`, `job`, `stored`) VALUES (@owner, @vehicle, @plate, @type, @job, @stored)', {
+		MySQL.Async.execute('INSERT INTO owned_vehicles (owner, vehicle, plate, type, job, `stored`) VALUES (@owner, @vehicle, @plate, @type, @job, @stored)', {
 			['@owner'] = xPlayer.identifier,
 			['@vehicle'] = json.encode(vehicleProps),
 			['@plate'] = vehicleProps.plate,
@@ -473,7 +473,7 @@ ESX.RegisterServerCallback('esx_policejob:storeNearbyVehicle', function(source, 
 	if not foundPlate then
 		cb(false)
 	else
-		MySQL.Async.execute('UPDATE owned_vehicles SET stored = true WHERE owner = @owner AND plate = @plate AND job = @job', {
+		MySQL.Async.execute('UPDATE owned_vehicles SET `stored` = true WHERE owner = @owner AND plate = @plate AND job = @job', {
 			['@owner'] = xPlayer.identifier,
 			['@plate'] = foundPlate,
 			['@job'] = xPlayer.job.name
