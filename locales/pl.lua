3714e5e268f9d30532a83a4f6b0db1ec380d04ec
ElPumpo <samo_kol@hotmail.com>
2019-03-15 13:12:37 +0100

    2 space indent

--- locales/pl.lua
+++ locales/pl.lua
@@ -1,4 +1,4 @@
 Locales['pl'] = {
-	['used_bread'] = 'zjadłeś/aś ~y~1x~s~ ~b~chleb~s~',
-	['used_water'] = 'wypiłeś/aś ~y~1x~s~ ~b~woda~s~',
+  ['used_bread'] = 'zjadłeś/aś ~y~1x~s~ ~b~chleb~s~',
+  ['used_water'] = 'wypiłeś/aś ~y~1x~s~ ~b~woda~s~',
 }
