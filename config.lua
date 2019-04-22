d6323124c233f0cc10cea5396516afb28f15048b
ElPumpo <samo_kol@hotmail.com>
2019-04-22 11:37:06 +0200

    OneSync note

--- config.lua
+++ config.lua
@@ -10,6 +10,6 @@ Config.DisableWantedLevel   = true
 Config.EnableHud            = true -- enable the default hud? Display current job and accounts (black, bank & cash)
 
 Config.PaycheckInterval     = 7 * 60000
-Config.MaxPlayers           = GetConvarInt('sv_maxclients', 32)
+Config.MaxPlayers           = GetConvarInt('sv_maxclients', 32) -- set this value to 255 if you're running OneSync
 
 Config.EnableDebug          = false
