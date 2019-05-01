47c81f36eb4205339576b48a2ab8def6f28c2522
ElPumpo <samo_kol@hotmail.com>
2019-05-01 21:56:49 +0200

    Implemented ESX.Table.Sort() for pairs

--- common/modules/table.lua
+++ common/modules/table.lua
@@ -126,3 +126,34 @@ function ESX.Table.Join(t, sep)
 
 	return str
 end
+
+-- Credit: https://stackoverflow.com/a/15706820
+-- Description: sort function for pairs
+function ESX.Table.Sort(t, order)
+	-- collect the keys
+	local keys = {}
+
+	for k,_ in pairs(t) do
+		keys[#keys + 1] = k
+	end
+
+	-- if order function given, sort by it by passing the table and keys a, b,
+	-- otherwise just sort the keys
+	if order then
+		table.sort(keys, function(a,b)
+			return order(t, a, b)
+		end)
+	else
+		table.sort(keys)
+	end
+
+	-- return the iterator function
+	local i = 0
+
+	return function()
+		i = i + 1
+		if keys[i] then
+			return keys[i], t[keys[i]]
+		end
+	end
+end
