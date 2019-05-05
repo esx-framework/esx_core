65b4b0bc588e9bbf8a0395bb98c732b81749fcd5
ElPumpo <samo_kol@hotmail.com>
2019-05-05 09:59:36 +0200

    Updated chat events usage

--- server/main.lua
+++ server/main.lua
@@ -25,27 +25,25 @@ end)
 TriggerEvent('es:addGroupCommand', 'heal', 'admin', function(source, args, user)
 	-- heal another player - don't heal source
 	if args[1] then
-		local target = tonumber(args[1])
-		
+		local playerId = tonumber(args[1])
+
 		-- is the argument a number?
-		if target ~= nil then
-			
+		if playerId then
 			-- is the number a valid player?
-			if GetPlayerName(target) then
-				print('esx_basicneeds: ' .. GetPlayerName(source) .. ' is healing a player!')
-				TriggerClientEvent('esx_basicneeds:healPlayer', target)
-				TriggerClientEvent('chatMessage', target, "HEAL", {223, 66, 244}, "You have been healed!")
+			if GetPlayerName(playerId) then
+				print(('esx_basicneeds: %s healed %s'):format(GetPlayerIdentifier(source, 0), GetPlayerIdentifier(playerId, 0)))
+				TriggerClientEvent('esx_basicneeds:healPlayer', playerId)
+				TriggerClientEvent('chat:addMessage', source, { args = { '^5HEAL', 'You have been healed.' } })
 			else
-				TriggerClientEvent('chatMessage', source, "HEAL", {255, 0, 0}, "Player not found!")
+				TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Player not online.' } })
 			end
 		else
-			TriggerClientEvent('chatMessage', source, "HEAL", {255, 0, 0}, "Incorrect syntax! You must provide a valid player ID")
+			TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Invalid player id.' } })
 		end
 	else
-		-- heal source
-		print('esx_basicneeds: ' .. GetPlayerName(source) .. ' is healing!')
+		print(('esx_basicneeds: %s healed self'):format(GetPlayerIdentifier(source, 0)))
 		TriggerClientEvent('esx_basicneeds:healPlayer', source)
 	end
 end, function(source, args, user)
-	TriggerClientEvent('chatMessage', source, "HEAL", {255, 0, 0}, "Insufficient Permissions.")
-end, {help = "Heal a player, or yourself - restores thirst, hunger and health."})
+	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
+end, {help = 'Heal a player, or yourself - restores thirst, hunger and health.', params = {{name = 'playerId', help = '(optional) player id'}}})
