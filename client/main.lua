cd0ede58651d137ffefe6ef426329f4d7e409b42
SurDaft - Jack Stupple <surdaft@users.noreply.github.com>
2019-05-06 01:08:58 +0100

    fix empty shop bug

--- client/main.lua
+++ client/main.lua
@@ -27,6 +27,10 @@ Citizen.CreateThread(function()
 
 	ESX.TriggerServerCallback('esx_shops:requestDBItems', function(ShopItems)
 		for k,v in pairs(ShopItems) do
+			if (Config.Zones[k] == nil) then
+				Config.Zones[k] = {}		
+			end
+					
 			Config.Zones[k].Items = v
 		end
 	end)
