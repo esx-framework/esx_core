a18fa50463ce9402595360bf5268969b94b56926
ElPumpo <samo_kol@hotmail.com>
2019-05-20 23:06:03 +0200

    Proper hud natives

--- client/main.lua
+++ client/main.lua
@@ -24,10 +24,9 @@ end)
 
 function drawTxt(x,y, width, height, scale, text, r,g,b,a, outline)
 	SetTextFont(0)
-	SetTextProportional(0)
 	SetTextScale(scale, scale)
 	SetTextColour(r, g, b, a)
-	SetTextDropShadow(0, 0, 0, 0,255)
+	SetTextDropshadow(0, 0, 0, 0,255)
 	SetTextEdge(1, 0, 0, 0, 255)
 	SetTextDropShadow()
 	if outline then SetTextOutline() end
