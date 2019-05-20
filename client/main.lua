f408e3a56a8c048c2379cdf7b8219a031498a859
ElPumpo <samo_kol@hotmail.com>
2019-05-20 23:04:04 +0200

    Proper hud natives

--- client/main.lua
+++ client/main.lua
@@ -14,10 +14,9 @@ local voice = {default = 5.0, shout = 12.0, whisper = 1.0, current = 0, level = 
 
 function drawLevel(r, g, b, a)
 	SetTextFont(4)
-	SetTextProportional(1)
 	SetTextScale(0.5, 0.5)
 	SetTextColour(r, g, b, a)
-	SetTextDropShadow(0, 0, 0, 0, 255)
+	SetTextDropshadow(0, 0, 0, 0, 255)
 	SetTextEdge(1, 0, 0, 0, 255)
 	SetTextDropShadow()
 	SetTextOutline()
