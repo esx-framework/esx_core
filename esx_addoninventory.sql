8d47719dbfc4a7b2911e90f661550f81069bb4d9
ElPumpo <samo_kol@hotmail.com>
2019-05-31 23:22:14 +0200

    One more index

--- esx_addoninventory.sql
+++ esx_addoninventory.sql
@@ -10,12 +10,13 @@ CREATE TABLE `addon_inventory` (
 
 CREATE TABLE `addon_inventory_items` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`inventory_name` VARCHAR(255) NOT NULL,
-	`name` VARCHAR(255) NOT NULL,
+	`inventory_name` VARCHAR(100) NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
 	`count` INT(11) NOT NULL,
 	`owner` VARCHAR(60) DEFAULT NULL,
 
 	PRIMARY KEY (`id`),
 	INDEX `index_addon_inventory_items_inventory_name_name` (`inventory_name`, `name`),
-	INDEX `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`, `name`, `owner`)
+	INDEX `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`, `name`, `owner`),
+	INDEX `index_addon_inventory_inventory_name` (`inventory_name`)
 );
