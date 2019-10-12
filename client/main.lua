6d2b30a511fd3ebf3853fa4605bc9e149eb69418
ElPumpo <samo_kol@hotmail.com>
2019-10-12 23:46:05 +0200

    Removed keys table

--- client/main.lua
+++ client/main.lua
@@ -1,15 +1,3 @@
-local Keys = {
-  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
-  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
-  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
-  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
 local isDead = false
 local inAnim = false
 ESX = nil
@@ -21,13 +9,9 @@ Citizen.CreateThread(function()
 	end
 end)
 
-AddEventHandler('esx:onPlayerDeath', function(data)
-	isDead = true
-end)
+AddEventHandler('esx:onPlayerDeath', function(data) isDead = true end)
 
-AddEventHandler('playerSpawned', function(spawn)
-	isDead = false
-end)
+AddEventHandler('playerSpawned', function(spawn) isDead = false end)
 
 function startAttitude(lib, anim)
 	ESX.Streaming.RequestAnimSet(lib, function()
@@ -37,7 +21,7 @@ end
 
 function startAnim(lib, anim)
 	ESX.Streaming.RequestAnimDict(lib, function()
-		TaskPlayAnim(PlayerPedId(), lib, anim, 8.0, -8.0, -1, 0, 0, false, false, false)
+		TaskPlayAnim(PlayerPedId(), lib, anim, 8.0, -8.0, -1, 0, 0.0, false, false, false)
 	end)
 end
 
@@ -52,8 +36,7 @@ function OpenAnimationsMenu()
 		table.insert(elements, {label = Config.Animations[i].label, value = Config.Animations[i].name})
 	end
 
-	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'animations',
-	{
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'animations', {
 		title    = 'Animations',
 		align    = 'top-left',
 		elements = elements
@@ -85,8 +68,7 @@ function OpenAnimationsSubMenu(menu)
 		end
 	end
 
-	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'animations_sub',
-	{
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'animations_sub', {
 		title    = title,
 		align    = 'top-left',
 		elements = elements
@@ -112,11 +94,11 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 
-		if IsControlJustReleased(0, Keys['F3']) and IsInputDisabled(0) and not isDead then
+		if IsControlJustReleased(0, 170) and IsInputDisabled(0) and not isDead then
 			OpenAnimationsMenu()
 		end
 
-		if IsControlJustReleased(0, Keys['X']) and IsInputDisabled(0) and not isDead then
+		if IsControlJustReleased(0, 73) and IsInputDisabled(0) and not isDead then
 			ClearPedTasks(PlayerPedId())
 		end
 
