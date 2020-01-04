f51d8cb281aa158b3bc544efc036b62d453c38c3
rex2630 <rex2630@gmail.com>
2020-01-04 15:16:16 +0100

    Updated SQL to weight

--- esx_basicneeds.sql
+++ esx_basicneeds.sql
@@ -1,6 +1,6 @@
 USE `essentialmode`;
 
-INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
 	('bread', 'Pain', 10),
 	('water', 'Eau', 5)
 ;
