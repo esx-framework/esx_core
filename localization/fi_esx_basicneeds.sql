68e2fc52bf8f7a2f0a370b914ca35fe30b241837
rex2630 <rex2630@gmail.com>
2020-01-06 15:52:24 +0100

    SQL weight

--- localization/fi_esx_basicneeds.sql
+++ localization/fi_esx_basicneeds.sql
@@ -1,6 +1,6 @@
 USE `essentialmode`;
 
-INSERT INTO `items` (`name`, `label`, `limit`) VALUES
-	('bread', 'Leipä', 10),
-	('water', 'Vesi', 5)
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
+	('bread', 'Leipä', 1),
+	('water', 'Vesi', 1)
 ;
