dc1ba4f1ed5f9738a6f80201b83fbe81fc06eca4
maiconkkl <ma_bonitao@hotmail.com>
2020-01-06 22:10:59 -0300

    Correcting and improving translation

--- locales/br.lua
+++ locales/br.lua
@@ -4,7 +4,7 @@ Locales['br'] = {
   ['received_invoice'] = 'você ~r~recebeu~s~ uma fatura',
   ['paid_invoice'] = 'você ~g~pagou~s~ uma fatura de ~r~$%s~s~',
   ['received_payment'] = 'você ~g~recebeu~s~ um pagamento de ~r~$%s~s~',
-  ['player_not_online'] = 'o jogador não está logado',
-  ['no_money'] = 'you do not have enough money to pay this bill',
-  ['target_no_money'] = 'the player ~r~does not~s~ have enough money to pay the bill!',
+  ['player_not_online'] = 'o jogador não está na cidade',
+  ['no_money'] = 'você não tem dinheiro suficiente para pagar esta fatura',
+  ['target_no_money'] = 'o cidadão ~r~não tem~s~ dinheiro suficiente para pagar a conta!',
 }
