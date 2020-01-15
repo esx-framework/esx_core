b587a25c924769038ac40b116c92e0c54cb105f4
ElPumpo <samo_kol@hotmail.com>
2020-01-15 10:47:18 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 Config = {}
 
+Config.Locale = 'fr'
+Config.OnlyFirstname = false
 Config.EnableESXIdentity = false -- only turn this on if you are using esx_identity and want to use RP names
-Config.OnlyFirstname     = false
