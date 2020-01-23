3e52e7ece0fbc1b9c756371f26d0138bbe89bc7c
rex2630 <rex2630@gmail.com>
2020-01-23 16:51:34 +0100

    Create cs_esx_accessories.sql

---
+++ localization/cs_esx_accessories.sql
@@ -0,0 +1,8 @@
+USE `essentialmode`;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('user_ears', 'Usi', 0),
+	('user_glasses', 'Bryle', 0),
+	('user_helmet', 'Helma', 0),
+	('user_mask', 'Maska', 0)
+;
