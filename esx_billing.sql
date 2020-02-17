0bb08e6f3ef054d989140f289d15fe6cf47ec2e8
ElPumpo <samo_kol@hotmail.com>
2020-02-17 23:26:20 +0100

    Update esx_billing.sql

--- esx_billing.sql
+++ esx_billing.sql
@@ -2,10 +2,10 @@ USE `essentialmode`;
 
 CREATE TABLE `billing` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`identifier` varchar(255) NOT NULL,
-	`sender` varchar(255) NOT NULL,
+	`identifier` varchar(40) NOT NULL,
+	`sender` varchar(40) NOT NULL,
 	`target_type` varchar(50) NOT NULL,
-	`target` varchar(255) NOT NULL,
+	`target` varchar(40) NOT NULL,
 	`label` varchar(255) NOT NULL,
 	`amount` int(11) NOT NULL,
 
