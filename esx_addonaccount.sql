f074a1bca504b1195b732a085ddd8bc10d532b55
ElPumpo <samo_kol@hotmail.com>
2020-02-17 23:26:08 +0100

    Update esx_addonaccount.sql

--- esx_addonaccount.sql
+++ esx_addonaccount.sql
@@ -12,7 +12,7 @@ CREATE TABLE `addon_account_data` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`account_name` VARCHAR(100) DEFAULT NULL,
 	`money` INT(11) NOT NULL,
-	`owner` VARCHAR(100) DEFAULT NULL,
+	`owner` VARCHAR(40) DEFAULT NULL,
 
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `index_addon_account_data_account_name_owner` (`account_name`, `owner`),
