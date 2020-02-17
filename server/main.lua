7a4968a6e0b0104f424591b6772730c6e7c72581
ElPumpo <samo_kol@hotmail.com>
2020-02-17 21:53:39 +0100

    Fix for latest ESX

--- server/main.lua
+++ server/main.lua
@@ -4,7 +4,6 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterUsableItem('bread', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
-
 	xPlayer.removeInventoryItem('bread', 1)
 
 	TriggerClientEvent('esx_status:add', source, 'hunger', 200000)
@@ -14,7 +13,6 @@ end)
 
 ESX.RegisterUsableItem('water', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
-
 	xPlayer.removeInventoryItem('water', 1)
 
 	TriggerClientEvent('esx_status:add', source, 'thirst', 200000)
@@ -22,28 +20,9 @@ ESX.RegisterUsableItem('water', function(source)
 	xPlayer.showNotification(_U('used_water'))
 end)
 
-TriggerEvent('es:addGroupCommand', 'heal', 'admin', function(source, args, user)
-	-- heal another player - don't heal source
-	if args[1] then
-		local playerId = tonumber(args[1])
-
-		-- is the argument a number?
-		if playerId then
-			-- is the number a valid player?
-			if GetPlayerName(playerId) then
-				print(('esx_basicneeds: %s healed %s'):format(GetPlayerIdentifier(source, 0), GetPlayerIdentifier(playerId, 0)))
-				TriggerClientEvent('esx_basicneeds:healPlayer', playerId)
-				TriggerClientEvent('chat:addMessage', source, { args = { '^5HEAL', 'You have been healed.' } })
-			else
-				TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Player not online.' } })
-			end
-		else
-			TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Invalid player id.' } })
-		end
-	else
-		print(('esx_basicneeds: %s healed self'):format(GetPlayerIdentifier(source, 0)))
-		TriggerClientEvent('esx_basicneeds:healPlayer', source)
-	end
-end, function(source, args, user)
-	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
-end, {help = 'Heal a player, or yourself - restores thirst, hunger and health.', params = {{name = 'playerId', help = '(optional) player id'}}})
+ESX.RegisterCommand('heal', 'admin', function(xPlayer, args, showError)
+	args.playerId.triggerEvent('esx_basicneeds:healPlayer')
+	args.playerId.triggerEvent('chat:addMessage', {args = {'^5HEAL', 'You have been healed.'}})
+end, true, {help = 'Heal a player, or yourself - restores thirst, hunger and health.', validate = true, arguments = {
+	{name = 'playerId', help = 'the player id', type = 'player'}
+}})
