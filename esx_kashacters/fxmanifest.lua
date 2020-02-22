2d63e61e32f652d122ec83427ee6b0b742c2aa2b
RikoDEV <18230443+RikoDEV@users.noreply.github.com>
2020-02-22 15:10:26 +0100

    Update and rename __resource.lua to fxmanifest.lua

--- esx_kashacters/fxmanifest.lua
+++ esx_kashacters/fxmanifest.lua
@@ -1,4 +1,6 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+
+game 'gta5'
 
 server_scripts {
     "@mysql-async/lib/MySQL.lua",
@@ -12,8 +14,11 @@ client_scripts {
 ui_page {
     'html/ui.html',
 }
+
 files {
     'html/ui.html',
     'html/css/main.css',
     'html/js/app.js',
 }
+
+dependency 'es_extended'
