7efcf85abb73d44fed3630a47a756878170fb007
ElPumpo <samo_kol@hotmail.com>
2020-03-09 12:35:40 +0100

    Fixed indent mistake

--- server/main.lua
+++ server/main.lua
@@ -282,7 +282,7 @@ AddEventHandler('esx:giveInventoryItem', function(target, type, itemName, itemCo
 	elseif type == 'item_ammo' then
 		if sourceXPlayer.hasWeapon(itemName) then
 			local weaponNum, weapon = sourceXPlayer.getWeapon(itemName)
-				
+
 			if targetXPlayer.hasWeapon(itemName) then
 				local _, weaponObject = ESX.GetWeapon(itemName)
 
