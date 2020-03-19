34df4887553ed4eccd78830842c94ab03b7be6e9
rex2630 <rex2630@gmail.com>
2020-03-19 23:01:19 +0100

    French as default locale

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config.DrawDistance = 100
 Config.Size = {x = 1.5, y = 1.5, z = 1.5}
 Config.Color = {r = 0, g = 128, b = 255}
 Config.Type = 1
-Config.Locale = 'en'
+Config.Locale = 'fr'
 
 Config.Zones = {
 
