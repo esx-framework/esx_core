dcc2aa762fcf9afd6c71976c5fd73a7dc67c009d
Marvbell110 <34713144+Marvbell110@users.noreply.github.com>
2020-03-21 14:20:39 +0100

    Update de.lua

--- locales/de.lua
+++ locales/de.lua
@@ -1,12 +1,12 @@
 Locales['de'] = {
   ['shop'] = 'geschäft',
   ['shops'] = 'geschäfte',
-  ['press_menu'] = 'drücke ~INPUT_CONTEXT~ um auf das Geschäft zuzugreifen.',
+  ['press_menu'] = 'drücke ~INPUT_CONTEXT~ um auf das Geschäft zuzugreifen',
   ['shop_item'] = '$%s',
-  ['bought'] = '~y~%sx~s~ ~b~%s~s~ wurde gekauft für ~r~$%s~s~.',
-  ['not_enough'] = '~r~ $%s fehlen dir!',
-  ['player_cannot_hold'] = 'du hast ~r~nicht genug~s~ ~y~freien Platz~s~, in deinem Inventar!',
+  ['bought'] = '~y~%sx~s~ ~b~%s~s~ wurde gekauft für ~r~$%s~s~',
+  ['not_enough'] = '~r~ $%s fehlen dir',
+  ['player_cannot_hold'] = 'du hast ~r~nicht genug~s~ ~y~freien Platz~s~, in deinem Inventar',
   ['shop_confirm'] = 'kaufe %sx %s für $%s?',
-  ['no'] = 'Nein',
-  ['yes'] = 'Ja',
+  ['no'] = 'nein',
+  ['yes'] = 'ja',
 }
