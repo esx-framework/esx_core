77bd7fa334083b5bb1e260a11d77b07cb0b63154
rex2630 <rex2630@gmail.com>
2020-03-21 16:34:36 +0100

    vector check fixed

--- client/main.lua
+++ client/main.lua
@@ -105,7 +105,7 @@ Citizen.CreateThread(function()
 
 		for k,v in pairs(Config.Zones) do
 			for i = 1, #v.Pos, 1 do
-				local distance = #(playerCoords - vector3(v.Pos[i].x, v.Pos[i].y, v.Pos[i].z))
+				local distance = #(playerCoords - v.Pos[i])
 
 				if distance < Config.DrawDistance then
 					DrawMarker(Config.Type, v.Pos[i], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Config.Size.x, Config.Size.y, Config.Size.z, Config.Color.r, Config.Color.g, Config.Color.b, 100, false, true, 2, false, nil, nil, false)
