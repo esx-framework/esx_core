544e0e0dc3b54d7a8bb131215effb86f8f605da4
Minikeysi <43723896+Minikeys@users.noreply.github.com>
2020-04-02 20:24:53 +0200

    Update fr.lua
    Better translate for FR.

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,8 +1,8 @@
 Locales['fr'] = {
-  ['whitelist_check']     = 'making sure you\'re whitelisted on this server . . .',
-  ['not_whitelisted']     = 'ceci est un serveur avec whitelist',
-  ['whitelist_empty']     = 'the whitelist hasn\'t been loaded yet, or alternatively no one has been whitelisted!',
-  ['license_missing']     = 'your license could not be found',
-  ['help_whitelist_add']  = 'ajouter quelqu\'un dans la Whitelist',
-  ['help_whitelist_load'] = 'recharger la Whitelist',
+  ['whitelist_check']     = 'Etes vous sûr d\'être whitelisté sur ce serveur ...',
+  ['not_whitelisted']     = 'Ce serveur utilise une whitelist',
+  ['whitelist_empty']     = 'La whitelist n\'a pas été rechargé ou vous n\'avez pas été whitelisté',
+  ['license_missing']     = 'Votre license est introuvable',
+  ['help_whitelist_add']  = 'Ajouter un joueur à la Whitelist',
+  ['help_whitelist_load'] = 'Recharger la Whitelist',
 }
