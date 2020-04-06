6c1bcd330bbbc60517c38921b65afb84cbb1f55d
ArkSeyonet <31637812+ArkSeyonet@users.noreply.github.com>
2020-04-06 10:14:55 -0500

    Fix Indent For Locales

--- locales/cs.lua
+++ locales/cs.lua
@@ -1,5 +1,5 @@
 Locales['cs'] = {
-    ['data_incorrect'] = 'Data byla zadána nesprávně. Prosím zkuste to znovu.',
-    ['invalid_format'] = 'Data byla nesprávně naformátována. Prosím zkuste to znovu.',
-    ['no_identifiers'] = '[Chyba] Nelze najít identifikátory.'
+  ['data_incorrect'] = 'Data byla zadána nesprávně. Prosím zkuste to znovu.',
+  ['invalid_format'] = 'Data byla nesprávně naformátována. Prosím zkuste to znovu.',
+  ['no_identifiers'] = '[Chyba] Nelze najít identifikátory.'
 }
--- locales/en.lua
+++ locales/en.lua
@@ -1,5 +1,5 @@
 Locales['en'] = {
-    ['data_incorrect'] = 'Data entered incorrectly. Please try again.',
-    ['invalid_format'] = 'Data formatted incorrectly. Please try again.',
-    ['no_identifiers'] = '[Error] Could not find identifiers.'
+  ['data_incorrect'] = 'Data entered incorrectly. Please try again.',
+  ['invalid_format'] = 'Data formatted incorrectly. Please try again.',
+  ['no_identifiers'] = '[Error] Could not find identifiers.'
 }
