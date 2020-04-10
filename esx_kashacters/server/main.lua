1fa58f645cec982559e080880f246cd35ab051da
Henric Backman <b-d-t@msn.com>
2020-04-10 04:46:50 +0200

    Fix cash/bank showing as undefined with new ESX

--- esx_kashacters/server/main.lua
+++ esx_kashacters/server/main.lua
@@ -57,6 +57,9 @@ function GetPlayerCharacters(source)
   for i = 1, #Chars, 1 do
     charJob = MySQLAsyncExecute("SELECT * FROM `jobs` WHERE `name` = '"..Chars[i].job.."'")
     charJobgrade = MySQLAsyncExecute("SELECT * FROM `job_grades` WHERE `grade` = '"..Chars[i].job_grade.."'")
+    local accounts = json.decode(Chars[1].accounts)
+    Chars[i].bank = accounts["bank"]
+    Chars[i].money = accounts["money"]
     Chars[i].job = charJob[1].label
     Chars[i].job_grade = charJobgrade[1].label
   end
