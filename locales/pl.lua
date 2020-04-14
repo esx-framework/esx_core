b053a419e7b07d4780e3fc839eed2f1a706b45fb
userMacieG <palatim3@gmail.com>
2020-04-14 19:35:49 +0200

    Create pl.lua

---
+++ locales/pl.lua
@@ -0,0 +1,31 @@
+Locales['pl'] = {
+  -- shop
+  ['boat_shop'] = 'sklep z łodziami',
+  ['boat_shop_open'] = 'naciśnij ~INPUT_CONTEXT~ aby otworzyć ~y~sklep z łodziami~s~.',
+  ['boat_shop_confirm'] = 'kup <span style="color: yellow;">%s</span> za <span style="color: orange;">$%s</span>?',
+  ['boat_shop_bought'] = '~y~zakupiłeś/aś~s~ ~b~%s~s~ za ~g~$%s~s~',
+  ['boat_shop_nomoney'] = 'nie posiadasz ~r~wystarczająco~s~ pieniędzy aby zakupić te łódź!',
+  ['confirm_no'] = 'nie',
+  ['confirm_yes'] = 'tak',
+
+  -- garage
+  ['garage'] = 'garaż: Lodzie',
+  ['garage_open'] = 'naciśnij ~INPUT_CONTEXT~ aby otworzyć ~y~garaż z łodziami~s~.',
+  ['garage_store'] = 'naciśnij ~INPUT_CONTEXT~ aby ~y~schować~s~ swoją łódź do garażu',
+  ['garage_taken'] = 'łódź zostałą wyjęta z garażu',
+  ['garage_stored'] = 'łódź została schowana do garażu',
+  ['garage_noboats'] = 'nie posiadasz żadnych łodzi!',
+  ['garage_blocked'] = 'łódź nie może zostać wyjęta dlatego że coś blokuje jej miejsce narodzenia',
+  ['garage_notowner'] = 'nie jesteś właścicielem tej łodzi!',
+
+  -- license
+  ['license_menu'] = 'kup licencje na płwanie łodziami?',
+  ['license_buy_no'] = 'nie',
+  ['license_buy_yes'] = 'kup licencje na pływanie łodziami <span style="color: green;">$%s</span>',
+  ['license_bought'] = 'zakupiłeś/aś ~y~licencje na pływanie łodziami~s~ za ~g~$%s~s~',
+  ['license_nomoney'] = 'nie posiadasz  ~r~wystarczająco~s~ pieniędzy aby zakupić ~y~licencje na pływanie łodziami~s~!',
+
+  -- blips
+  ['blip_garage'] = 'garaż: Lodzie',
+  ['blip_shop'] = 'sklep z łodziami',
+}
