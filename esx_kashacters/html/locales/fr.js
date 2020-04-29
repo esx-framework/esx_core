55b126172a7a7785866641f697f72bc3656d06fb
Henric Backman <b-d-t@msn.com>
2020-04-29 22:53:28 +0200

    add missing translation

--- esx_kashacters/html/locales/fr.js
+++ esx_kashacters/html/locales/fr.js
@@ -1,5 +1,6 @@
 const translate = new Object();
 
+translate.name = "Nom";
 translate.job = "Métier";
 translate.bank = "Banque";
 translate.money = "Argent";
@@ -8,5 +9,6 @@ translate.dob = "Date de naissance";
 translate.new = "Créer un personnage";
 translate.delete = "SUPPRIMER";
 translate.play = "JOUER";
+translate.playNew = "CRÉER UN PERSONNAGE";
 translate.modalTitle = "Êtes-vous sûr?";
 translate.modalText = "En supprimant votre personnage, vous ne pourrez pas le récupérer. Toutes les informations seront supprimées.";
--- esx_kashacters/html/locales/pl.js
+++ esx_kashacters/html/locales/pl.js
@@ -1,5 +1,6 @@
 const translate = new Object();
 
+translate.name = "imię";
 translate.job = "Zajęcie";
 translate.bank = "Bank";
 translate.money = "Gotówka";
@@ -8,5 +9,6 @@ translate.dob = "Data urodzenia";
 translate.new = "Utwórz nową postać";
 translate.delete = "KASOWAĆ";
 translate.play = "GRAĆ";
+translate.playNew = "TWORZYĆ POSTAĆ";
 translate.modalTitle = "Jesteś pewny?";
 translate.modalText = "Po usunięciu konta nie będziemy mogli go odzyskać, wszystkie nieruchomości, samochody itp. Zostaną usunięte";
