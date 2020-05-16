738c109c0d188d895856a96cf1d667ddc09c3698
ArkSeyonet <31637812+ArkSeyonet@users.noreply.github.com>
2020-05-16 17:02:21 -0500

    Update config.lua
    - Improve Config Documentation
    - Fix Config.UseDeferrals to default false

--- config.lua
+++ config.lua
@@ -1,12 +1,30 @@
 Config                  = {}
 Config.Locale           = 'en'
+
+-- [Config.EnableCommands]
+-- Enables Commands Such As /char and /chardel
 Config.EnableCommands   = true
-Config.UseSteamID       = false
-Config.UseDeferrals     = true
+
+-- [Config.UseSteamID]
+-- Changes the identifiers from Rockstar License To Steam. In order to use this, you will have to have
+-- made edits to your ESX in order to use Steam identifiers all around, because new ESX uses Rockstar
+-- license. I just made it easier for people that convert to steam identifiers.
+Config.UseSteamID       = false 
+
+-- [Config.UseDeferrals]
+-- EXPERIMENTAL Character Registration Method. This will allow players to create identities in deferrals
+-- before they ever join the server. This still needs time of testing to see what issues that we have
+-- in linux servers and windows servers, and which artifacts it will work on, and which artifacts it
+-- will not work on. If you don't know a lot about this, I would suggest to set it false for now.
+Config.UseDeferrals     = false -- EXPERIMENTAL Character Registration Method.
+
+
+-- These values are for the second input validation in server/main.lua
 Config.MaxNameLength    = 16
 Config.MinHeight        = 48
 Config.MaxHeight        = 96
 Config.LowestYear       = 1900
 Config.HighestYear      = 2020
 
+-- You can leave this set to false, unless you are trying to solve an issue with your identity.
 Config.EnableDebugging  = false
