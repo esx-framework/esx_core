10ef24add730e974ac21eb03e90dd8840e7d947d
shortybsd <shortybsd@gmail.com>
2020-05-16 22:23:09 -0500

    Update esx_addonaccount.sql
    INT - Integer display width is deprecated and will be removed in a future release.

--- esx_addonaccount.sql
+++ esx_addonaccount.sql
@@ -3,15 +3,15 @@ USE `es_extended`;
 CREATE TABLE `addon_account` (
 	`name` VARCHAR(60) NOT NULL,
 	`label` VARCHAR(100) NOT NULL,
-	`shared` INT(11) NOT NULL,
+	`shared` INT NOT NULL,
 
 	PRIMARY KEY (`name`)
 );
 
 CREATE TABLE `addon_account_data` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`id` INT NOT NULL AUTO_INCREMENT,
 	`account_name` VARCHAR(100) DEFAULT NULL,
-	`money` INT(11) NOT NULL,
+	`money` INT NOT NULL,
 	`owner` VARCHAR(40) DEFAULT NULL,
 
 	PRIMARY KEY (`id`),
