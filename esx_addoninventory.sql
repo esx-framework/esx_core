861d03555dff6e1c54bbcb089303c831d180d934
shortybsd <shortybsd@gmail.com>
2020-05-16 22:24:36 -0500

    Update esx_addoninventory.sql
    INT - Integer display width is deprecated and will be removed in a future release.

--- esx_addoninventory.sql
+++ esx_addoninventory.sql
@@ -3,16 +3,16 @@ USE `es_extended`;
 CREATE TABLE `addon_inventory` (
 	`name` VARCHAR(60) NOT NULL,
 	`label` VARCHAR(100) NOT NULL,
-	`shared` INT(11) NOT NULL,
+	`shared` INT NOT NULL,
 
 	PRIMARY KEY (`name`)
 );
 
 CREATE TABLE `addon_inventory_items` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`id` INT NOT NULL AUTO_INCREMENT,
 	`inventory_name` VARCHAR(100) NOT NULL,
 	`name` VARCHAR(100) NOT NULL,
-	`count` INT(11) NOT NULL,
+	`count` INT NOT NULL,
 	`owner` VARCHAR(40) DEFAULT NULL,
 
 	PRIMARY KEY (`id`),
