2856cf44235b55d745aa7c547b799f88822d4786
Jimmy Bassy <38838740+JimmyBassy@users.noreply.github.com>
2020-06-28 23:14:04 +0200

    Create es.lua

---
+++ locales/es.lua
@@ -0,0 +1,12 @@
+Locales['es'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'Mandar tweet',
+  ['twtanon_help'] = 'Mandar tweet anonimo',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'Accion Global',
+  ['me_prefix'] = 'me | %s',
+  ['do_help'] = 'Accion Personal',
+  ['do_prefix'] = 'do | %s',
+  ['generic_argument_name'] = 'mensaje',
+  ['generic_argument_help'] = 'el mensaje',
+}
