ef3a1990fd0bd32f2cdab8c68556608e6d0cef40
Jougito - Kheros <jougito@hotmail.es>
2020-07-17 18:43:08 +0200

    Spanish translation implemented

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,33 +7,35 @@ description 'ESX Accessories'
 version '1.1.0'
 
 server_scripts {
-	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/ru.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/cs.lua',
-	'locales/pl.lua',
-	'config.lua',
-	'server/main.lua'
+    '@es_extended/locale.lua',
+    'locales/en.lua',
+    'locales/es.lua',
+    'locales/ru.lua',
+    'locales/fi.lua',
+    'locales/fr.lua',
+    'locales/sv.lua',
+    'locales/cs.lua',
+    'locales/pl.lua',
+    'config.lua',
+    'server/main.lua'
 }
 
 client_scripts {
-	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/ru.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/cs.lua',
-	'locales/pl.lua',
-	'config.lua',
-	'client/main.lua'
+    '@es_extended/locale.lua',
+    'locales/en.lua',
+    'locales/es.lua',
+    'locales/ru.lua',
+    'locales/fi.lua',
+    'locales/fr.lua',
+    'locales/sv.lua',
+    'locales/cs.lua',
+    'locales/pl.lua',
+    'config.lua',
+    'client/main.lua'
 }
 
 dependencies {
-	'es_extended',
-	'esx_skin',
-	'esx_datastore'
+    'es_extended',
+    'esx_skin',
+    'esx_datastore'
 }
