63ffc284011a91ea995959eaede96f8badbde010
userMacieG <palatim3@gmail.com>
2020-07-19 12:54:09 +0200

    Update fxmanifest.lua (#19)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,6 +11,7 @@ server_scripts {
 	'@es_extended/locale.lua',
 	'locales/en.lua',
 	'locales/sv.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -20,6 +21,7 @@ client_scripts {
 	'locales/en.lua',
 	'locales/fr.lua',
 	'locales/sv.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'client/main.lua',
 	'client/marker.lua'
