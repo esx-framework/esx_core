ba14cf2832a326ccf04be651fa060d4507595747
Jougito - Kheros <jougito@hotmail.es>
2020-07-19 13:02:57 +0200

    Spanish translation implemented (#9)
    * Spanish translation implemented
    
    * Create es_esx_optionalneeds.sql

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,15 +7,16 @@ description 'ESX Optional Needs'
 version '1.0.0'
 
 server_scripts {
-	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/pl.lua',
-	'config.lua',
-	'server/main.lua'
+    '@es_extended/locale.lua',
+    'locales/en.lua',
+    'locales/es.lua',
+    'locales/fi.lua',
+    'locales/fr.lua',
+    'locales/pl.lua',
+    'config.lua',
+    'server/main.lua'
 }
 
 client_scripts {
-	'client/main.lua'
+    'client/main.lua'
 }
