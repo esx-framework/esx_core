4bd2b1b5ca7f971472e0f78f4437c9b64dd633d5
Jougito - Kheros <jougito@hotmail.es>
2020-07-19 12:54:38 +0200

    Spanish translation implemented (#39)
    * Create es_esx_basicneeds.sql
    
    * Spanish translation implemented

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,32 +7,34 @@ description 'ESX Basic Needs'
 version '1.0.1'
 
 server_scripts {
-	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'config.lua',
-	'server/main.lua'
+    '@es_extended/locale.lua',
+    'locales/de.lua',
+    'locales/br.lua',
+    'locales/en.lua',
+    'locales/es.lua',
+    'locales/fi.lua',
+    'locales/fr.lua',
+    'locales/sv.lua',
+    'locales/pl.lua',
+    'config.lua',
+    'server/main.lua'
 }
 
 client_scripts {
-	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'config.lua',
-	'client/main.lua'
+    '@es_extended/locale.lua',
+    'locales/de.lua',
+    'locales/br.lua',
+    'locales/en.lua',
+    'locales/es.lua',
+    'locales/fi.lua',
+    'locales/fr.lua',
+    'locales/sv.lua',
+    'locales/pl.lua',
+    'config.lua',
+    'client/main.lua'
 }
 
 dependencies {
-	'es_extended',
-	'esx_status'
+    'es_extended',
+    'esx_status'
 }
