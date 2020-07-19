b0b7a8157f04107ac38b2b6005c37e73efc8e1f3
Jougito - Kheros <jougito@hotmail.es>
2020-07-19 13:00:48 +0200

    Spanish translation implemented (#90)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,36 +7,38 @@ description 'ESX Society'
 version '1.0.4'
 
 server_scripts {
-	'@mysql-async/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/nl.lua',
-	'locales/cs.lua',
-	'config.lua',
-	'server/main.lua'
+    '@mysql-async/lib/MySQL.lua',
+    '@es_extended/locale.lua',
+    'locales/br.lua',
+    'locales/en.lua',
+    'locales/es.lua',
+    'locales/fi.lua',
+    'locales/fr.lua',
+    'locales/sv.lua',
+    'locales/pl.lua',
+    'locales/nl.lua',
+    'locales/cs.lua',
+    'config.lua',
+    'server/main.lua'
 }
 
 client_scripts {
-	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/nl.lua',
-	'locales/cs.lua',
-	'config.lua',
-	'client/main.lua'
+    '@es_extended/locale.lua',
+    'locales/br.lua',
+    'locales/en.lua',
+    'locales/es.lua',
+    'locales/fi.lua',
+    'locales/fr.lua',
+    'locales/sv.lua',
+    'locales/pl.lua',
+    'locales/nl.lua',
+    'locales/cs.lua',
+    'config.lua',
+    'client/main.lua'
 }
 
 dependencies {
-	'es_extended',
-	'cron',
-	'esx_addonaccount'
+    'es_extended',
+    'cron',
+    'esx_addonaccount'
 }
