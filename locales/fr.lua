8066b87e212521bb2440855d0bd5d54740260244
Kevin O'Byrne <62954271+TehZombiJesus@users.noreply.github.com>
2020-07-20 10:16:35 -0400

    Update fr.lua (#50)

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,5 +1,5 @@
 Locales['fr'] = {
-  ['skin_menu'] = 'Skin Menu',
+  ['skin_menu'] = 'menu de Skin',
   ['use_rotate_view'] = 'utilisez ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ et ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ pour tourner la vue.',
   ['skin'] = 'changer de skin',
   ['saveskin'] = 'sauvegarder skin dans un fichier',
