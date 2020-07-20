e45385eaee3025bc3188fc1c4828650ad0f64e52
userMacieG <palatim3@gmail.com>
2020-07-20 16:22:24 +0200

    Add `pl.lua` into `fxmanifest.lua` (#14)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -15,6 +15,7 @@ server_scripts {
 	'locales/fr.lua',
 	'locales/es.lua',
 	'locales/cs.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -28,6 +29,7 @@ client_scripts {
 	'locales/fr.lua',
 	'locales/es.lua',
 	'locales/cs.lua',
+	'locales/pl.lua',
 	'config.lua',
 	'client/main.lua'
 }
