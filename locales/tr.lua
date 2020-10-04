46fc1924a0e6e2ac2624710c2e846513a456f2f1
Ege Türker <66169371+EgeTurker@users.noreply.github.com>
2020-10-05 02:08:44 +0300

    Added Turkish Language Support to esx_skin (#84)

---
+++ locales/tr.lua
@@ -0,0 +1,6 @@
+Locales['tr'] = {
+  ['skin_menu'] = 'Karakter Menüsü',
+  ['use_rotate_view'] = '~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ ve ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ ile kamera açısını değiştirebilirsiniz.',
+  ['skin'] = 'Karakter Değiştir',
+  ['saveskin'] = 'Karakteri bir dosyaya kaydet',
+}
