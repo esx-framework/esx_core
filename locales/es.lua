cc0d5cd0ceaecbb736bf4c1542b1c9d6c3360130
SEOAlexRamon <37023161+seoalexramon@users.noreply.github.com>
2020-10-29 18:44:50 +0100

    fixed: es.lua

--- locales/es.lua
+++ locales/es.lua
@@ -1,13 +1,13 @@
 Locales ['es'] = {
-  ['buy_license'] = 'buy weapon license?',
+  ['buy_license'] = '¿comprar la licencia de armas?',
   ['yes'] = '%s',
   ['no'] = 'no',
-  ['weapon_bought'] = 'purchased for %s EUR',
+  ['weapon_bought'] = 'comprada por $%s',
   ['not_enough_black'] = 'usted no tiene ~r~suficiente~s~ dinero negro',
   ['not_enough'] = 'usted no tiene ~r~suficiente~s~ dinero',
-  ['already_owned'] = 'you already own this weapon!',
+  ['already_owned'] = '¡Ya dispones de esta arma!',
   ['shop_menu_title'] = 'tienda',
   ['shop_menu_prompt'] = 'presione ~INPUT_CONTEXT~ para acceder a la tienda.',
-  ['shop_menu_item'] = '%s EUR',
-  ['map_blip'] = 'arsenal',
+  ['shop_menu_item'] = '$%s',
+  ['map_blip'] = 'ammu-Nation',
 }
