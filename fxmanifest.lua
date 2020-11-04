7dc79c331995f44736136c017d109b3fc0019918
Ruqen <73693641+Ruqen@users.noreply.github.com>
2020-11-04 23:52:28 +0300

    Fix of manifest
    It fixes the mistake on locales made by user EgeTurker.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -17,6 +17,7 @@ server_scripts {
 	'locales/sv.lua',
 	'locales/pl.lua',
 	'locales/es.lua',
+	'locales/tr.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -31,6 +32,7 @@ client_scripts {
 	'locales/sv.lua',
 	'locales/pl.lua',
 	'locales/es.lua',
+	'locales/tr.lua',
 	'config.lua',
 	'client/main.lua'
 }
