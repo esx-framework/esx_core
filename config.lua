d336bf252735af547ec3f13d94b4781c299bae6c
HypeLevels <61633775+HypeLevels@users.noreply.github.com>
2020-11-28 22:30:18 +0000

    Update config.lua
    Changed the default language to be English as it is the universal language.

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.MarkerSize   = vector3(1.5, 1.5, 1.0)
 Config.MarkerColor  = {r = 102, g = 102, b = 204}
 Config.MarkerType   = 1
 
-Config.Locale = 'fr'
+Config.Locale = 'en'
 
 Config.Shops = {
 	vector3(-814.3, -183.8, 36.6),
