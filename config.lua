e208c3ac887286326d5ee48b13269e5229d5feda
tabarra <tabarra@vexguard.net>
2020-12-01 12:56:05 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config              = {}
 Config.DrawDistance = 100.0
-Config.Locale       = 'fr'
+Config.Locale       = 'en'
 Config.Jobs         = {}
 
 Config.MaxCaution = 10000 -- the max caution allowed
