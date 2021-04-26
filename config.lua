997fe7fc2461afa4e36776f3179eaf20343e06ad
Mycroft <kaseyfitton@hotmail.co.uk>
2021-04-26 16:28:08 +0100

    Add Comments

--- config.lua
+++ config.lua
@@ -13,5 +13,5 @@ Config.EnableSocietyPayouts = false -- pay from the society account that the pla
 Config.EnableHud            = true -- enable the default hud? Display current job and accounts (black, bank & cash)
 Config.MaxWeight            = 24   -- the max inventory weight without backpack
 Config.PaycheckInterval     = 7 * 60000 -- how often to recieve pay checks in milliseconds
-Config.EnableDebug          = false
-Config.EnableDefaultInventory = true
+Config.EnableDebug          = false -- Use Debug options?
+Config.EnableDefaultInventory = true -- Display the default Inventory ( F2 )
