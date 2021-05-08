6232541cfb0bc8fc0f6632c6bdec5524ecceb709
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-09 04:53:57 +1000

    Use PrimaryIdentifier

--- server/main.lua
+++ server/main.lua
@@ -14,8 +14,8 @@ function onPlayerJoined(playerId)
 	local identifier
 
 	for k,v in ipairs(GetPlayerIdentifiers(playerId)) do
-		if string.match(v, 'license:') then
-			identifier = string.sub(v, 9)
+		if string.match(v, Config.PrimaryIdentifier) then
+			identifier = string.gsub(v, Config.PrimaryIdentifier..':', '')
 			break
 		end
 	end
@@ -65,8 +65,8 @@ AddEventHandler('playerConnecting', function(name, setCallback, deferrals)
 	Citizen.Wait(100)
 
 		for k,v in ipairs(GetPlayerIdentifiers(playerId)) do
-			if string.match(v, 'license:') then
-				identifier = string.sub(v, 9)
+			if string.match(v, Config.PrimaryIdentifier) then
+			identifier = string.gsub(v, Config.PrimaryIdentifier..':', '')
 				break
 			end
 		end
