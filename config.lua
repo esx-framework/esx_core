eff2c7cbd9661a26047a28469f013ba8e6b4dda9
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-09 14:43:13 +1000

    Remove config for PrimaryIdentifier

--- config.lua
+++ config.lua
@@ -8,7 +8,6 @@ Config.Accounts = {
 }
 
 Config.StartingAccountMoney 	= {bank = 50000}
-Config.PrimaryIdentifier		= 'license'	-- recommended
 
 Config.EnableSocietyPayouts 	= false -- pay from the society account that the player is employed at? Requirement: esx_society
 Config.EnableHud            	= true -- enable the default hud? Display current job and accounts (black, bank & cash)
