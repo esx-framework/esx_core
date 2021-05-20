008a78198572bad00785e0f01c61d829cc98055c
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-21 00:21:03 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,14 @@
 fx_version 'adamant'
-
 game 'gta5'
+description 'https://github.com/thelindat/esx_multicharacter'
+version '1.0.0'
+
+dependency 'es_extended'
+
+shared_script 'config.lua'
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
-    'config.lua',
     'server/main.lua',
 }
 
@@ -12,6 +16,7 @@ client_scripts {
     'client/main.lua',
 }
 
+--[[	Disabled for now
 ui_page {
     'html/ui.html',
 }
@@ -23,6 +28,4 @@ files {
     'html/locales/fr.js',
     'html/locales/en.js',
     'html/locales/pl.js',
-}
-
-dependency 'es_extended'
+}]]
