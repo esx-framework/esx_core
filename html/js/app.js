a6e78684075cc5e310d2270a017be91e70a92d4f
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-21 02:34:00 +1000

    fix(nui/js): Format money and bank with grouping and currency type

--- html/js/app.js
+++ html/js/app.js
@@ -1,4 +1,8 @@
-
+var money = Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'USD',
+	minimumFractionDigits: 0
+});
 
 (() => {
     Kashacter = {};
@@ -6,7 +10,7 @@
     Kashacter.ShowUI = function(data) {
         $('body').css({"display":"block"});
         $('.main-container').css({"display":"block"});
-		$('[data-charid=1]').html('<div class="character-info"><p class="character-info-name"><h1>' + `${translate.name} ` + '</h1><span>' + data.firstname +' '+ data.lastname +'</span></p><p class="character-info-work"><h1>' + `${translate.job} ` + '</h1><span>'+ data.job +' '+ data.job_grade +'</span></p><p class="character-info-money"><h1>' + `${translate.money} ` + '</h1><span> $'+ data.money +'</span></p><p class="character-info-bank"><h1>' + `${translate.bank} ` + '</h1><span> $'+ data.bank +'</span></p> <p class="character-info-dateofbirth"><h1>' + `${translate.dob} ` + '</h1><span>'+ data.dateofbirth +'</span></p> <p class="character-info-gender"><h1>' + `${translate.gender} ` + '</h1><span>'+ data.sex +'</span></p></div>').attr("data-ischar", "true");
+		$('[data-charid=1]').html('<div class="character-info"><p class="character-info-name"><h1>' + `${translate.name} ` + '</h1><span>' + data.firstname +' '+ data.lastname +'</span></p><p class="character-info-work"><h1>' + `${translate.job} ` + '</h1><span>'+ data.job +' '+ data.job_grade +'</span></p><p class="character-info-money"><h1>' + `${translate.money} ` + '</h1><span> '+ money.format(data.money) +'</span></p><p class="character-info-bank"><h1>' + `${translate.bank} ` + '</h1><span> '+ money.format(data.bank) +'</span></p> <p class="character-info-dateofbirth"><h1>' + `${translate.dob} ` + '</h1><span>'+ data.dateofbirth +'</span></p> <p class="character-info-gender"><h1>' + `${translate.gender} ` + '</h1><span>'+ data.sex +'</span></p></div>').attr("data-ischar", "true");
     };
 
     Kashacter.CloseUI = function() {
