01d28a4153fc7f70db2607a376b1b0c141282581
X00LA <X00LA@users.noreply.github.com>
2021-06-05 09:51:18 +0200

    Added locale de
    Added language file for german (Deutsch).

---
+++ locales/de.lua
@@ -0,0 +1,9 @@
+Locales['de'] = {
+	['delete_label'] = "Lösche %s %s?",
+	['select_char'] = "Charakter auswählen",
+	['create_char'] = "Neuen Charakter erstellen",
+	['char_play'] = "Diesen Charakter spielen",
+	['char_delete'] = "Diesen Charakter löschen",
+	['cancel'] = "Abbrechen",
+	['confirm'] = "Bestätigen",
+}
