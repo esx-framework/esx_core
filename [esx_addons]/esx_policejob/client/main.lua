c5611f47b49028171ef65870c40b4607bead5415
Linden <heliowaltonau@gmail.com>
2021-06-30 01:57:37 +1000

    fix(ambulance+police): Put more than 1 person into a vehicle

--- [esx_addons]/esx_policejob/client/main.lua
+++ [esx_addons]/esx_policejob/client/main.lua
@@ -1114,25 +1114,21 @@ RegisterNetEvent('esx_policejob:putInVehicle')
 AddEventHandler('esx_policejob:putInVehicle', function()
 	if isHandcuffed then
 		local playerPed = PlayerPedId()
-		local coords = GetEntityCoords(playerPed)
+		local vehicle, distance = ESX.Game.GetClosestVehicle()
 
-		if IsAnyVehicleNearPoint(coords, 5.0) then
-			local vehicle = GetClosestVehicle(coords, 5.0, 0, 71)
+		if vehicle and distance < 5 then
+			local maxSeats, freeSeat = GetVehicleMaxNumberOfPassengers(vehicle)
 
-			if DoesEntityExist(vehicle) then
-				local maxSeats, freeSeat = GetVehicleMaxNumberOfPassengers(vehicle)
-
-				for i=maxSeats - 1, 0, -1 do
-					if IsVehicleSeatFree(vehicle, i) then
-						freeSeat = i
-						break
-					end
+			for i=maxSeats - 1, 0, -1 do
+				if IsVehicleSeatFree(vehicle, i) then
+					freeSeat = i
+					break
 				end
+			end
 
-				if freeSeat then
-					TaskWarpPedIntoVehicle(playerPed, vehicle, freeSeat)
-					dragStatus.isDragged = false
-				end
+			if freeSeat then
+				TaskWarpPedIntoVehicle(playerPed, vehicle, freeSeat)
+				dragStatus.isDragged = false
 			end
 		end
 	end
