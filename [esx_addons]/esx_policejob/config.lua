23a484db511599c0e5ca15e02c5bd35428d5ceb7
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-07-05 08:46:17 +0100

    Added config for optionalneeds

--- [esx_addons]/esx_policejob/config.lua
+++ [esx_addons]/esx_policejob/config.lua
@@ -8,6 +8,7 @@ Config.MarkerColor                = {r = 50, g = 50, b = 204}
 Config.EnablePlayerManagement     = true -- Enable if you want society managing.
 Config.EnableArmoryManagement     = false
 Config.EnableESXIdentity          = true -- Enable if you're using esx_identity.
+Config.EnableESXOptionalneeds     = false -- Enable if you're using esx_optionalneeds
 Config.EnableLicenses             = false -- Enable if you're using esx_license.
 
 Config.EnableHandcuffTimer        = true -- Enable handcuff timer? will unrestrain player after the time ends.
