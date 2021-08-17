79bc96a62346bd7cb5064853645ec6216ffe9443
Konrad <45127358+Konraaaad@users.noreply.github.com>
2021-08-17 18:55:34 +0200

    update esx billing de.lua

--- [esx_addons]/esx_billing/locales/de.lua
+++ [esx_addons]/esx_billing/locales/de.lua
@@ -1,12 +1,12 @@
 Locales['de'] = {
-  ['invoices'] = 'rechnungen',
+  ['invoices'] = 'Rechnungen',
   ['invoices_item'] = '$%s',
-  ['received_invoice'] = 'you hast eine Rechnung ~r~erhlaten~s~',
+  ['received_invoice'] = 'du hast eine Rechnung ~r~erhalten~s~',
   ['paid_invoice'] = 'du ~g~bezahlst~s~ eine Rechnung von ~r~$%s~s~',
-  ['no_invoices'] = 'you do not have any bills to pay at this moment',
+  ['no_invoices'] = 'du hast keine Offenen Rechnungen',
   ['received_payment'] = 'du ~g~erhältst~s~ eine Zahlung von ~r~$%s~s~',
   ['player_not_online'] = 'der Spieler ist nicht online',
-  ['no_money'] = 'you do not have enough money to pay this bill',
-  ['target_no_money'] = 'the player ~r~does not~s~ have enough money to pay the bill!',
-  ['keymap_showbills'] = 'open bills menu',
+  ['no_money'] = 'du hast nicht genug Geld',
+  ['target_no_money'] = 'der Spieler ~r~nicht genug~s~ Geld um diese Rechnung zu bezahlen!',
+  ['keymap_showbills'] = 'öffnet das Rechnungs Menu',
 }
