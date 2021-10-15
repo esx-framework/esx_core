a1cb26da5d96fe8a132a890bbca9dd7c75e9e36d
Linden <65407488+thelindat@users.noreply.github.com>
2021-10-16 06:37:34 +1100

    feat(sql): Setup proper keys and collation
    - These fields should only be referenced, so index them for faster queries

--- esx_multicharacter.sql
+++ esx_multicharacter.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `multicharacter_slots` (
-	`identifier` VARCHAR(60) NOT NULL,
-	`slots` INT NOT NULL
-);
-
-ALTER TABLE `users` 
-	ADD `disabled` BIT NOT NULL DEFAULT 0;
+	`identifier` VARCHAR(60) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`slots` INT(11) NOT NULL,
+	PRIMARY KEY (`identifier`) USING BTREE,
+	INDEX `slots` (`slots`) USING BTREE
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB
+;
