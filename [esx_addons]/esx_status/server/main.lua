d9e95ced5a4f7ef3ad1137e9d04784666f609d96
Linden <65407488+thelindat@users.noreply.github.com>
2021-10-24 02:31:26 +1100

    feat(server): Convert to oxmysql and replace bulk statement with prepared statement

--- [esx_addons]/esx_status/server/main.lua
+++ [esx_addons]/esx_status/server/main.lua
@@ -2,21 +2,17 @@ AddEventHandler('onResourceStart', function(resourceName)
 	if (GetCurrentResourceName() ~= resourceName) then
 	  	return
 	end
-	
+
 	for _, xPlayer in pairs(ESX.Players) do
 		local status = xPlayer.get('status')
 		if status then
 			ESX.Players[xPlayer.source] = status
 		else
-			MySQL.Async.fetchAll('SELECT status FROM users WHERE identifier = @identifier', {
-				['@identifier'] = xPlayer.identifier
+			exports.oxmysql:scalar('SELECT status FROM users WHERE identifier = ?', {
+				xPlayer.identifier
 			}, function(result)
-				local data = {}
-		
-				if result[1].status then
-					data = json.decode(result[1].status)
-				end
-			
+				local data = result and json.decode(result) or {}
+
 				xPlayer.set('status', data)	-- save to xPlayer for compatibility
 				ESX.Players[xPlayer.source] = data -- save locally for performance
 			end)
@@ -26,14 +22,10 @@ AddEventHandler('onResourceStart', function(resourceName)
 end)
 
 AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
-	MySQL.Async.fetchAll('SELECT status FROM users WHERE identifier = @identifier', {
-		['@identifier'] = xPlayer.identifier
+	exports.oxmysql:scalar('SELECT status FROM users WHERE identifier = ?', {
+		xPlayer.identifier
 	}, function(result)
-		local data = {}
-
-		if result[1].status then
-			data = json.decode(result[1].status)
-		end
+		local data = result and json.decode(result) or {}
 
 		xPlayer.set('status', data)
 		ESX.Players[xPlayer.source] = data
@@ -45,16 +37,16 @@ AddEventHandler('esx:playerDropped', function(playerId, reason)
 	local xPlayer = ESX.GetPlayerFromId(playerId)
 	local status = ESX.Players[xPlayer.source]
 
-	MySQL.Async.execute('UPDATE users SET status = @status WHERE identifier = @identifier', {
-		['@status']     = json.encode(status),
-		['@identifier'] = xPlayer.identifier
+	exports.oxmysql:update('UPDATE users SET status = ? WHERE identifier = ?', {
+		json.encode(status), xPlayer.identifier
 	}, function(result)
 		ESX.Players[xPlayer.source] = nil
 	end)
 end)
 
 AddEventHandler('esx_status:getStatus', function(playerId, statusName, cb)
-	for i=1, #ESX.Players, 1 do
+	local status = ESX.Players[playerId]
+	for i=1, #status, 1 do
 		if status[i].name == statusName then
 			cb(status[i])
 			break
@@ -66,56 +58,23 @@ RegisterServerEvent('esx_status:update')
 AddEventHandler('esx_status:update', function(status)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	if xPlayer then
-		xPlayer.set('status', status)	-- save to xPlayer for compatibility
-		ESX.Players[xPlayer.source] = status	-- save locally for performance
+		xPlayer.set('status', status) -- save to xPlayer for compatibility
+		ESX.Players[xPlayer.source] = status -- save locally for performance
 	end
 end)
 
 Citizen.CreateThread(function()
-	while(true) do
+	while true do
 		Citizen.Wait(10 * 60 * 1000)
-		SaveData()
-	end
-end)
-
-function SaveData()
-	-- Example of a bulk update statement that we are building below
-	--[[
-	UPDATE users
-    SET status = (case when identifier = 'license:123' then '{hunger:45, thirst:23}'
-                         when identifier = 'license:456' then '{hunger:1000, thirst:1000}'
-                         when identifier = 'license:789' then '{hunger:1000, thirst:1000}'
-                    end)
-    WHERE identifier in ('license:123', 'license:456', 'license:789')
-
-	]]
-	local updateStatement = 'UPDATE users SET status = (case %s end) where identifier in (%s)'
-	local whenList = ''
-	local whereList = ''
-	local firstItem = true
-	local playerCount = 0
-
-	local xPlayers = ESX.GetExtendedPlayers()
-	
-	for _, xPlayer in pairs(xPlayers) do
-		local status = ESX.Players[xPlayer.source]
-		if status then
-			whenList = whenList .. string.format('when identifier = \'%s\' then \'%s\' ', xPlayer.identifier, json.encode(status))
-
-			if firstItem == false then
-				whereList = whereList .. ', '
+		local parameters = {}
+		for _, xPlayer in pairs(ESX.GetExtendedPlayers()) do
+			local status = ESX.Players[xPlayer.source]
+			if status and next(status) then
+				parameters[#parameters+1] = {json.encode(status), xPlayer.identifier}
 			end
-			whereList = whereList .. string.format('\'%s\'', xPlayer.identifier)
-
-			firstItem = false
-			playerCount = playerCount + 1
+		end
+		if #parameters > 0 then
+			exports.oxmysql:prepare('UPDATE users SET status = ? WHERE identifier = ?', parameters)
 		end
 	end
-
-	if playerCount > 0 then
-		local sql = string.format(updateStatement, whenList, whereList)
-		MySQL.Async.execute(sql)
-
-	end
-
-end
+end)
