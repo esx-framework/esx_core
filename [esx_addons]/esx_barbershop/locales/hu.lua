28244317c180735a080185e04500d0943ffd9ed8
Marci <mihmar17@gmail.com>
2022-01-21 18:18:28 +0100

    Added some Hungarian translation

---
+++ [esx_addons]/esx_barbershop/locales/hu.lua
@@ -0,0 +1,9 @@
+Locales['hu'] = {
+  ['valid_purchase'] = 'Biztosan kifizeted?',
+  ['yes'] = 'Igen',
+  ['no'] = 'Nem',
+  ['not_enough_money'] = 'Nincsen elég pénzed',
+  ['press_access'] = 'Nyomj ~INPUT_CONTEXT~ hogy megnyisd a ~y~Fodrászatot~s~.',
+  ['barber_blip'] = 'Fodrászat',
+  ['you_paid'] = 'Fizettél ~g~$%s~s~',
+}
---
+++ [esx_addons]/esx_basicneeds/locales/hu.lua
@@ -0,0 +1,4 @@
+Locales['hu'] = {
+  ['used_bread'] = 'Megettél egy ~y~1x~s~ ~b~kenyeret~s~.',
+  ['used_water'] = 'Megittál egy ~y~1x~s~ ~b~vizet~s~.',
+}
