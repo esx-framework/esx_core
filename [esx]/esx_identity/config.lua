3fc14ec91ac32bcdd1fce747350e3ea9e48e2d20
PyschoShock <94499608+PsychoShock@users.noreply.github.com>
2022-02-06 19:37:31 -0500

    feat(year): Passing to 2022

--- [esx]/esx_identity/config.lua
+++ [esx]/esx_identity/config.lua
@@ -1,19 +1,15 @@
 Config                  = {}
 Config.Locale           = 'en'
 
--- [Config.EnableCommands]
--- Enables Commands Such As /char and /chardel
-Config.EnableCommands   = false
-
-Config.UseDeferrals     = false -- EXPERIMENTAL Character Registration Method.
-
+Config.EnableCommands   = false -- Enables Commands Such As /char and /chardel
 
 -- These values are for the second input validation in server/main.lua
 Config.MaxNameLength    = 20
 Config.MinHeight        = 45
 Config.MaxHeight        = 96
 Config.LowestYear       = 1900
-Config.HighestYear      = 2021
+Config.HighestYear      = 2022
 
 Config.FullCharDelete   = false
 Config.EnableDebugging  = false
+Config.UseDeferrals     = false -- EXPERIMENTAL Character Registration Method.
