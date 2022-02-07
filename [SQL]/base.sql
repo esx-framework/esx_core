2707a3b61bba61248bb3e32306b0dd27caa05f15
Mycroft <kaseyfitton@hotmail.co.uk>
2022-02-07 00:00:54 +0000

    fix:Add default value for unemployed job

--- [SQL]/base.sql
+++ [SQL]/base.sql
@@ -53,4 +53,4 @@ CREATE TABLE `jobs` (
 	PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `jobs` VALUES ('unemployed','Unemployed');
+INSERT INTO `jobs` VALUES ('unemployed','Unemployed',0);
