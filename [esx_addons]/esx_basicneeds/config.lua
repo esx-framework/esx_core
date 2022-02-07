6568530f17ad7355f232b0d718e3f0018dc13645
PyschoShock <94499608+PsychoShock@users.noreply.github.com>
2022-02-07 15:23:00 -0500

    feat(basicneeds): new config method
    Player can now add a simple config.

--- [esx_addons]/esx_basicneeds/config.lua
+++ [esx_addons]/esx_basicneeds/config.lua
@@ -1,3 +1,8 @@
 Config = {}
 Config.Locale = 'en'
 Config.Visible = true
+
+Config.food = {
+     ["bread"] = {type = "hunger", value= 87714, action = "onEat", notif = _U("used_bread")},    -- Food
+     ["water"] = {type = "thirst", value = 69478, action = "onDrink", notif = _U("used_water")}, -- Drink
+}
