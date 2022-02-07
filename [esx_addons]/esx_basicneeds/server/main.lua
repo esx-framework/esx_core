9e879fdc799ee8d0d1946fddf2ea8dd765fb2939
PyschoShock <94499608+PsychoShock@users.noreply.github.com>
2022-02-07 15:26:41 -0500

    feat(config): new config method

--- [esx_addons]/esx_basicneeds/server/main.lua
+++ [esx_addons]/esx_basicneeds/server/main.lua
@@ -1,24 +1,27 @@
-ESX.RegisterUsableItem('bread', function(source)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem('bread', 1)
-
-	TriggerClientEvent('esx_status:add', source, 'hunger', 200000)
-	TriggerClientEvent('esx_basicneeds:onEat', source)
-	xPlayer.showNotification(_U('used_bread'))
-end)
-
-ESX.RegisterUsableItem('water', function(source)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem('water', 1)
-
-	TriggerClientEvent('esx_status:add', source, 'thirst', 200000)
-	TriggerClientEvent('esx_basicneeds:onDrink', source)
-	xPlayer.showNotification(_U('used_water'))
-end)
-
 ESX.RegisterCommand('heal', 'admin', function(xPlayer, args, showError)
 	args.playerId.triggerEvent('esx_basicneeds:healPlayer')
 	args.playerId.triggerEvent('chat:addMessage', {args = {'^5HEAL', 'You have been healed.'}})
 end, true, {help = 'Heal a player, or yourself - restores thirst, hunger and health.', validate = true, arguments = {
 	{name = 'playerId', help = 'the player id', type = 'player'}
 }})
+
+for i,v in pairs(Config.food) do
+	ESX.RegisterUsableItem(i, function(source)
+		local xPlayer = ESX.GetPlayerFromId(source)
+		xPlayer.removeInventoryItem(i, 1)
+		local action = "esx_basicneeds:"..v.action
+		if v.type == "both" then
+			TriggerClientEvent('esx_status:add', source, 'thirst', v.valueT)
+			TriggerClientEvent('esx_status:add', source, 'hunger', v.valueH)
+		elseif v.type == "drunk" then
+			TriggerClientEvent('esx_status:add', source, 'drunk', v.value)
+			TriggerClientEvent('esx_status:add', source, 'thirst', 50000)
+		else
+			TriggerClientEvent('esx_status:add', source, v.type, v.value)
+		end
+
+		TriggerClientEvent(action, source)
+        xPlayer.showNotification(v.notif)
+
+	end)
+end
