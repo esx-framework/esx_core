23fd0289c6955e3c3b51577051388241febc8b4a
Linden <65407488+thelindat@users.noreply.github.com>
2022-02-14 13:07:58 +1100

    fix(sql): remove hard-set table charset and collation
    Remove some instances of collation mixing.
    
    includes:
    fix(sql): don't allow NULL identifier in billing, user_licenses, and user_parkings

--- [esx]/es_extended/es_extended.sql
+++ [esx]/es_extended/es_extended.sql
@@ -1,6 +1,12 @@
 CREATE DATABASE IF NOT EXISTS `es_extended`;
 USE `es_extended`;
 
+ALTER DATABASE `es_extended`
+	DEFAULT CHARACTER SET UTF8MB4;
+	
+ALTER DATABASE `es_extended`
+	DEFAULT COLLATE UTF8MB4_UNICODE_CI;
+
 CREATE TABLE `users` (
 	`identifier` VARCHAR(60) NOT NULL,
 	`accounts` LONGTEXT NULL DEFAULT NULL,
@@ -12,7 +18,7 @@ CREATE TABLE `users` (
 	`position` VARCHAR(255) NULL DEFAULT '{"x":-269.4,"y":-955.3,"z":31.2,"heading":205.8}',
 
 	PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB;
 
 
 CREATE TABLE `items` (
@@ -23,7 +29,7 @@ CREATE TABLE `items` (
 	`can_remove` TINYINT NOT NULL DEFAULT 1,
 
 	PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB;
 
 
 CREATE TABLE `job_grades` (
@@ -37,7 +43,7 @@ CREATE TABLE `job_grades` (
 	`skin_female` LONGTEXT NOT NULL,
 
 	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB;
 
 
 INSERT INTO `job_grades` VALUES (1,'unemployed',0,'unemployed','Unemployed',200,'{}','{}');
@@ -47,7 +53,7 @@ CREATE TABLE `jobs` (
 	`label` VARCHAR(50) DEFAULT NULL,
 
 	PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB;
 
 
 INSERT INTO `jobs` VALUES ('unemployed','Unemployed');
