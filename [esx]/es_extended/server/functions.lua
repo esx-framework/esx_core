d1025d1020f6a6d0de7ffcd52b77bc833a10d00b
sepehr-gr <80945111+sepehr-gr@users.noreply.github.com>
2022-02-18 23:56:22 +0330

    Update functions.lua

--- [esx]/es_extended/server/functions.lua
+++ [esx]/es_extended/server/functions.lua
@@ -248,7 +248,7 @@ end
 function ESX.GetIdentifier(playerId)
 	for k,v in ipairs(GetPlayerIdentifiers(playerId)) do
 		if string.match(v, 'license:') then
-			local identifier = string.gsub(v, 'license:', '')
+			local identifier = v
 			return identifier
 		end
 	end
