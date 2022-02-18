4c24bf23c64906a8957404edf933ea9974b259cb
narathipgamecaster@gmail.com <61742272+NRTnarathip@users.noreply.github.com>
2022-02-18 18:39:27 +0700

    Update esx_dmvschool.sql

--- [esx_addons]/esx_dmvschool/esx_dmvschool.sql
+++ [esx_addons]/esx_dmvschool/esx_dmvschool.sql
@@ -1,8 +1,8 @@
 USE `es_extended`;
 
 INSERT INTO `licenses` (`type`, `label`) VALUES
-	('dmv', 'Code de la route'),
-	('drive', 'Permis de conduire'),
-	('drive_bike', 'Permis moto'),
-	('drive_truck', 'Permis camion')
+	('dmv', 'Driving Permit'),
+	('drive', 'Drivers License'),
+	('drive_bike', 'Motorcycle License'),
+	('drive_truck', 'Commercial Drivers License')
 ;
