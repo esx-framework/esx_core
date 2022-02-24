1ad89463ccc79e003e7510fda554baf7f09af8fe
ffpetrozzi <51758256+ffpetrozzi@users.noreply.github.com>
2022-02-24 08:42:01 +0100

    Update config.lua

--- [esx]/es_extended/config.lua
+++ [esx]/es_extended/config.lua
@@ -20,3 +20,4 @@ Config.EnablePVP                = true -- Allow Player to player combat
 
 Config.Multichar                = false -- Enable support for esx_multicharacter
 Config.Identity                 = true -- Select a characters identity data before they have loaded in (this happens by default with multichar)
+Config.Identifier               = 'steam' -- Choose the identifier that will be used to save informations
