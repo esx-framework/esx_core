ce53e09f5ea14120d50f894801f74f0450bf3d2b
JonarSonaer <haikara98@gmail.com>
2022-02-27 16:35:03 +0100

    Update sv.lua

--- [esx]/esx_skin/locales/sv.lua
+++ [esx]/esx_skin/locales/sv.lua
@@ -1,6 +1,6 @@
 Locales['sv'] = {
-  ['skin_menu'] = 'skin meny',
-  ['use_rotate_view'] = 'använd ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ och ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ för att rotera.',
-  ['skin'] = 'ändra skin',
-  ['saveskin'] = 'spara skin till fil',
+  ['skin_menu'] = 'Skin meny',
+  ['use_rotate_view'] = 'Använd ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ och ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ för att rotera vyn.',
+  ['skin'] = 'Ändra skin',
+  ['saveskin'] = 'spara skin till en fil',
 }
