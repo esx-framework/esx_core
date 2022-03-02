6044d3f42df87e3054b1f780055e0563559a0aef
PandaSLO <90091858+PandaSLO@users.noreply.github.com>
2022-03-02 22:37:50 +0100

    Create si.lua

---
+++ [esx_addons]/esx_barbershop/locales/si.lua
@@ -0,0 +1,9 @@
+Locales['si'] = {
+   ['valid_purchase'] = 'ali potrdite ta nakup?',
+   ['da'] = 'da',
+   ['ne'] = 'ne',
+   ['not_enough_money'] = 'nimate dovolj denarja',
+   ['press_access'] = 'pritisnite ~INPUT_CONTEXT~ za dostop do ~y~Brivnice~s~.',
+   ['barber_blip'] = 'brivnica',
+   ['you_paid'] = 'plačali ste ~g~$%s~s~',
+}
