fd648a6f7ede7eb82667da87aaf413960b6ecadc
Mycroft <kaseyfitton@hotmail.co.uk>
2022-03-02 20:56:59 +0000

    Fixes: Accessories & DMV School

--- [esx_addons]/esx_accessories/config.lua
+++ [esx_addons]/esx_accessories/config.lua
@@ -9,7 +9,7 @@ Config.EnableControls = true
 Config.DrawDistance = 10
 Config.Size   = {x = 1.0, y = 1.0, z = 1.0}
 Config.Color  = {r = 102, g = 102, b = 204}
-Config.Type   = 29
+Config.Type   = 1
 
 -- Fill this if you want to see the blips,
 -- If you have esx_clothesshop you should not fill this
