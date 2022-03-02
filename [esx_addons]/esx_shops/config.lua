b37ac788f4aa89bea1910628a74124260b3e3ce0
Mycroft <kaseyfitton@hotmail.co.uk>
2022-03-02 19:53:50 +0000

    Refactor: ESX Shops - Optimisations and  Security Patches

--- [esx_addons]/esx_shops/config.lua
+++ [esx_addons]/esx_shops/config.lua
@@ -1,7 +1,7 @@
 Config = {}
-Config.DrawDistance = 100
+Config.DrawDistance = 10
 Config.MarkerSize = {x = 1.1, y = 1.1, z = 0.9}
-Config.MarkerType =  21
+Config.MarkerType =  29
 Config.MarkerColor = {r = 102, g = 102, b = 204, a = 255}
 Config.Locale = 'en'
 
--- [esx_addons]/esx_shops/server/main.lua
+++ [esx_addons]/esx_shops/server/main.lua
@@ -1,10 +1,30 @@
 local ShopItems = {}
 
+
+function GetItemFromShop(Item, Zone)
+	local item = {}
+	local found = false
+	for i=1, #Config.Zones[Zone].Items, 1 do
+		if Config.Zones[Zone].Items[i].name == Item then
+			item = Config.Zones[Zone].Items[i]
+			found = true
+			break
+		end
+	end
+
+	if found then 
+		return true, item.price, item.label
+	else
+		return false
+	end
+end
+
+
 RegisterServerEvent('esx_shops:buyItem')
 AddEventHandler('esx_shops:buyItem', function(itemName, amount, zone)
 	local source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-
+	local Exists, price,label = GetItemFromShop(itemName,zone)
 	amount = ESX.Math.Round(amount)
 
 	if amount < 0 then
@@ -12,33 +32,26 @@ AddEventHandler('esx_shops:buyItem', function(itemName, amount, zone)
 		return
 	end
 
-	-- get price
-	local price = 0
-	local itemLabel = ''
-
-	for i=1, #Config.Zones[zone].Items, 1 do
-		local item = Config.Zones[zone].Items[i]
-		if item.name == itemName then
-			price = item.price
-			itemLabel = item.label
-			break
-		end
+	if not Exists then
+		print('esx_shops: ' .. xPlayer.identifier .. ' attempted to exploit the shop!')
+		return
 	end
 
-	price = price * amount
-
+	if Exists then
+		price = price * amount
 	-- can the player afford this item?
-	if xPlayer.getMoney() >= price then
-		-- can the player carry the said amount of x item?
-		if xPlayer.canCarryItem(itemName, amount) then
-			xPlayer.removeMoney(price)
-			xPlayer.addInventoryItem(itemName, amount)
-			xPlayer.showNotification(_U('bought', amount, itemLabel, ESX.Math.GroupDigits(price)))
+		if xPlayer.getMoney() >= price then
+			-- can the player carry the said amount of x item?
+			if xPlayer.canCarryItem(itemName, amount) then
+				xPlayer.removeMoney(price)
+				xPlayer.addInventoryItem(itemName, amount)
+				xPlayer.showNotification(_U('bought', amount, itemLabel, ESX.Math.GroupDigits(price)))
+			else
+				xPlayer.showNotification(_U('player_cannot_hold'))
+			end
 		else
-			xPlayer.showNotification(_U('player_cannot_hold'))
+			local missingMoney = price - xPlayer.getMoney()
+			xPlayer.showNotification(_U('not_enough', ESX.Math.GroupDigits(missingMoney)))
 		end
-	else
-		local missingMoney = price - xPlayer.getMoney()
-		xPlayer.showNotification(_U('not_enough', ESX.Math.GroupDigits(missingMoney)))
 	end
 end)
