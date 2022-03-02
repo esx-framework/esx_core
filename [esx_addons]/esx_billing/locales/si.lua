21a0e11899cf738a809057d188f201a1e69936c5
PandaSLO <90091858+PandaSLO@users.noreply.github.com>
2022-03-02 22:40:19 +0100

    Create si.lua

---
+++ [esx_addons]/esx_billing/locales/si.lua
@@ -0,0 +1,12 @@
+Locales['si'] = {
+   ['invoices'] = 'računi',
+   ['invoices_item'] = '$%s',
+   ['received_invoice'] = 'pravkar ste ~r~prejeli~ račun',
+   ['paid_invoice'] = '~g~plačali~s~ račun ~r~$%s~s~',
+   ['no_invoices'] = 'trenutno nimate nobenih računov za plačilo',
+   ['received_payment'] = 'prejeli ste ~g~s~ plačilo ~r~$%s~s~',
+   ['player_not_online'] = 'igralec ni prijavljen',
+   ['no_money'] = 'Nimate dovolj denarja za plačilo tega računa',
+   ['target_no_money'] = 'igralec ~r~ nima~ dovolj denarja za plačilo računa!',
+   ['keymap_showbills'] = 'odpri meni računov',
+}
