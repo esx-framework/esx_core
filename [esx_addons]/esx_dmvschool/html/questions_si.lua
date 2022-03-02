47e13af9a75e9804e3b51434ba22f16e8e56c116
PandaSLO <90091858+PandaSLO@users.noreply.github.com>
2022-03-02 22:32:01 +0100

    Create questions_si.lua

---
+++ [esx_addons]/esx_dmvschool/html/questions_si.lua
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+    {
+        question: "Če vozite 80 km/h in se približujete stanovanjskemu naselju, morate:",
+        propositionA: "Pospešiš",
+        propositionB: "Ohranjaj svojo hitrost, če ne greš mimo drugih vozil",
+        propositionC: "Počasi",
+        propositionD: "Ohranite svojo hitrost",
+        answer: "C"
+    },
+
+    {
+        question: "Če na semaforju zavijete desno, vendar vidite prehod za pešce, kaj storite:",
+        propositionA: "Mimo pešca",
+        propositionB: "Preveriš, da ni drugih vozil v bližini",
+        propositionC: "Počakaj, dokler pešec ne prečka",
+        propositionD: "Ustreliš pešca in nadaljuješ z vožnjo",
+        answer: "C"
+    },
+
+    {
+        question: "Brez predhodne navedbe je hitrost v stanovanjskem območju: __ km/h",
+        propositionA: "30 km/h",
+        propositionB: "50 km/h",
+        propositionC: "40 km/h",
+        propositionD: "60 km/h",
+        answer: "B"
+    },
+
+    {
+        question: "Pred vsako spremembo voznega pasu morate:",
+        propositionA: "Preveri svoja ogledala",
+        propositionB: "Preveri svoje slepe kote",
+        propositionC: "Signite svoje namere",
+        predlogD: "Vse našteto",
+        answer: "D"
+    },
+
+    {
+        question: "Kakšna raven alkohola v krvi je razvrščena kot vožnja pod vplivom alkohola?",
+        propositionA: "0,05 %",
+        propositionB: "0,18 %",
+        predlogC: "0,08%",
+        predlogD: "0,06%",
+        answer: "C"
+    },
+
+    {
+        question: "Kdaj lahko nadaljujete z vožnjo na semaforju?",
+        propositionA: "Ko je zeleno",
+        propositionB: "Ko v križišču ni nikogar",
+        propositionC: "Ste v šolskem območju",
+        propositionD: "Ko je zelena in/ali rdeča in zavijete desno",
+        answer: "D"
+    },
+
+    {
+        question: "Pešec ima signal za ne prečkaj, kaj narediš?",
+        propositionA: "Pustiš jih mimo",
+        propositionB: "Preden nadaljuješ, opazuj",
+        propositionC: "Pomahaš, da jim rečeš, naj prečkajo",
+        propositionD: "Nadaljuj, ker tvoj semafor sveti zeleno",
+        answer: "D"
+    },
+
+    {
+        question: "Kaj je dovoljeno pri prehodu drugega vozila",
+        propositionA: "Pozorno slediš, da ga hitreje preideš",
+        propositionB: "Mimo ga, ne da bi zapustili cestišče",
+        propositionC: "Vozite po nasprotni strani ceste, da bi mimo",
+        propositionD: "Presegli ste omejitev hitrosti, da bi jih prepeljali",
+        answer: "C"
+    },
+
+    {
+        question: "Vozite po avtocesti, ki označuje največjo hitrost 120 km/h. Toda večina prometnikov vozi s 125 km/h, zato ne smete voziti hitreje od:",
+        propositionA: "120 km/h",
+        propositionB: "125 km/h",
+        propositionC: "130 km/h",
+        propositionD: "110 km/h",
+        answer: "A"
+    },
+
+    {
+        question: "Ko vas prehiteva drugo vozilo, je pomembno, da NE:",
+        propositionA: "Upočasni",
+        propositionB: "Preveri svoja ogledala",
+        propositionC: "Pazi na druge voznike",
+        propositionD: "Povečaj svojo hitrost",
+        answer: "D"
+    },
+]
