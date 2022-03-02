9490d4a34afdfdb61fe8a85a7ba18902d84f79c7
PandaSLO <90091858+PandaSLO@users.noreply.github.com>
2022-03-02 22:43:14 +0100

    Create si.lua

---
+++ [esx_addons]/esx_clotheshop/locales/si.lua
@@ -0,0 +1,12 @@
+Locales['si'] = {
+   ['valid_this_purchase'] = 'ali potrdite ta nakup?',
+   ['yes'] = 'da',
+   ['no'] = 'ne',
+   ['not_enough_money'] = 'nimate dovolj denarja',
+   ['press_menu'] = 'pritisnite ~INPUT_CONTEXT~ za dostop do ~y~Clothing Shop~s~.',
+   ['clothes'] = 'trgovina z oblačili',
+   ['you_paid'] = 'plačali ste ~g~$%s~s~',
+   ['save_in_dressing'] = 'ali želite shraniti obleko v svoji lastnini?',
+   ['name_outfit'] = 'imenuj svojo obleko',
+   ['saved_outfit'] = 'obleka je bila shranjena!',
+}
