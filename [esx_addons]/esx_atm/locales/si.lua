7436201f79a6969c8110c3187bdb5a45e5a2f5e7
PandaSLO <90091858+PandaSLO@users.noreply.github.com>
2022-03-02 22:36:15 +0100

    Create si.lua

---
+++ [esx_addons]/esx_atm/locales/si.lua
@@ -0,0 +1,7 @@
+Locales['si'] = {
+   ['invalid_amount'] = '~r~To je neveljaven znesek~s~',
+   ['deposit_money'] = 'položili ste ~g~$%s~s~',
+   ['withdraw_money'] = 'dvignili ste ~g~$%s~s~',
+   ['press_e_atm'] = 'pritisnite ~INPUT_PICKUP~ za polog ali dvig iz ~g~ATM~s~',
+   ['atm_blip'] = 'bankomat',
+}
