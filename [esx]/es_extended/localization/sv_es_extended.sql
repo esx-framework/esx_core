04d488c15a373a084add0d69c97771d06d30de48
JonarSonaer <haikara98@gmail.com>
2022-03-02 23:34:15 +0100

    Add files via upload

---
+++ [esx]/es_extended/localization/sv_es_extended.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE IF NOT EXISTS `es_extended`;
+USE `es_extended`;
+
+ALTER DATABASE `es_extended`
+	DEFAULT CHARACTER SET UTF8MB4;
+	
+ALTER DATABASE `es_extended`
+	DEFAULT COLLATE UTF8MB4_UNICODE_CI;
+
+CREATE TABLE `users` (
+	`identifier` VARCHAR(60) NOT NULL,
+	`accounts` LONGTEXT NULL DEFAULT NULL,
+	`group` VARCHAR(50) NULL DEFAULT 'user',
+	`inventory` LONGTEXT NULL DEFAULT NULL,
+	`job` VARCHAR(20) NULL DEFAULT 'unemployed',
+	`job_grade` INT NULL DEFAULT 0,
+	`loadout` LONGTEXT NULL DEFAULT NULL,
+	`position` VARCHAR(255) NULL DEFAULT '{"x":165.95,"y":-979.92,"z":30.1,"heading":330.3}',
+
+	PRIMARY KEY (`identifier`)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE `items` (
+	`name` VARCHAR(50) NOT NULL,
+	`label` VARCHAR(50) NOT NULL,
+	`weight` INT NOT NULL DEFAULT 1,
+	`rare` TINYINT NOT NULL DEFAULT 0,
+	`can_remove` TINYINT NOT NULL DEFAULT 1,
+
+	PRIMARY KEY (`name`)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE `job_grades` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`job_name` VARCHAR(50) DEFAULT NULL,
+	`grade` INT NOT NULL,
+	`name` VARCHAR(50) NOT NULL,
+	`label` VARCHAR(50) NOT NULL,
+	`salary` INT NOT NULL,
+	`skin_male` LONGTEXT NOT NULL,
+	`skin_female` LONGTEXT NOT NULL,
+
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+
+INSERT INTO `job_grades` VALUES (1,'unemployed',0,'unemployed','A-Kassa',200,'{}','{}');
+
+CREATE TABLE `jobs` (
+	`name` VARCHAR(50) NOT NULL,
+	`label` VARCHAR(50) DEFAULT NULL,
+
+	PRIMARY KEY (`name`)
+) ENGINE=InnoDB;
+
+
+INSERT INTO `jobs` VALUES ('unemployed','Arbetslös');
