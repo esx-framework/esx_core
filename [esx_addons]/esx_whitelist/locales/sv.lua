9a71e762aec23ae120852fca6160e130609c7e96
JonarSonaer <haikara98@gmail.com>
2022-03-02 23:39:26 +0100

    Add files via upload

--- [esx_addons]/esx_whitelist/locales/sv.lua
+++ [esx_addons]/esx_whitelist/locales/sv.lua
@@ -1,8 +1,8 @@
 Locales['sv'] = {
-  ['whitelist_check']     = 'making sure you\'re whitelisted on this server . . .',
-  ['not_whitelisted']     = 'du är inte whitelistad på denna server!',
-  ['whitelist_empty']     = 'whitelisten har ännu inte laddats in, eller så är ingen whitelitad!',
-  ['license_missing']     = 'din licens kunde ej hittas',
-  ['help_whitelist_add']  = 'lägg till någon till whitelisten',
-  ['help_whitelist_load'] = 'ladda om whitelist',
+  ['whitelist_check']     = 'Ser till att du är vitlistad på den här servern . . .',
+  ['not_whitelisted']     = 'Du är inte vitlistad på den här servern!',
+  ['whitelist_empty']     = 'Vitlistan har ännu inte laddats in, eller så är ingen vitlistan!',
+  ['license_missing']     = 'Din licens kunde inte hittas',
+  ['help_whitelist_add']  = 'Lägg till någon på vitlistan',
+  ['help_whitelist_load'] = 'Ladda om vitlistan',
 }
