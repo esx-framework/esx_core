c07f36ea69a7bc1d92e05680a6d553b98ec7d8b9
JonarSonaer <haikara98@gmail.com>
2022-03-06 19:56:09 +0100

    Updated Swedish Translations

--- [esx_addons]/instance/locales/sv.lua
+++ [esx_addons]/instance/locales/sv.lua
@@ -1,8 +1,8 @@
 Locales['sv'] = {
-  ['left_instance'] = 'du har lämnat byggnaden',
-  ['invite_expired'] = 'din inbjudan har nekas',
-  ['press_to_enter'] = 'tryck ~INPUT_CONTEXT~ för att gå in i byggnaden',
-  ['entered_instance'] = 'du är inne i byggnaden',
+  ['left_instance'] = 'Du har lämnat byggnaden',
+  ['invite_expired'] = 'Inbjudan har löpt ut',
+  ['press_to_enter'] = 'Tryck ~INPUT_CONTEXT~ för att gå in i byggnaden',
+  ['entered_instance'] = 'Du gick in i byggnaden',
   ['entered_into'] = '%s gick in i byggnaden',
   ['left_out'] = '%s har lämnat byggnaden',
 }
