be158cbfbcf5e39c4c4e77b894a9ae981658d6ec
Dr Bad <58116609+Dr-Bad@users.noreply.github.com>
2022-03-16 22:48:33 +0330

    Fix: add missing esx_dmvschool sql

--- [SQL]/legacy.sql
+++ [SQL]/legacy.sql
@@ -317,6 +317,10 @@ CREATE TABLE `licenses` (
 --
 
 INSERT INTO `licenses` (`type`, `label`) VALUES
+('dmv', 'Code de la route'),
+('drive', 'Permis de conduire'),
+('drive_bike', 'Permis moto'),
+('drive_truck', 'Permis camion'),
 ('weed_processing', 'Weed Processing License');
 
 -- --------------------------------------------------------
