cfd232d89ea6bbb24eb359000d82137c628f3e13
FantomasIsBad <102142635+FantomasIsBad@users.noreply.github.com>
2022-03-23 12:19:54 +0100

    Fix fxmanifest in esx_ambulancejob

--- [esx_addons]/esx_ambulancejob/fxmanifest.lua
+++ [esx_addons]/esx_ambulancejob/fxmanifest.lua
@@ -6,41 +6,19 @@ description 'ESX Ambulance Job'
 
 version '1.6.5'
 
-c
+shared_scripts { 
+	'@es_extended/imports.lua'
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	'config.lua'
+}
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/de.lua',
-	'locales/cs.lua',
-	'locales/nl.lua',
-	'locales/tr.lua',
-	'locales/hu.lua',
-	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
-	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/es.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/cs.lua',
-	'locales/nl.lua',
-	'locales/tr.lua',
-	'locales/hu.lua',
-	'config.lua',
 	'client/main.lua',
 	'client/job.lua',
 	'client/vehicle.lua',
