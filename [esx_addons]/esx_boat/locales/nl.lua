6683f2e6b423c4729afb33300e937df6a3aefe7d
Unslaught <102593774+unslaught@users.noreply.github.com>
2022-04-07 22:44:53 +0200

    feat(dutch language translation for esx_boat)
    Additional Translation for esx_boat

---
+++ [esx_addons]/esx_boat/locales/nl.lua
@@ -0,0 +1,31 @@
+Locales['nl'] = {
+  -- shop
+  ['boat_shop'] = 'boten winkel',
+  ['boat_shop_open'] = 'Druk op ~INPUT_CONTEXT~ tom de ~y~boten winkel~s~te openen.',
+  ['boat_shop_confirm'] = 'Koop <span style="color: yellow;">%s</span> voor <span style="color: orange;">€%s</span>?',
+  ['boat_shop_bought'] = 'Je hebt een ~y~gekocht~s~een ~b~%s~s~ voor ~g~€%s~s~',
+  ['boat_shop_nomoney'] = 'Deze boot kan je niet betalen!',
+  ['confirm_no'] = 'nee',
+  ['confirm_yes'] = 'ja',
+
+  -- garage
+  ['garage'] = 'boten garage',
+  ['garage_open'] = 'Druk op ~INPUT_CONTEXT~ om je ~y~boten garage~s~te openen.',
+  ['garage_store'] = 'Druk op ~INPUT_CONTEXT~ om je boot op te slaan.',
+  ['garage_taken'] = 'de boot staat klaar!',
+  ['garage_stored'] = 'de boot is opgeslagen in je garage!',
+  ['garage_noboats'] = 'je hebt geen opgeslagen boten! Ga naar de ~y~boten winkel~s~ om er een te kopen.',
+  ['garage_blocked'] = 'de boot kan er niet uit omdat iets het spawnpunt blokkeerd!',
+  ['garage_notowner'] = 'je bent geen eigenaar van deze boot!',
+
+  -- license
+  ['license_menu'] = 'Koop Vaarbewijs?',
+  ['license_buy_no'] = 'nee',
+  ['license_buy_yes'] = 'Koop vaarbewijs <span style="color: green;">€%s</span>',
+  ['license_bought'] = 'je kocht het ~y~Vaar Bewijs~s~ voor ~g~€%s~s~',
+  ['license_nomoney'] = 'je kan het ~y~Vaar Bewijs~s~niet betalen! ',
+
+  -- blips
+  ['blip_garage'] = 'boten garage',
+  ['blip_shop'] = 'boten winkel',
+}
