664f1c41c4060b6b932a5d8c29ac0242b4a868b9
manutrix <patrickbeaudoin@manutrix.com>
2022-04-11 12:47:39 -0400

    Add esx_shops.sql. This table is being used in esx_optionalneeds but never created

---
+++ [esx_addons]/esx_shops/esx_shops.sql
@@ -0,0 +1,10 @@
+USE `es_extended`;
+
+CREATE TABLE `shops` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`store` varchar(100) NOT NULL,
+	`item` varchar(100) NOT NULL,
+	`price` int(11) NOT NULL,
+
+	PRIMARY KEY (`id`)
+);
