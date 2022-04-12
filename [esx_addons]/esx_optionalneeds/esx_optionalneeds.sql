f293ef770ca72014bc57707608a95e3bfb561740
manutrix <patrickbeaudoin@manutrix.com>
2022-04-12 07:57:17 -0400

    Add shops table

--- [esx_addons]/esx_optionalneeds/esx_optionalneeds.sql
+++ [esx_addons]/esx_optionalneeds/esx_optionalneeds.sql
@@ -1,5 +1,14 @@
 USE `es_extended`;
 
+CREATE TABLE `shops` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`store` varchar(100) NOT NULL,
+	`item` varchar(100) NOT NULL,
+	`price` int(11) NOT NULL,
+
+	PRIMARY KEY (`id`)
+);
+
 INSERT INTO `items` (`name`, `label`, `weight`) VALUES
 	('beer', 'Beer', 1)
 ;
