d6cd2fa37f36d09c433dd58c597f1e6145ab605e
manutrix <patrickbeaudoin@manutrix.com>
2022-04-14 07:39:29 -0400

    Remove an insert on a table that doesn't exist in esx_shops

--- [esx_addons]/esx_optionalneeds/esx_optionalneeds.sql
+++ [esx_addons]/esx_optionalneeds/esx_optionalneeds.sql
@@ -1,20 +1,5 @@
 USE `es_extended`;
 
-CREATE TABLE `shops` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`store` varchar(100) NOT NULL,
-	`item` varchar(100) NOT NULL,
-	`price` int(11) NOT NULL,
-
-	PRIMARY KEY (`id`)
-);
-
 INSERT INTO `items` (`name`, `label`, `weight`) VALUES
 	('beer', 'Beer', 1)
-;
-
-INSERT INTO `shops` (store, item, price) VALUES
-	('TwentyFourSeven', 'beer', 45),
-	('RobsLiquor', 'beer', 45),
-	('LTDgasoline', 'beer', 45)
 ;
