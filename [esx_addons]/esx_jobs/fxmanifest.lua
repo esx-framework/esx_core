c7a89aab5ab172a91977988af894e2112b5da058
iSentrie <xvender7@gmail.com>
2022-04-14 19:10:32 +0300

    refactor

--- [esx_addons]/esx_jobs/fxmanifest.lua
+++ [esx_addons]/esx_jobs/fxmanifest.lua
@@ -10,17 +10,16 @@ shared_scripts {
 	'@es_extended/imports.lua',
 	'@es_extended/locale.lua',
 	'locales/*.lua',
-	'config.lua'
+	'config.lua',
+	'jobs/*.lua'
 }
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
 	'server/main.lua'
-	'jobs/*.lua'
 }
 
 client_scripts {
-	'client/main.lua',
-	'jobs/*.lua'
+	'client/main.lua'
 }
 
 dependency 'es_extended'
