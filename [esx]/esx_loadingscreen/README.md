b01c1321334fcef956d6ca3201894666eb605330
Mycroft <kaseyfitton@hotmail.co.uk>
2022-04-25 18:18:57 +0100

    Feat(esx/esx_loadscreen): New Loadingscreen

---
+++ [esx]/esx_loadingscreen/README.md
@@ -0,0 +1,3 @@
+# ESX Loadingscreen
+
+A loadingscreen
---
+++ [esx]/esx_loadingscreen/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'ESX-Framework'
+version '1.2.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    './vid/*.mp4',
+    './vid/*.webm',
+    './js/index.js',
+    './css/index.css',
+    'index.html',
+}
