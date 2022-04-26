0ae7aa424a3629800eba4e20cb835d17aba96aca
CsokiHUN <feketetibi71@gmail.com>
2022-04-26 18:58:27 +0200

    Add some hungarian translations

--- [esx_addons]/esx_lscustom/fxmanifest.lua
+++ [esx_addons]/esx_lscustom/fxmanifest.lua
@@ -11,22 +11,14 @@ shared_script '@es_extended/imports.lua'
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
-	'locales/pl.lua',
-	'locales/br.lua',
-	'locales/de.lua',
+	'locales/*.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
-	'locales/pl.lua',
-	'locales/br.lua',
-	'locales/de.lua',
+	'locales/*.lua',
 	'config.lua',
 	'client/main.lua'
 }
---
+++ [esx_addons]/esx_optionalneeds/localization/hu_esx_optionalneeds.sql
@@ -0,0 +1,5 @@
+USE `es_extended`;
+
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
+	('beer', 'Sör', 1)
+;
