3721449a4fd21e7759a8d02a15b404597ebdb078
CsokiHUN <feketetibi71@gmail.com>
2022-04-26 18:12:54 +0200

    Add hungarian translation - esx_drugs + fxmanifest update

---
+++ [esx_addons]/esx_drugs/locales/hu.lua
@@ -0,0 +1,31 @@
+Locales ['hu'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'Nyomd meg a ~INPUT_CONTEXT~ gombot a betakarításhoz ~g~Kannabisz~s~',
+  ['weed_quitprocess'] = 'Nyomd meg a ~INPUT_CONTEXT~ gombot a betakarítás befejezéséhez',
+  ['weed_inventoryfull'] = 'Nincs több hely a  ~g~Kannabisz~s~ számára.',
+  ['weed_processprompt'] = 'Nyomd meg a ~INPUT_CONTEXT~ gombot a kezdéshez ~g~Kannabisz betakarítása~s~.',
+  ['weed_processingstarted'] = 'Folyamatban ~g~Kannabisz~s~ into ~g~Marihuana~s~...',
+  ['weed_processingfull'] = 'Betakarítás ~r~befejezve~s~ a raktár tele',
+  ['weed_processingenough'] = 'Rendelkezned kell ~b~3x~s~ ~g~Kannabisz~s~ a végrehajtáshoz',
+  ['weed_processed'] = 'Feldolgoztad ~b~3x~s~ ~g~Kannabisz~s~ to ~b~1x~s~ ~g~Marihuana~s~',
+  ['weed_processingtoofar'] = 'A feldolgozás ~r~megszakadt~s~ elhagytad a területet.',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'Nyomd meg a ~INPUT_CONTEXT~ gombot a találkozáshoz a ~r~Dílerrel~s~.',
+  ['dealer_title'] = 'Díler',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = 'Nincs elég anyagod az eladáshoz',
+  ['dealer_sold'] = 'Sikeres eladás ~b~%sx~s~ ~y~%s~s~ for ~g~$%s~s~',
+
+  -- license
+  ['license_title'] = 'A feldolgozáshoz engedélyek kellenek, melyeket itt megvehetsz',
+  ['license_no'] = 'Nem',
+  ['license_bought'] = 'Megvetted a  ~b~%s~s~ for ~r~$%s~s~',
+  ['license_bought_fail'] = 'Nincs elég pénzed ~b~%s~s~!',
+  ['license_weed'] = 'Kannabisz Feldolgozási Engedély',
+
+  -- blips
+  ['blip_weedfield'] = 'Kannabisz ültetvény',
+  ['blip_weedprocessing'] = 'Kannabisz feldolgozás',
+  ['blip_drugdealer'] = 'Díler',
+}
