02035019aa4ae90da900339aa32589561589bac2
CsokiHUN <feketetibi71@gmail.com>
2022-04-26 18:05:58 +0200

    Add some hungarian translations

---
+++ [esx_addons]/esx_basicneeds/localization/hu_esx_basicneeds.sql
@@ -0,0 +1,6 @@
+USE `es_extended`;
+
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
+	('bread', 'Kenyér', 1),
+	('water', 'Víz', 1)
+;
---
+++ [esx_addons]/esx_clotheshop/locales/hu.lua
@@ -0,0 +1,12 @@
+Locales["hu"] = {
+	["valid_this_purchase"] = "Megerősíted a vásárlást?",
+	["yes"] = "igen",
+	["no"] = "nem",
+	["not_enough_money"] = "Nincs elég pénzed",
+	["press_menu"] = "Nyomd meg a ~INPUT_CONTEXT~ gombot a  ~y~Ruhabolt~s~ megnyitásához.",
+	["clothes"] = "ruhabolt",
+	["you_paid"] = "fizettél ~g~%s$-t~s~",
+	["save_in_dressing"] = "Menteni szeretnéd a ruhát az ingatlanodban?",
+	["name_outfit"] = "Ruha neve",
+	["saved_outfit"] = "Ruha mentésre került!",
+}
