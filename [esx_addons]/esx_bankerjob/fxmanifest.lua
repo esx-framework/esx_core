1fb5a2e237430a625042e892cad9aab1368a1067
CsokiHUN <feketetibi71@gmail.com>
2022-04-26 18:06:30 +0200

    Update locale files in some resource fxmanifest

--- [esx_addons]/esx_bankerjob/fxmanifest.lua
+++ [esx_addons]/esx_bankerjob/fxmanifest.lua
@@ -13,28 +13,14 @@ server_scripts {
 	'@oxmysql/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'config.lua',
-	'locales/de.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/es.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/hu.lua',
+	'locales/*.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
 	'config.lua',
-	'locales/de.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/es.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/hu.lua',
+	'locales/*.lua',
 	'client/main.lua'
 }
 
--- [esx_addons]/esx_barbershop/fxmanifest.lua
+++ [esx_addons]/esx_barbershop/fxmanifest.lua
@@ -10,32 +10,14 @@ shared_script '@es_extended/imports.lua'
 
 server_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/es.lua',
-	'locales/cs.lua',
-	'locales/pl.lua',
-	'locales/tr.lua',
-	'locales/hu.lua',
+	'locales/*.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/de.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/es.lua',
-	'locales/cs.lua',
-	'locales/pl.lua',
-	'locales/tr.lua',
-	'locales/hu.lua',
+	'locales/*.lua',
 	'config.lua',
 	'client/main.lua'
 }
