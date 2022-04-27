a802930b005356e7100d8c64901af14fdbf94208
CsokiHUN <feketetibi71@gmail.com>
2022-04-27 09:36:00 +0200

    Add hungarian translations - esx_policejob, esx_property, esx_realestateagentjob, esx_rpchat, esx_shops, esx_society, esx_taxijob

---
+++ [esx_addons]/esx_realestateagentjob/locales/hu.lua
@@ -0,0 +1,27 @@
+Locales['hu'] = {
+  ['properties'] = 'tulajdonságok',
+  ['clients'] = 'kliensek',
+  ['realtor'] = 'ingatlanügynök',
+  ['amount_withdraw'] = 'kifizetett összeg',
+  ['invalid_amount'] = 'érvénytelen mennyiség',
+  ['press_to_access'] = 'nyomd meg a ~INPUT_CONTEXT~ gombot a menü eléréséhez',
+  ['amount'] = 'mennyiség',
+  ['no_play_near'] = 'nincs a közelben játékos',
+  ['realtors'] = 'intaglatügynökök',
+  ['boss_action'] = 'főnök műveletek',
+  ['client_poor'] = 'a kliens nem engedheti meg magának',
+
+  -- Property menu
+  ['property_name'] = 'ingatlan',
+  ['property_actions'] = 'műveletek',
+  ['property_actionbuttons'] = '{{Hozzárendelés [Eladás]|sell}} {{Hozzárendelés [Bérlés]|rent}} {{GPS|gps}}',
+
+  -- Customer menu
+  ['customer_client'] = 'kliens',
+  ['customer_property'] = 'ingatlan',
+  ['customer_agreement'] = 'megegyezés',
+  ['customer_actions'] = 'műveletek',
+  ['customer_contractbuttons'] = '{{Szerződés visszavonása|revoke}} {{GPS|gps}}',
+  ['customer_rent'] = 'bérelt',
+  ['customer_sell'] = 'eladott',
+}
