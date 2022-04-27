ec76e175793cf876469157c06498945e0eaa0899
CsokiHUN <feketetibi71@gmail.com>
2022-04-27 09:46:48 +0200

    Add hungarian translations - esx_weaponshop

---
+++ [esx_addons]/esx_weaponshop/locales/hu.lua
@@ -0,0 +1,13 @@
+Locales ['hu'] = {
+  ['buy_license'] = 'Veszel fegyver engedélyt?',
+  ['yes'] = '%s',
+  ['no'] = 'Igen',
+  ['weapon_bought'] = 'Megvetted ennyiért: %s$',
+  ['not_enough_black'] = 'Nincsen elég pénzed',
+  ['not_enough'] = 'Nincsen elég pénzed',
+  ['already_owned'] = 'Van már ilyened!',
+  ['shop_menu_title'] = 'Fegyverbolt',
+  ['shop_menu_prompt'] = 'Nyomj ~INPUT_CONTEXT~ hogy megnézd a kinálatot',
+  ['shop_menu_item'] = '%s$',
+  ['map_blip'] = 'Fegyverbolt',
+}
