aba074abe00f5d5b9ae00538c431a3fb931f9b50
Mert Ayhan <com9@hotmail.com.tr>
2022-05-14 10:16:29 +0300

    tr language support for esx_bankerjob

---
+++ [esx_addons]/esx_bankerjob/locales/tr.lua
@@ -0,0 +1,19 @@
+Locales['tr'] = {
+    ['actions']                           = 'hareketler',
+    ['amount']                            = 'tutar',
+    ['balance']                           = 'bakiye',
+    ['bank']                              = 'banka',
+    ['bill_amount']                       = 'fatura tutarı',
+    ['billing']                           = 'faturalandırma',
+    ['customer']                          = 'müşteri',
+    ['customers']                         = 'müşteriler',
+    ['deposit']                           = 'para yatır',
+    ['invalid_amount']                    = 'geçersiz tutar',
+    ['no_player_nearby']                  = 'yakında oyuncu yok',
+    ['press_input_context_to_open_menu']  = '~g~Banka Menüsünü~s~ açmak için ~INPUT_CONTEXT~ tuşuna basın.',
+    ['withdraw']                          = 'para çek',
+    ['boss_actions']                      = 'patron Hareketleri',
+    ['phone_receive']                     = 'banka Müşterisi',
+    ['phone_label']                       = 'banka',
+  }
+  
---
+++ [esx_addons]/esx_bankerjob/localization/tr_esx_bankerjob.sql
@@ -0,0 +1,18 @@
+USE `es_extended`;
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_banker','Banka',1),
+	('bank_savings','Banka Birikimi',0)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('banker','Bankacı')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('banker',0,'advisor','Danışman',10,'{}','{}'),
+	('banker',1,'banker','Bankacı',20,'{}','{}'),
+	('banker',2,'business_banker','İşletme Bankacısı',30,'{}','{}'),
+	('banker',3,'trader','Borsacı',40,'{}','{}'),
+	('banker',4,'boss','Yönetici',0,'{}','{}')
+;
