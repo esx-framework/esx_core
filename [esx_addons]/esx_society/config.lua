efeaeb21aa4076e509cd9e440950a89c79135af0
AnthoHansen <62178203+Antho7Twitch@users.noreply.github.com>
2022-06-04 19:51:55 +0200

    Update config.lua

--- [esx_addons]/esx_society/config.lua
+++ [esx_addons]/esx_society/config.lua
@@ -3,3 +3,10 @@ Config = {}
 Config.Locale = 'en'
 Config.EnableESXIdentity = true
 Config.MaxSalary = 3500
+
+
+Config.Withdraw = true
+Config.Deposit  = true
+Config.Wash     = false
+Config.EmployeesManagement = true
+Config.GradesManagement = false
