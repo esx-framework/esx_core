87010bb24c00be08facae8dce8210161f49fe742
LAB Network <106167115+LABNetwork1@users.noreply.github.com>
2022-06-08 11:30:00 +0100

    Optimizing menu_default
    Removed all loops

--- [esx]/esx_menu_default/client/main.lua
+++ [esx]/esx_menu_default/client/main.lua
@@ -1,5 +1,4 @@
-local GUI, MenuType = {}, 'default'
-GUI.Time = 0
+local MenuType = 'default'
 
 local function openMenu(namespace, name, data)
 	SendNUIMessage({
@@ -56,38 +55,38 @@ RegisterNUICallback('menu_change', function(data, cb)
 	cb('OK')
 end)
 
-CreateThread(function()
-	while true do
-		Wait(15)
-
-		if IsControlPressed(0, 18) and IsUsingKeyboard(0) and (GetGameTimer() - GUI.Time) > 150 then
-			SendNUIMessage({action = 'controlPressed', control = 'ENTER'})
-			GUI.Time = GetGameTimer()
-		end
-
-		if IsControlPressed(0, 177) and IsUsingKeyboard(0) and (GetGameTimer() - GUI.Time) > 150 then
-			SendNUIMessage({action  = 'controlPressed', control = 'BACKSPACE'})
-			GUI.Time = GetGameTimer()
-		end
-
-		if IsControlPressed(0, 27) and IsUsingKeyboard(0) and (GetGameTimer() - GUI.Time) > 200 then
-			SendNUIMessage({action  = 'controlPressed', control = 'TOP'})
-			GUI.Time = GetGameTimer()
-		end
-
-		if IsControlPressed(0, 173) and IsUsingKeyboard(0) and (GetGameTimer() - GUI.Time) > 200 then
-			SendNUIMessage({action  = 'controlPressed', control = 'DOWN'})
-			GUI.Time = GetGameTimer()
-		end
-
-		if IsControlPressed(0, 174) and IsUsingKeyboard(0) and (GetGameTimer() - GUI.Time) > 150 then
-			SendNUIMessage({action  = 'controlPressed', control = 'LEFT'})
-			GUI.Time = GetGameTimer()
-		end
-
-		if IsControlPressed(0, 175) and IsUsingKeyboard(0) and (GetGameTimer() - GUI.Time) > 150 then
-			SendNUIMessage({action  = 'controlPressed', control = 'RIGHT'})
-			GUI.Time = GetGameTimer()
-		end
-	end
+RegisterCommand("ENTER", function()
+    SendNUIMessage({action = "controlPressed", control = "ENTER"})
 end)
+
+RegisterKeyMapping("ENTER", "[ESX MENU DEFAULT] ENTER", "keyboard", "RETURN")
+
+RegisterCommand("BACKSPACE", function()
+    SendNUIMessage({action  = "controlPressed", control = "BACKSPACE"})
+end)
+
+RegisterKeyMapping("BACKSPACE", "[ESX MENU DEFAULT] BACKSPACE", "keyboard", "BACK")
+
+RegisterCommand("TOP", function()
+    SendNUIMessage({action  = "controlPressed", control = "TOP"})
+end)
+
+RegisterKeyMapping("TOP", "[ESX MENU DEFAULT] TOP", "keyboard", "UP")
+
+RegisterCommand("DOWN", function()
+    SendNUIMessage({action  = "controlPressed", control = "DOWN"})
+end)
+
+RegisterKeyMapping("DOWN", "[ESX MENU DEFAULT] DOWN", "keyboard", "DOWN")
+
+RegisterCommand("LEFT", function()
+    SendNUIMessage({action  = "controlPressed", control = "LEFT"})
+end)
+
+RegisterKeyMapping("LEFT", "[ESX MENU DEFAULT] LEFT", "keyboard", "LEFT")
+
+RegisterCommand("RIGHT", function()
+    SendNUIMessage({action  = "controlPressed", control = "RIGHT"})
+end)
+
+RegisterKeyMapping("RIGHT", "[ESX MENU DEFAULT] RIGHT", "keyboard", "RIGHT")
