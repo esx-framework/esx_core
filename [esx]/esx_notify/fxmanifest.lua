2063bab6de9ba33a963e0a2b6ef40650bd0a6a86
Benzo <102178921+Benzo00@users.noreply.github.com>
2022-06-09 01:58:39 +0200

    fix(textui) support color codes

--- [esx]/esx_notify/fxmanifest.lua
+++ [esx]/esx_notify/fxmanifest.lua
@@ -1,15 +1,14 @@
 fx_version 'adamant'
 game 'gta5'
 author 'ESX-Framework'
-description 'ESX Notification'
+description 'ESX TextUI'
 
-client_scripts { 'Notify.lua' }
-
+client_scripts { 'TextUI.lua' }
+shared_script '@es_extended/imports.lua'
 ui_page 'nui/index.html'
 
 files {
     'nui/index.html',
     'nui/js/*.js',
-    'nui/css/*.css',
-    'nui/img/*.png',
+    'nui/css/*.css'
 }
