ef1b31c0f769dc6f78f8c45c15a0a3f3d1a9cfb1
xorzo <80540494+xorzo1@users.noreply.github.com>
2022-07-06 13:53:35 +0200

    Update TextUI.lua

--- [esx]/esx_textui/TextUI.lua
+++ [esx]/esx_textui/TextUI.lua
@@ -1,3 +1,7 @@
+Debug = false
+
+---@param message any
+---@param type string
 local function TextUI(message, type)
     SendNUIMessage({
         action = 'show',
@@ -16,3 +20,21 @@ exports('TextUI', TextUI)
 exports('HideUI', HideUI)
 RegisterNetEvent('ESX:TextUI', TextUI)
 RegisterNetEvent('ESX:HideUI', HideUI)
+
+if Debug then
+    RegisterCommand("textui:error", function()
+        TriggerEvent("ESX:TextUI", "i ~r~love~s~ donuts", "error")
+    end)
+
+    RegisterCommand("textui:success", function()
+        TriggerEvent("ESX:TextUI", "i ~r~love~s~ donuts", "success")
+    end)
+
+    RegisterCommand("textui:info", function()
+        TriggerEvent("ESX:TextUI", "i ~r~love~s~ donuts", "info")
+    end)
+
+    RegisterCommand("textui:hide", function()
+        TriggerEvent("ESX:HideUI")
+    end)
+end
