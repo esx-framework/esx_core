b3580bc10bf596c807895713368ff496c5c9cb0b
Mycroft <kaseyfitton@hotmail.co.uk>
2022-07-16 15:06:12 +0100

    feat: ES Extended: Better Relog Support

--- [esx]/es_extended/imports.lua
+++ [esx]/es_extended/imports.lua
@@ -1,10 +1,22 @@
 ESX = exports['es_extended']:getSharedObject()
 
 if not IsDuplicityVersion() then -- Only register this event for the client
-	AddEventHandler('esx:setPlayerData', function(key, val, last)
-		if GetInvokingResource() == 'es_extended' then
-			ESX.PlayerData[key] = val
-			if OnPlayerData ~= nil then OnPlayerData(key, val, last) end
-		end
-	end)
+    AddEventHandler('esx:setPlayerData', function(key, val, last)
+        if GetInvokingResource() == 'es_extended' then
+            ESX.PlayerData[key] = val
+            if OnPlayerData then
+                OnPlayerData(key, val, last)
+            end
+        end
+    end)
+
+    RegisterNetEvent('esx:playerLoaded', function(xPlayer)
+        ESX.PlayerData = xPlayer
+        ESX.PlayerLoaded = true
+    end)
+
+    RegisterNetEvent('esx:onPlayerLogout', function()
+        ESX.PlayerLoaded = false
+        ESX.PlayerData = {}
+    end)
 end
