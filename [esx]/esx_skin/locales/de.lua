305137815fd065fd82396c58722e96c8305691ba
SlyJordi <82180160+slyjordi@users.noreply.github.com>
2022-07-28 01:49:23 +0200

    Update de.lua

--- [esx]/esx_skin/locales/de.lua
+++ [esx]/esx_skin/locales/de.lua
@@ -1,6 +1,6 @@
 Locales['de'] = {
   ['skin_menu'] = 'Skin Menü',
-  ['use_rotate_view'] = 'Nutze ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ und ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ um dich umzuschauen.',
+  ['use_rotate_view'] = 'Nutze ~INPUT_FRONTEND_LS~ und ~INPUT_CHARACTER_WHEEL~ um dich umzuschauen.',
   ['skin'] = 'Skin ändern',
   ['saveskin'] = 'Skin in Datei speichern',
 }
