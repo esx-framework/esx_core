1f6bf49b7a2c9c460ea22b307b265934a926d2e1
SlyJordi <82180160+slyjordi@users.noreply.github.com>
2022-07-28 01:34:54 +0200

    fixed the keys

--- [esx]/esx_skin/locales/br.lua
+++ [esx]/esx_skin/locales/br.lua
@@ -1,6 +1,6 @@
 Locales['br'] = {
   ['skin_menu'] = 'Skin Menu',
-  ['use_rotate_view'] = 'Use ~INPUT_CHARACTER_WHEEL~ e ~INPUT_SPRINT~ para girar a visão.',
+  ['use_rotate_view'] = 'Use ~INPUT_FRONTEND_LS~ e ~INPUT_CHARACTER_WHEEL~ para girar a visão.',
   ['skin'] = 'Trocar',
   ['saveskin'] = 'Salvar',
 }
