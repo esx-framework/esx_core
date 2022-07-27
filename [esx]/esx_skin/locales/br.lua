f726fdb96403f37b00486c129953b8d39cd2a044
SlyJordi <82180160+slyjordi@users.noreply.github.com>
2022-07-28 01:49:09 +0200

    Update br.lua

--- [esx]/esx_skin/locales/br.lua
+++ [esx]/esx_skin/locales/br.lua
@@ -1,6 +1,6 @@
 Locales['br'] = {
   ['skin_menu'] = 'Skin Menu',
-  ['use_rotate_view'] = 'Use ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ e ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para girar a visão.',
+  ['use_rotate_view'] = 'Use ~INPUT_FRONTEND_LS~ e ~INPUT_CHARACTER_WHEEL~ para girar a visão.',
   ['skin'] = 'Trocar',
   ['saveskin'] = 'Salvar',
 }
