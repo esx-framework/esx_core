6d118f7f7a0f006e1177b16f8b2c4f0ad8bcdb33
SlyJordi <82180160+slyjordi@users.noreply.github.com>
2022-07-28 01:35:28 +0200

    Update es.lua

--- [esx]/esx_skin/locales/es.lua
+++ [esx]/esx_skin/locales/es.lua
@@ -1,6 +1,6 @@
 Locales['es'] = {
   ['skin_menu'] = 'Menú Aspecto',
-  ['use_rotate_view'] = 'Utiliza ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ y ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para rotar la vista.',
+  ['use_rotate_view'] = 'Utiliza ~INPUT_FRONTEND_LS~ y ~INPUT_CHARACTER_WHEEL~ para rotar la vista.',
   ['skin'] = 'cambiar aspecto',
   ['saveskin'] = 'guardar aspecto en un archivo',
 }
