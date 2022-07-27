f6bff07dc1a80c8c1e20f3c84593858fb39421d3
SlyJordi <82180160+slyjordi@users.noreply.github.com>
2022-07-28 01:36:07 +0200

    fixed the keys

--- [esx]/esx_skin/locales/fi.lua
+++ [esx]/esx_skin/locales/fi.lua
@@ -1,6 +1,6 @@
 Locales['fi'] = {
   ['skin_menu'] = 'ulkonäkö',
-  ['use_rotate_view'] = 'paina ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ tai ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ liikutaaksesi kameraa.',
+  ['use_rotate_view'] = 'paina ~INPUT_FRONTEND_LS~ tai ~INPUT_CHARACTER_WHEEL~ liikutaaksesi kameraa.',
   ['skin'] = 'vaihda ulkonäköä',
   ['saveskin'] = 'talleta ulkonäkö tiedostoon',
 }
