065d7b8e9e93072b3eb36d20b840342e400a9c60
SlyJordi <82180160+slyjordi@users.noreply.github.com>
2022-07-28 01:33:46 +0200

    fixed the keys

--- [esx]/esx_skin/locales/de.lua
+++ [esx]/esx_skin/locales/de.lua
@@ -1,6 +1,6 @@
 Locales['de'] = {
   ['skin_menu'] = 'Skin Menü',
-  ['use_rotate_view'] = 'Nutze ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ und ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ um dich umzuschauen.',
+  ['use_rotate_view'] = 'Nutze ~INPUT_CHARACTER_WHEEL~ und ~INPUT_SPRINT~ um dich umzuschauen.',
   ['skin'] = 'Skin ändern',
   ['saveskin'] = 'Skin in Datei speichern',
 }
