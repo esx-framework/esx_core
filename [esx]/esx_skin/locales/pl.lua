286e301f51069048817c2fa18f966efb070b240e
SlyJordi <82180160+slyjordi@users.noreply.github.com>
2022-07-28 01:37:31 +0200

    Update pl.lua

--- [esx]/esx_skin/locales/pl.lua
+++ [esx]/esx_skin/locales/pl.lua
@@ -1,6 +1,6 @@
 Locales['pl'] = {
   ['skin_menu'] = 'menu wyglądu',
-  ['use_rotate_view'] = 'użyj ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ i ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ aby obrócić ekran.',
+  ['use_rotate_view'] = 'użyj ~INPUT_FRONTEND_LS~ i ~INPUT_CHARACTER_WHEEL~ aby obrócić ekran.',
   ['skin'] = 'zmień wygląd',
   ['saveskin'] = 'zapisz wygląd do pliku',
 }
