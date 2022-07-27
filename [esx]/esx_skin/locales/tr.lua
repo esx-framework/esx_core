11ca093312deb00f374bef99beeb26a066acdcf3
SlyJordi <82180160+slyjordi@users.noreply.github.com>
2022-07-28 01:50:56 +0200

    Update tr.lua

--- [esx]/esx_skin/locales/tr.lua
+++ [esx]/esx_skin/locales/tr.lua
@@ -1,6 +1,6 @@
 Locales['tr'] = {
   ['skin_menu'] = 'Karakter Menüsü',
-  ['use_rotate_view'] = '~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ ve ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ ile kamera açısını değiştirebilirsiniz.',
+  ['use_rotate_view'] = '~INPUT_FRONTEND_LS~ ve ~INPUT_CHARACTER_WHEEL~ ile kamera açısını değiştirebilirsiniz.',
   ['skin'] = 'Karakter Değiştir',
   ['saveskin'] = 'Karakteri bir dosyaya kaydet',
 }
