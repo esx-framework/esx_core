57104f1c37677bf3fa93d8a86bc0f32b8ccef7b9
SlyJordi <82180160+slyjordi@users.noreply.github.com>
2022-08-08 01:37:17 +0200

    Italian language for esx_shops

---
+++ [esx_addons]/esx_shops/locales/it.lua
@@ -0,0 +1,12 @@
+Locales['it'] = {
+  ['shop'] = 'supermercato',
+  ['shops'] = 'supermercati',
+  ['press_menu'] = 'premi [E] per fare acquisti in supermercato.',
+  ['shop_item'] = '%s€',
+  ['bought'] = 'hai acquistato %sx %s per ~r~%s€',
+  ['not_enough'] = 'non hai ~r~abbastanza soldi: %s',
+  ['player_cannot_hold'] = 'non hai spazio libero nel tuo inventario...',
+  ['shop_confirm'] = 'Acquista %sx %s per $%s?',
+  ['no'] = 'no',
+  ['yes'] = 'sì',
+}
