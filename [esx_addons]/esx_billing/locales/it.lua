c5690248d18b2a9845d5b431b0f59877b3f7636b
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-08-07 18:33:43 +0200

    added the Italian language for esx_billing

---
+++ [esx_addons]/esx_billing/locales/it.lua
@@ -0,0 +1,12 @@
+Locales['it'] = {
+  ['invoices'] = 'fatture',
+  ['invoices_item'] = '$%s',
+  ['received_invoice'] = 'hai appena ~r~ricevuto~s~ una fattura',
+  ['paid_invoice'] = 'hai ~g~pagato~s~ una fattura di ~r~$%s~s~',
+  ['no_invoices'] = 'non hai nessuna fattura da pagare',
+  ['received_payment'] = 'hai ~g~ricevuto~s~ il pagamento di ~r~$%s~s~',
+  ['player_not_online'] = 'il giocatore non ha effettuato l\'accesso',
+  ['no_money'] = 'non hai abbastanza soldi per pagare questo conto',
+  ['target_no_money'] = 'il giocatore ~r~non ha~s~ abbastanza soldi per pagare la fattura!',
+  ['keymap_showbills'] = 'apri il menu delle fatture',
+}
