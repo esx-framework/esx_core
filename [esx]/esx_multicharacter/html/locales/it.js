559ffe4895a0141e8317df4aea1374479e4f0fb7
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-08-08 19:14:30 +0200

    Italian translation esx_multicharacter

---
+++ [esx]/esx_multicharacter/html/locales/it.js
@@ -0,0 +1,8 @@
+const translate = new Object();
+
+translate.name = "Nome";
+translate.job = "Lavoro";
+translate.bank = "Banca";
+translate.money = "Contanti";
+translate.gender = "Genere";
+translate.dob = "Data di nascita";
