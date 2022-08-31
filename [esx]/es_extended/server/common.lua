3d569b3e9a22b4c6ed71250868052976fccb2241
Mycroft <kaseyfitton@hotmail.co.uk>
2022-08-31 15:17:28 +0100

    es_extended -> formatting

--- [esx]/es_extended/server/common.lua
+++ [esx]/es_extended/server/common.lua
@@ -13,115 +13,101 @@ Core.PickupId = 0
 Core.PlayerFunctionOverrides = {}
 
 AddEventHandler('esx:getSharedObject', function(cb)
-	cb(ESX)
+  cb(ESX)
 end)
 
 exports('getSharedObject', function()
-	return ESX
+  return ESX
 end)
 
 if GetResourceState('ox_inventory') ~= 'missing' then
-	Config.OxInventory = true
-	Config.PlayerFunctionOverride = 'OxInventory'
-	SetConvarReplicated('inventory:framework', 'esx')
-	SetConvarReplicated('inventory:weight', Config.MaxWeight * 1000)
+  Config.OxInventory = true
+  Config.PlayerFunctionOverride = 'OxInventory'
+  SetConvarReplicated('inventory:framework', 'esx')
+  SetConvarReplicated('inventory:weight', Config.MaxWeight * 1000)
 end
 
 local function StartDBSync()
-	CreateThread(function()
-		while true do
-			Wait(10 * 60 * 1000)
-			Core.SavePlayers()
-		end
-	end)
+  CreateThread(function()
+    while true do
+      Wait(10 * 60 * 1000)
+      Core.SavePlayers()
+    end
+  end)
 end
 
 MySQL.ready(function()
-	if not Config.OxInventory then
-		local items = MySQL.query.await('SELECT * FROM items')
-		for k, v in ipairs(items) do
-			ESX.Items[v.name] = {
-				label = v.label,
-				weight = v.weight,
-				rare = v.rare,
-				canRemove = v.can_remove
-			}
-		end
-	else
-		TriggerEvent('__cfx_export_ox_inventory_Items', function(ref)
-			if ref then
-				ESX.Items = ref()
-			end
-		end)
+  if not Config.OxInventory then
+    local items = MySQL.query.await('SELECT * FROM items')
+    for k, v in ipairs(items) do
+      ESX.Items[v.name] = {label = v.label, weight = v.weight, rare = v.rare, canRemove = v.can_remove}
+    end
+  else
+    TriggerEvent('__cfx_export_ox_inventory_Items', function(ref)
+      if ref then
+        ESX.Items = ref()
+      end
+    end)
 
-		AddEventHandler('ox_inventory:itemList', function(items)
-			ESX.Items = items
-		end)
+    AddEventHandler('ox_inventory:itemList', function(items)
+      ESX.Items = items
+    end)
 
-		while not next(ESX.Items) do Wait(0) end
-	end
+    while not next(ESX.Items) do
+      Wait(0)
+    end
+  end
 
-	local Jobs = {}
-	local jobs = MySQL.query.await('SELECT * FROM jobs')
+  local Jobs = {}
+  local jobs = MySQL.query.await('SELECT * FROM jobs')
 
-	for _, v in ipairs(jobs) do
-		Jobs[v.name] = v
-		Jobs[v.name].grades = {}
-	end
+  for _, v in ipairs(jobs) do
+    Jobs[v.name] = v
+    Jobs[v.name].grades = {}
+  end
 
-	local jobGrades = MySQL.query.await('SELECT * FROM job_grades')
+  local jobGrades = MySQL.query.await('SELECT * FROM job_grades')
 
-	for _, v in ipairs(jobGrades) do
-		if Jobs[v.job_name] then
-			Jobs[v.job_name].grades[tostring(v.grade)] = v
-		else
-			print(('[^3WARNING^7] Ignoring job grades for ^5"%s"^0 due to missing job'):format(v.job_name))
-		end
-	end
+  for _, v in ipairs(jobGrades) do
+    if Jobs[v.job_name] then
+      Jobs[v.job_name].grades[tostring(v.grade)] = v
+    else
+      print(('[^3WARNING^7] Ignoring job grades for ^5"%s"^0 due to missing job'):format(v.job_name))
+    end
+  end
 
-	for _, v in pairs(Jobs) do
-		if ESX.Table.SizeOf(v.grades) == 0 then
-			Jobs[v.name] = nil
-			print(('[^3WARNING^7] Ignoring job ^5"%s"^0 due to no job grades found'):format(v.name))
-		end
-	end
+  for _, v in pairs(Jobs) do
+    if ESX.Table.SizeOf(v.grades) == 0 then
+      Jobs[v.name] = nil
+      print(('[^3WARNING^7] Ignoring job ^5"%s"^0 due to no job grades found'):format(v.name))
+    end
+  end
 
-	if not Jobs then
-		-- Fallback data, if no jobs exist
-		ESX.Jobs['unemployed'] = {
-			label = 'Unemployed',
-			grades = {
-				['0'] = {
-					grade = 0,
-					label = 'Unemployed',
-					salary = 200,
-                    onDuty = false,
-					skin_male = {},
-					skin_female = {}
-				}
-			}
-		}
-	else
-		ESX.Jobs = Jobs
-	end
+  if not Jobs then
+    -- Fallback data, if no jobs exist
+    ESX.Jobs['unemployed'] = {label = 'Unemployed',
+                              grades = {['0'] = {grade = 0, label = 'Unemployed', salary = 200, skin_male = {}, skin_female = {}}}}
+  else
+    ESX.Jobs = Jobs
+  end
 
-	print('[^2INFO^7] ESX ^5Legacy^0 initialized')
-	StartDBSync()
-	StartPayCheck()
+  print('[^2INFO^7] ESX ^5Legacy^0 initialized')
+  StartDBSync()
+  StartPayCheck()
 end)
 
 RegisterServerEvent('esx:clientLog')
 AddEventHandler('esx:clientLog', function(msg)
-	if Config.EnableDebug then
-		print(('[^2TRACE^7] %s^7'):format(msg))
-	end
+  if Config.EnableDebug then
+    print(('[^2TRACE^7] %s^7'):format(msg))
+  end
 end)
 
 RegisterServerEvent('esx:triggerServerCallback')
 AddEventHandler('esx:triggerServerCallback', function(name, requestId, ...)
-	local playerId = source
+  local playerId = source
 
-	ESX.TriggerServerCallback(name, requestId, playerId, function(...)
-		TriggerClientEvent('esx:serverCallback', playerId, requestId, ...)
-	end, ...)
+  ESX.TriggerServerCallback(name, requestId, playerId, function(...)
+    TriggerClientEvent('esx:serverCallback', playerId, requestId, ...)
+  end, ...)
 end)
--- [esx]/es_extended/server/paycheck.lua
+++ [esx]/es_extended/server/paycheck.lua
@@ -1,41 +1,45 @@
 function StartPayCheck()
-	CreateThread(function()
-		while true do
-			Wait(Config.PaycheckInterval)
-			local xPlayers = ESX.GetExtendedPlayers()
-			for i=1, #(xPlayers) do 
-				local xPlayer = xPlayers[i]
-				local job     = xPlayer.job.grade_name
-				local salary  = xPlayer.job.grade_salary
+  CreateThread(function()
+    while true do
+      Wait(Config.PaycheckInterval)
+      local xPlayers = ESX.GetExtendedPlayers()
+      for i = 1, #(xPlayers) do
+        local xPlayer = xPlayers[i]
+        local job = xPlayer.job.grade_name
+        local salary = xPlayer.job.grade_salary
 
-				if salary > 0 then
-					if job == 'unemployed' then -- unemployed
-						xPlayer.addAccountMoney('bank', salary)
-						TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_help', salary), 'CHAR_BANK_MAZE', 9)
-					elseif Config.EnableSocietyPayouts then -- possibly a society
-						TriggerEvent('esx_society:getSociety', xPlayer.job.name, function (society)
-							if society ~= nil then -- verified society
-								TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function (account)
-									if account.money >= salary then -- does the society money to pay its employees?
-										xPlayer.addAccountMoney('bank', salary)
-										account.removeMoney(salary)
+        if salary > 0 then
+          if job == 'unemployed' then -- unemployed
+            xPlayer.addAccountMoney('bank', salary)
+            TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_help', salary),
+              'CHAR_BANK_MAZE', 9)
+          elseif Config.EnableSocietyPayouts then -- possibly a society
+            TriggerEvent('esx_society:getSociety', xPlayer.job.name, function(society)
+              if society ~= nil then -- verified society
+                TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function(account)
+                  if account.money >= salary then -- does the society money to pay its employees?
+                    xPlayer.addAccountMoney('bank', salary)
+                    account.removeMoney(salary)
 
-										TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
-									else
-										TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), '', _U('company_nomoney'), 'CHAR_BANK_MAZE', 1)
-									end
-								end)
-							else -- not a society
-								xPlayer.addAccountMoney('bank', salary)
-								TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
-							end
-						end)
-					else -- generic job
-						xPlayer.addAccountMoney('bank', salary)
-						TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
-					end
-				end
-			end
-		end
-	end)
+                    TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'),
+                      _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
+                  else
+                    TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), '', _U('company_nomoney'), 'CHAR_BANK_MAZE', 1)
+                  end
+                end)
+              else -- not a society
+                xPlayer.addAccountMoney('bank', salary)
+                TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary),
+                  'CHAR_BANK_MAZE', 9)
+              end
+            end)
+          else -- generic job
+            xPlayer.addAccountMoney('bank', salary)
+            TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary),
+              'CHAR_BANK_MAZE', 9)
+          end
+        end
+      end
+    end
+  end)
 end
