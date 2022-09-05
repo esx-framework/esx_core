6520a8b11ed6faa09339d6ed70fe5a95fe7e5e45
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-09-05 19:11:36 +0200

    feat: (created Italian language for esx_banking)

---
+++ [esx_addons]/esx_banking/locales/it.lua
@@ -0,0 +1,7 @@
+Locales['it'] = {
+  ['invalid_amount'] = 'Questa è una somma di denaro non valida',
+  ['deposit_money']  = 'hai depositato $%s',
+  ['withdraw_money'] = 'hai prelevato $%s',
+  ['press_e_banking']    = 'premi [E] per accedere alla banca',
+  ['banking_blip']       = 'Banca',
+}
