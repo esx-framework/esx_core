dffaa5caae3e8e77c2252675aaa7ba2482c1d7d8
Csoki <feketetibi71@gmail.com>
2022-09-15 00:24:55 +0200

    refactor translate function

--- [esx]/es_extended/locale.lua
+++ [esx]/es_extended/locale.lua
@@ -1,19 +1,15 @@
 Locales = {}
 
 function _(str, ...)  -- Translate string
-
-	if Locales[Config.Locale] ~= nil then
-
-		if Locales[Config.Locale][str] ~= nil then
-			return string.format(Locales[Config.Locale][str], ...)
-		else
-			return 'Translation [' .. Config.Locale .. '][' .. str .. '] does not exist'
-		end
-
-	else
+	if not Locales[Config.Locale] then 
 		return 'Locale [' .. Config.Locale .. '] does not exist'
 	end
 
+	if Locales[Config.Locale][str] then
+		return string.format(Locales[Config.Locale][str], ...)
+	end
+	
+	return 'Translation [' .. Config.Locale .. '][' .. str .. '] does not exist'
 end
 
 function _U(str, ...) -- Translate string first char uppercase
