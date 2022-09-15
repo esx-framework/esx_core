8d60e0ea73c103fdc6cb354e51f841c5dce97b4c
Mycroft <kaseyfitton@hotmail.co.uk>
2022-09-15 22:48:44 +0100

    feat(es_extended): Experimental fxDK support

--- [esx]/es_extended/server/functions.lua
+++ [esx]/es_extended/server/functions.lua
@@ -253,6 +253,10 @@ function ESX.GetPlayerFromIdentifier(identifier)
 end
 
 function ESX.GetIdentifier(playerId)
+  local fxDk = GetConvarInt('sv_fxdkMode', 0) 
+  if fxDk == 1 then
+    return "ESX-DEBUG-LICENCE"
+  end
   for k, v in ipairs(GetPlayerIdentifiers(playerId)) do
     if string.match(v, 'license:') then
       local identifier = string.gsub(v, 'license:', '')
