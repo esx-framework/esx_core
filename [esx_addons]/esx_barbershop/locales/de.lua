13bfa64d66470e2c2dd14927ab48394c3e400837
RoadToSix <92928205+roadtosixx@users.noreply.github.com>
2022-09-18 16:33:58 +0200

    esx_barbershop: update de.lua

--- [esx_addons]/esx_barbershop/locales/de.lua
+++ [esx_addons]/esx_barbershop/locales/de.lua
@@ -2,8 +2,8 @@ Locales['de'] = {
   ['valid_purchase'] = 'Einkauf bestätigen?',
   ['yes'] = 'ja',
   ['no'] = 'nein',
-  ['not_enough_money'] = 'du hast nicht genug Geld',
+  ['not_enough_money'] = 'Du hast nicht genug Geld',
   ['press_access'] = 'Drücke [E] um das Menü zu öffnen',
   ['barber_blip'] = 'Friseur',
-  ['you_paid'] = 'du bezahlst $%s',
+  ['you_paid'] = 'Du bezahlst $%s',
 }
