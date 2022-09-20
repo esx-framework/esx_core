c19ff41a6d25a510f6e4fb9cb38f5a95ce3177d4
RoadToSix <92928205+roadtosixx@users.noreply.github.com>
2022-09-20 12:12:47 +0200

    esx_joblisting: added missing locale

--- [esx_addons]/esx_joblisting/locales/de.lua
+++ [esx_addons]/esx_joblisting/locales/de.lua
@@ -2,4 +2,6 @@ Locales['de'] = {
   ['new_job'] = 'Du hast einen neuen Job!',
   ['access_job_center'] = 'Drücke [E] um das Arbeitsamt zu öffnen.',
   ['job_center'] = 'Arbeitsamt',
+  ["blip_text"] = "Job Center"
+
 }
