7d1306f78c74a72c0dc079539d8a9e966b132566
Taavi <97351942+Taavi-AU@users.noreply.github.com>
2022-09-22 20:25:11 +0800

    Update config.lua

--- [esx_addons]/esx_basicneeds/config.lua
+++ [esx_addons]/esx_basicneeds/config.lua
@@ -1,3 +1,17 @@
 Config = {}
 Config.Locale = 'en'
 Config.Visible = true
+
+Config.Food = {
+	["bread"] = {
+		status = 10000,
+		remove = false -- want the item to remove on use
+	}
+}
+
+Config.Drinks = {
+	["water"] = {
+		status = 10000,
+		remove = true  -- want the item to remove on use
+	}
+}
