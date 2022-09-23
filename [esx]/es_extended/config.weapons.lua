d8af09efe2c38e38901ef373e0ab1eabeef2abd9
Mycroft <kaseyfitton@hotmail.co.uk>
2022-09-23 19:20:57 +0100

    fix(es_extended): missing bracket

--- [esx]/es_extended/config.weapons.lua
+++ [esx]/es_extended/config.weapons.lua
@@ -961,28 +961,20 @@ Config.Weapons = {
 	-- Tools
 	{name = 'WEAPON_FIREEXTINGUISHER', label = _U('weapon_fireextinguisher'), components = {}, ammo = {label = _U('ammo_charge'), hash = `AMMO_FIREEXTINGUISHER`}},
 	{name = 'WEAPON_DIGISCANNER', label = _U('weapon_digiscanner'), components = {}},
-	{name = 'GADGET_PARACHUTE', label = _U('gadget_parachute'), components = {},
-	{
-				name = 'WEAPON_TACTICALRIFLE',
-				label = _U('weapon_tactilerifle'),
-				ammo = {label = _U('ammo_rounds'), hash = `AMMO_RIFLE`},
-				tints = Config.DefaultWeaponTints,
-				components = {
+	{name = 'GADGET_PARACHUTE', label = _U('gadget_parachute'), components = {}},
+	{name = 'WEAPON_TACTICALRIFLE',label = _U('weapon_tactilerifle'),ammo = {label = _U('ammo_rounds'), hash = `AMMO_RIFLE`},tints = Config.DefaultWeaponTints,
+		components = {
 						{name = 'clip_default', label = _U('component_clip_default'), hash = `COMPONENT_TACTICALRIFLE_CLIP_01`},
 						{name = 'clip_extended', label = _U('component_clip_extended'), hash = `COMPONENT_TACTICALRIFLE_CLIP_02`},
 						{name = 'flashlight', label = _U('component_flashlight'), hash = `COMPONENT_AT_AR_FLSH_REH`},
 						{name = 'grip', label = _U('component_grip'), hash = `COMPONENT_AT_AR_AFGRIP`},
 						{name = 'suppressor', label = _U('component_suppressor'), hash = `COMPONENT_AT_AR_SUPP_02`}
 				}
-			},
-			{
-				name = 'WEAPON_PRECISIONRIFLE',
-				label = _U('weapon_precisionrifle'),
-				ammo = {label = _U('ammo_rounds'), hash = `AMMO_SNIPER`},
-				tints = Config.DefaultWeaponTints,
+	},
+	{name = 'WEAPON_PRECISIONRIFLE', label = _U('weapon_precisionrifle'), ammo = {label = _U('ammo_rounds'), hash = `AMMO_SNIPER`}, tints = Config.DefaultWeaponTints,
 				components = {
 						{name = 'clip_default', label = _U('component_clip_default'), hash = `COMPONENT_PRECISIONRIFLE_CLIP_01`},
 				}
-			},
-			{name = 'WEAPON_METALDETECTOR', label = _U('weapon_metaldetector'), components = {}}
-		}
+	},
+	{name = 'WEAPON_METALDETECTOR', label = _U('weapon_metaldetector'), components = {}}
+}
