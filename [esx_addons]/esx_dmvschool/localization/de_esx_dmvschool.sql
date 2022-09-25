fec4331226c1373755bd95bf0b8fd697d8f9fd0a
ItzCelyrian <ItzCelyrian@gmail.com>
2022-09-26 00:41:57 +0200

    add de.lua

---
+++ [esx_addons]/esx_dmvschool/localization/de_esx_dmvschool.sql
@@ -0,0 +1,6 @@
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('dmv', 'Probeführerschein'),
+	('drive', 'Führerschein'),
+	('drive_bike', 'Motorradschein'),
+	('drive_truck', 'Lastkraftschein')
+;
