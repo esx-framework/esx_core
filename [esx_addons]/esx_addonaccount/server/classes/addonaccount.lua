6e9a8e57c674052870c1dbe2c8ae09c526ba1678
Anton Stjernquist <32514829+antonstjernquist@users.noreply.github.com>
2022-10-03 00:15:40 +0200

    feat: events for updating addon accounts balance

--- [esx_addons]/esx_addonaccount/server/classes/addonaccount.lua
+++ [esx_addons]/esx_addonaccount/server/classes/addonaccount.lua
@@ -1,34 +1,37 @@
 function CreateAddonAccount(name, owner, money)
-	local self = {}
+    local self = {}
 
-	self.name  = name
-	self.owner = owner
-	self.money = money
+    self.name = name
+    self.owner = owner
+    self.money = money
 
-	function self.addMoney(m)
-		self.money = self.money + m
-		self.save()
-	end
+    function self.addMoney(m)
+        self.money = self.money + m
+        self.save()
+        TriggerEvent('esx_addonaccount:addMoney', self.name, m)
+    end
 
-	function self.removeMoney(m)
-		self.money = self.money - m
-		self.save()
-	end
+    function self.removeMoney(m)
+        self.money = self.money - m
+        self.save()
+        TriggerEvent('esx_addonaccount:removeMoney', self.name, m)
+    end
 
-	function self.setMoney(m)
-		self.money = m
-		self.save()
-	end
+    function self.setMoney(m)
+        self.money = m
+        self.save()
+        TriggerEvent('esx_addonaccount:setMoney', self.name, m)
+    end
 
-	function self.save()
-		if self.owner == nil then
-			MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ?', {self.money, self.name})
-		else
-			MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ? AND owner = ?', {self.money, self.name, self.owner})
-		end
-		TriggerEvent('esx_addonaccount:setMoney', self.name, self.money)
-		TriggerClientEvent('esx_addonaccount:setMoney', -1, self.name, self.money)
-	end
+    function self.save()
+        if self.owner == nil then
+            MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ?', {self.money, self.name})
+        else
+            MySQL.update('UPDATE addon_account_data SET money = ? WHERE account_name = ? AND owner = ?',
+                {self.money, self.name, self.owner})
+        end
+        TriggerClientEvent('esx_addonaccount:setMoney', -1, self.name, self.money)
+    end
 
-	return self
+    return self
 end
