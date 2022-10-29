e49e51f99a9b39e84adf14eb7b645bdc7768fb54
Amir_Lynx <60301485+AmirLynx@users.noreply.github.com>
2022-10-29 16:51:45 +0330

    change event to server call back

--- [esx_addons]/esx_lscustom/server/main.lua
+++ [esx_addons]/esx_lscustom/server/main.lua
@@ -1,36 +1,50 @@
 local Vehicles
 
-RegisterServerEvent('esx_lscustom:buyMod')
-AddEventHandler('esx_lscustom:buyMod', function(price)
-	local source = source
-	local xPlayer = ESX.GetPlayerFromId(source)
-	price = tonumber(price)
+ESX.RegisterServerCallback('esx_lscustom:buyMod', function(src, cb, price)
+   local xPlayer = ESX.GetPlayerFromId(src)
+   	if xPlayer then
+		price = tonumber(price)
+		if price then
+			if Config.IsMechanicJobOnly then
+				local societyAccount
 
-	if Config.IsMechanicJobOnly then
-		local societyAccount
-
-		TriggerEvent('esx_addonaccount:getSharedAccount', 'society_mechanic', function(account)
-			societyAccount = account
-		end)
-
-		if price < societyAccount.money then
-			TriggerClientEvent('esx_lscustom:installMod', source)
-			TriggerClientEvent('esx:showNotification', source, TranslateCap('purchased'))
-			societyAccount.removeMoney(price)
+				TriggerEvent('esx_addonaccount:getSharedAccount', 'society_mechanic', function(account)
+					societyAccount = account
+				end)
+				
+				if societyAccount ~= nil then
+					if price < societyAccount.money then
+						TriggerClientEvent('esx:showNotification', source, TranslateCap('purchased'))
+						societyAccount.removeMoney(price)
+						cb(true)
+					else
+						TriggerClientEvent('esx_lscustom:cancelInstallMod', source)
+						TriggerClientEvent('esx:showNotification', source, TranslateCap('not_enough_money'))
+						cb(false)
+					end
+				else
+					TriggerClientEvent('esx_lscustom:cancelInstallMod', source)
+					TriggerClientEvent('esx:showNotification', source, TranslateCap('missing_society'))
+					print("society_mechanic is not register in addonaccount")
+					cb(false)
+				end
+			else
+				if price < xPlayer.getMoney() then
+					TriggerClientEvent('esx:showNotification', source, TranslateCap('purchased'))
+					xPlayer.removeMoney(price, "LSC Purchase")
+					cb(true)
+				else
+					TriggerClientEvent('esx_lscustom:cancelInstallMod', source)
+					TriggerClientEvent('esx:showNotification', source, TranslateCap('not_enough_money'))
+					cb(false)
+				end
+			end
 		else
-			TriggerClientEvent('esx_lscustom:cancelInstallMod', source)
-			TriggerClientEvent('esx:showNotification', source, TranslateCap('not_enough_money'))
+			cb(false)
 		end
-	else
-		if price < xPlayer.getMoney() then
-			TriggerClientEvent('esx_lscustom:installMod', source)
-			TriggerClientEvent('esx:showNotification', source, TranslateCap('purchased'))
-			xPlayer.removeMoney(price, "LSC Purchase")
-		else
-			TriggerClientEvent('esx_lscustom:cancelInstallMod', source)
-			TriggerClientEvent('esx:showNotification', source, TranslateCap('not_enough_money'))
-		end
-	end
+   	else
+		cb(false)
+   	end
 end)
 
 RegisterServerEvent('esx_lscustom:refreshOwnedVehicle')
