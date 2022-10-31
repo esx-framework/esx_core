f2d901cdd85f28fedb6e0c1162d123e4271f6c8b
Kuuzoo <81863846+Kuuzoo@users.noreply.github.com>
2022-10-31 17:14:41 +0100

    Update main.lua

--- [esx_addons]/esx_basicneeds/client/main.lua
+++ [esx_addons]/esx_basicneeds/client/main.lua
@@ -6,7 +6,6 @@ AddEventHandler('esx_basicneeds:resetStatus', function()
 	TriggerEvent('esx_status:set', 'thirst', 500000)
 end)
 
-
 RegisterNetEvent('esx_basicneeds:healPlayer')
 AddEventHandler('esx_basicneeds:healPlayer', function()
 	-- restore hunger & thirst
@@ -27,11 +26,10 @@ AddEventHandler('esx:onPlayerSpawn', function(spawn)
 		TriggerEvent('esx_basicneeds:resetStatus')
 	end
 
-	IsDead = false
+	IsDead = false	
 end)
 
 AddEventHandler('esx_status:loaded', function(status)
-
 	TriggerEvent('esx_status:registerStatus', 'hunger', 1000000, '#CFAD0F', function(status)
 		return Config.Visible
 	end, function(status)
@@ -43,7 +41,6 @@ AddEventHandler('esx_status:loaded', function(status)
 	end, function(status)
 		status.remove(75)
 	end)
-
 end)
 
 AddEventHandler('esx_status:onTick', function(data)
@@ -74,11 +71,18 @@ AddEventHandler('esx_basicneeds:isEating', function(cb)
 	cb(IsAnimated)
 end)
 
-RegisterNetEvent('esx_basicneeds:onEat')
-AddEventHandler('esx_basicneeds:onEat', function(prop_name)
+RegisterNetEvent('esx_basicneeds:onUse')
+AddEventHandler('esx_basicneeds:onUse', function(type, prop_name)
 	if not IsAnimated then
-		prop_name = prop_name or 'prop_cs_burger_01'
+		local anim = {dict = 'mp_player_inteat@burger', name = 'mp_player_int_eat_burger_fp', settings = {8.0, -8, -1, 49, 0, 0, 0, 0}}
 		IsAnimated = true
+		if type == 'food' then
+			prop_name = prop_name or 'prop_cs_burger_01'
+			anim = {dict = 'mp_player_inteat@burger', name = 'mp_player_int_eat_burger_fp', settings = {8.0, -8, -1, 49, 0, 0, 0, 0}}
+		elseif type == 'drink' then
+			prop_name = prop_name or 'prop_ld_flow_bottle'
+			anim = {dict = 'mp_player_intdrink', name = 'loop_bottle', settings = {1.0, -1.0, 2000, 0, 1, true, true, true}}
+		end
 
 		CreateThread(function()
 			local playerPed = PlayerPedId()
@@ -87,9 +91,10 @@ AddEventHandler('esx_basicneeds:onEat', function(prop_name)
 			local boneIndex = GetPedBoneIndex(playerPed, 18905)
 			AttachEntityToEntity(prop, playerPed, boneIndex, 0.12, 0.028, 0.001, 10.0, 175.0, 0.0, true, true, false, true, 1, true)
 
-			ESX.Streaming.RequestAnimDict('mp_player_inteat@burger', function()
-				TaskPlayAnim(playerPed, 'mp_player_inteat@burger', 'mp_player_int_eat_burger_fp', 8.0, -8, -1, 49, 0, 0, 0, 0)
-				RemoveAnimDict('mp_player_inteat@burger')
+			print(json.encode(anim))
+			ESX.Streaming.RequestAnimDict(anim.dict, function()
+				TaskPlayAnim(playerPed, anim.dict, anim.name, anim.settings[1], anim.settings[2], anim.settings[3], anim.settings[4], anim.settings[5], anim.settings[6], anim.settings[7], anim.settings[8])
+				RemoveAnimDict(anim.dict)
 
 				Wait(3000)
 				IsAnimated = false
@@ -97,33 +102,5 @@ AddEventHandler('esx_basicneeds:onEat', function(prop_name)
 				DeleteObject(prop)
 			end)
 		end)
-
-	end
-end)
-
-RegisterNetEvent('esx_basicneeds:onDrink')
-AddEventHandler('esx_basicneeds:onDrink', function(prop_name)
-	if not IsAnimated then
-		prop_name = prop_name or 'prop_ld_flow_bottle'
-		IsAnimated = true
-
-		CreateThread(function()
-			local playerPed = PlayerPedId()
-			local x,y,z = table.unpack(GetEntityCoords(playerPed))
-			local prop = CreateObject(joaat(prop_name), x, y, z + 0.2, true, true, true)
-			local boneIndex = GetPedBoneIndex(playerPed, 18905)
-			AttachEntityToEntity(prop, playerPed, boneIndex, 0.12, 0.028, 0.001, 10.0, 175.0, 0.0, true, true, false, true, 1, true)
-
-			ESX.Streaming.RequestAnimDict('mp_player_intdrink', function()
-				TaskPlayAnim(playerPed, 'mp_player_intdrink', 'loop_bottle', 1.0, -1.0, 2000, 0, 1, true, true, true)
-				RemoveAnimDict('mp_player_intdrink')
-
-				Wait(3000)
-				IsAnimated = false
-				ClearPedSecondaryTask(playerPed)
-				DeleteObject(prop)
-			end)
-		end)
-
 	end
 end)
