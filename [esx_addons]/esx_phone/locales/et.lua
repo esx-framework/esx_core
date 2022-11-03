281fa05dfc79b9319b06f8858df443798bf2b051
walls9919 <blumvaido@gmail.com>
2022-11-04 01:22:36 +0200

    Create et.lua
    Estonian translation for the resource

---
+++ [esx_addons]/esx_phone/locales/et.lua
@@ -0,0 +1,13 @@
+Locales['et'] = {
+  ['new_message'] = 'Sulle tuli uus sõnum: %s',
+  ['press_take_call'] = '%s - Vajuta [E], et vastata kõnele',
+  ['taken_call'] = '%s vastas kõnele',
+  ['gps_position'] = 'Asukoht on märgitud sinu GPSil',
+  ['message_sent'] = 'Sõnum saadetud!',
+  ['cannot_add_self'] = 'Sa ei saa iseennast lisada!',
+  ['number_in_contacts'] = 'See number on juba kontaktide listis.',
+  ['contact_added'] = 'Kontakt lisatud!',
+  ['contact_removed'] = 'Kontakt eemaldatud!',
+  ['number_not_assigned'] = 'Numbrit pole määratud!',
+  ['invalid_number'] = 'See number ei eksisteeri!',
+}
