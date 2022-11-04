48accda123863cdb2500d4be32b4f3bb77c98a35
walls9919 <blumvaido@gmail.com>
2022-11-04 02:00:15 +0200

    Create et.lua
    Estonian translation for the resource

---
+++ [esx_addons]/esx_rpchat/locales/et.lua
@@ -0,0 +1,11 @@
+Locales['et'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'Saada tweet',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'Isiku tegevus',
+  ['me_prefix'] = 'me | %s',
+  ['do_help'] = 'RP informatsioon',
+  ['do_prefix'] = 'do | %s',
+  ['generic_argument_name'] = 'sõnum',
+  ['generic_argument_help'] = 'sõnum',
+}
