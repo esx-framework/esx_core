e615c570b815f4469a3daf9b82ea1c37084d3932
P Gergő <69343477+Rav3n95@users.noreply.github.com>
2022-11-24 23:01:49 +0100

    Fixed Hungarian locale

--- [esx_addons]/esx_allowlist/locales/hu.lua
+++ [esx_addons]/esx_allowlist/locales/hu.lua
@@ -1,8 +1,8 @@
 Locales['hu'] = {
-  ['allowlist_check']     = 'Ellenőrzi, hogy fent vagy-e az engedélyezett listára.',
+  ['allowlist_check']     = 'Ellenőrizzük fent vagy-e a menők listáján.',
   ['not_allowlisted']     = 'Engedélyezettnek kell lenned ahhoz, hogy csatlakozz ehhez a szerverhez!',
-  ['allowlist_empty']     = 'Ehhez a szerverhez nincsenek mentett engedélyezési listák.',
-  ['license_missing']     = 'Hiba: Te Identifier-ed hiányzik!',
-  ['help_allowlist_add']  = 'jétékos felvéltele a allowlist-re',
-  ['help_allowlist_load'] = 'allowlist újratöltése',
+  ['allowlist_empty']     = 'Ehhez a szerverhez nincsenek mentett engedélyezési lista.',
+  ['license_missing']     = 'Hiba: Te azonosítód hiányzik!',
+  ['help_allowlist_add']  = 'játékos felvétele az engedélyezettek listájára',
+  ['help_allowlist_load'] = 'engedélyezési lista újratöltése',
 }
