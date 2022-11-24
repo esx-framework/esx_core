28f5a377d151dd1a26434cd711b77c5367f71b15
GLN <48370836+GLN123@users.noreply.github.com>
2022-11-25 00:01:12 +0100

    esx_lscustom fix

--- [esx_addons]/esx_lscustom/server/main.lua
+++ [esx_addons]/esx_lscustom/server/main.lua
@@ -79,7 +79,7 @@ AddEventHandler('esx_lscustom:refreshOwnedVehicle', function(vehicleProps)
 			local vehicle = json.decode(result.vehicle)
 			if vehicleProps.model == vehicle.model then
 				MySQL.update('UPDATE owned_vehicles SET vehicle = ? WHERE plate = ?', {json.encode(vehicleProps), vehicleProps.plate})
-				Customs[tostring(source)][tostring(vehicleProps.plate)].props = props
+				Customs[tostring(source)][tostring(vehicleProps.plate)].props = vehicleProps
 			else
 				print(('[^3WARNING^7] Player ^5%s^7 Attempted To upgrade with mismatching vehicle model'):format(xPlayer.source))
 			end
