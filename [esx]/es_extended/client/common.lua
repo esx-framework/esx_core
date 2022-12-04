eb983f1206fa14f9e715f393e1d382d083db72c9
Benzo <102178921+Benzo00@users.noreply.github.com>
2022-12-04 04:44:23 +0100

    refactor(es_extended) removing getshared event

--- [esx]/es_extended/client/common.lua
+++ [esx]/es_extended/client/common.lua
@@ -1,9 +1,3 @@
-AddEventHandler('esx:getSharedObject', function(cb)
-  local Invoke = GetInvokingResource()
-  print(('[^3WARNING^7] ^5%s^7 used ^5esx:getSharedObject^7, this method is deprecated and should not be used! Refer to ^5https://docs.esx-framework.org/tutorials/sharedevent^7 for more info!'):format(Invoke))
-  cb(ESX)
-end)
-
 exports('getSharedObject', function()
 	return ESX
 end)
