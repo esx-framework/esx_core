a5a65376ec75758c94ced63ffd2c4385436a37a3
paarbo <35691223+paarbo@users.noreply.github.com>
2022-12-21 13:02:38 +0000

    Update es.lua

--- [esx_addons]/esx_joblisting/locales/es.lua
+++ [esx_addons]/esx_joblisting/locales/es.lua
@@ -2,5 +2,5 @@ Locales['es'] = {
   ['new_job'] = '¡Tienes un trabajo nuevo!',
   ['access_job_center'] = 'Pulsa ~INPUT_PICKUP~ para entrar a la Oficina de Empleo.',
   ['job_center'] = 'Oficina de Empleo',
-	["blip_text"] = "Oficina de Empleo"
+	['blip_text'] = 'Oficina de Empleo'
 }
