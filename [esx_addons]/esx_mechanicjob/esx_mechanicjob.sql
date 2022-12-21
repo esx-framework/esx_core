8be9f3be2c4b25494b4ad437ad61855afa64228c
paarbo <35691223+paarbo@users.noreply.github.com>
2022-12-21 13:10:13 +0000

    Translated default sql to en

--- [esx_addons]/esx_mechanicjob/esx_mechanicjob.sql
+++ [esx_addons]/esx_mechanicjob/esx_mechanicjob.sql
@@ -1,5 +1,3 @@
-
-
 INSERT INTO `addon_account` (name, label, shared) VALUES
 	('society_mechanic', 'Mechanic', 1)
 ;
@@ -17,18 +15,18 @@ INSERT INTO `jobs` (name, label) VALUES
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('mechanic',0,'recrue','Recrue',12,'{}','{}'),
+	('mechanic',0,'recrue','Recruit',12,'{}','{}'),
 	('mechanic',1,'novice','Novice',24,'{}','{}'),
-	('mechanic',2,'experimente','Experimente',36,'{}','{}'),
-	('mechanic',3,'chief',"Chef d\'équipe",48,'{}','{}'),
-	('mechanic',4,'boss','Patron',0,'{}','{}')
+	('mechanic',2,'experimente','Experienced',36,'{}','{}'),
+	('mechanic',3,'chief','Leader',48,'{}','{}'),
+	('mechanic',4,'boss','Boss',0,'{}','{}')
 ;
 
 INSERT INTO `items` (name, label, weight) VALUES
-	('gazbottle', 'bouteille de gaz', 2),
-	('fixtool', 'outils réparation', 2),
-	('carotool', 'outils carosserie', 2),
-	('blowpipe', 'Chalumeaux', 2),
-	('fixkit', 'Kit réparation', 3),
-	('carokit', 'Kit carosserie', 3)
+	('gazbottle', 'Gas Bottle', 2),
+	('fixtool', 'Repair Tools', 2),
+	('carotool', 'Tools', 2),
+	('blowpipe', 'Blowtorch', 2),
+	('fixkit', 'Repair Kit', 3),
+	('carokit', 'Body Kit', 3)
 ;
