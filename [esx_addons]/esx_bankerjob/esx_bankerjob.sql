5db53ab91fe7cbb5afa32be6ec2c48f1b72f1fbb
paarbo <35691223+paarbo@users.noreply.github.com>
2022-12-21 12:43:33 +0000

    Update esx_bankerjob.sql

--- [esx_addons]/esx_bankerjob/esx_bankerjob.sql
+++ [esx_addons]/esx_bankerjob/esx_bankerjob.sql
@@ -1,18 +1,18 @@
 
 
 INSERT INTO `addon_account` (name, label, shared) VALUES
-	('society_banker','Banque',1),
-	('bank_savings','Livret Bleu',0)
+	('society_banker','Bank',1),
+	('bank_savings','Bank Savings',0)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-	('banker','Banquier')
+	('banker','Banker')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('banker',0,'advisor','Conseiller',10,'{}','{}'),
-	('banker',1,'banker','Banquier',20,'{}','{}'),
-	('banker',2,'business_banker',"Banquier d\'affaire",30,'{}','{}'),
+	('banker',0,'advisor','Advisor',10,'{}','{}'),
+	('banker',1,'banker','Banker',20,'{}','{}'),
+	('banker',2,'business_banker','Business Banker',30,'{}','{}'),
 	('banker',3,'trader','Trader',40,'{}','{}'),
-	('banker',4,'boss','Patron',0,'{}','{}')
+	('banker',4,'boss','Lead Banker',0,'{}','{}')
 ;
