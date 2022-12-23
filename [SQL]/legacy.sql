f71dcae72a8d8c92a1aa8d8a6e080d68157630b9
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-12-23 13:18:58 +0100

    fix:(boat license missing)

--- [SQL]/legacy.sql
+++ [SQL]/legacy.sql
@@ -329,7 +329,8 @@ INSERT INTO `licenses` (`type`, `label`) VALUES
 ('drive', 'Drivers License'),
 ('drive_bike', 'Motorcycle License'),
 ('drive_truck', 'Commercial Drivers License'),
-('weed_processing', 'Weed Processing License');
+('weed_processing', 'Weed Processing License'),
+('boat', 'Boat License');
 
 -- --------------------------------------------------------
 
