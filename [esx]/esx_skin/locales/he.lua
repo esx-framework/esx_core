087585216deba7bbf7efdb4d66fc3a5c76a506dd
thefourCraft <COO@ims-network.net>
2022-12-28 23:01:35 +0200

    Hebrew V0.0.1

---
+++ [esx]/esx_skin/locales/he.lua
@@ -0,0 +1,6 @@
+Locales['he'] = {
+    ['skin_menu'] = 'תפריט צבעי עור',
+    ['use_rotate_view'] = 'השתמשו ~INPUT_FRONTEND_LS~ ו ~INPUT_CHARACTER_WHEEL~ בשביל לסובב את דמות.',
+    ['skin'] = 'שינוי צבע עור',
+    ['saveskin'] = 'שמירת צבע עור לקובץ',
+  }
