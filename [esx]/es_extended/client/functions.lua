fad2bef0538eec84c973be8df734d9c263d31c3d
Gellipapa <gellerthorvath0@gmail.com>
2023-01-09 21:30:07 +0100

    refactor RegisterInput

--- [esx]/es_extended/client/functions.lua
+++ [esx]/es_extended/client/functions.lua
@@ -193,13 +193,24 @@ else
 end
 
 
-ESX.RegisterInput = function(command_name, label, input_group, key, on_press, on_release)
-    RegisterCommand(on_release ~= nil and "+" .. command_name or command_name, on_press)
-    Core.Input[command_name] = on_release ~= nil and ESX.HashString("+" .. command_name) or ESX.HashString(command_name)
-    if on_release then
-        RegisterCommand("-" .. command_name, on_release)
-    end
-    RegisterKeyMapping(on_release ~= nil and "+" .. command_name or command_name, label, input_group, key)
+ESX.RegisterInput = function(command_name, label, input_group, key, on_press, on_release, restricted)
+
+	if not command_name or not label or not input_group or not key or not on_press then
+		print("[^1ERROR^7] You can't register input because missing parameters! Current resource: [^2"..GetInvokingResource().."^7]")
+		return
+	end
+
+	on_release = on_release ~= nil and on_release or false
+	restricted = restricted ~= nil and restricted or false
+
+	local commandString = "+".. command_name
+	RegisterCommand(on_release and commandString or command_name, on_press, restricted)
+	Core.Input[command_name] = on_release and ESX.HashString(commandString) or ESX.HashString(command_name)
+	if on_release then
+		commandString = "-" .. command_name
+		RegisterCommand(commandString, on_release)
+	end
+	RegisterKeyMapping(on_release and commandString or command_name, label, input_group, key)
 end
 
 function ESX.TriggerServerCallback(name, cb, ...)
