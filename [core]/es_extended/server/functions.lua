e4f81a75233f1cd2a482bae0f1762414d6b5484e
Csoki <feketetibi71@gmail.com>
2023-01-22 14:35:36 +0100

    refactor(server/functions): ESX.GetPlayers()
    it only stayed for backwards compatibility

--- [core]/es_extended/server/functions.lua
+++ [core]/es_extended/server/functions.lua
@@ -216,15 +216,7 @@ function Core.SavePlayers(cb)
   end
 end
 
-function ESX.GetPlayers()
-  local sources = {}
-
-  for k, v in pairs(ESX.Players) do
-    sources[#sources + 1] = k
-  end
-
-  return sources
-end
+ESX.GetPlayers = GetPlayers
 
 function ESX.GetExtendedPlayers(key, val)
   local xPlayers = {}
