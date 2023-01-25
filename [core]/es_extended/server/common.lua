312c6383ec787a6ba4a9e6ff8b1cdefa622f9fcc
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-01-25 05:14:43 -0500

    tweak(server): reduce code redundancy

--- [core]/es_extended/server/common.lua
+++ [core]/es_extended/server/common.lua
@@ -60,39 +60,12 @@ MySQL.ready(function()
       Wait(0)
     end
   end
-
-  local Jobs = {}
-  local jobs = MySQL.query.await('SELECT * FROM jobs')
-
-  for _, v in ipairs(jobs) do
-    Jobs[v.name] = v
-    Jobs[v.name].grades = {}
+  
+  while not ESX.RefreshJobs do
+    Wait(0)
   end
 
-  local jobGrades = MySQL.query.await('SELECT * FROM job_grades')
-
-  for _, v in ipairs(jobGrades) do
-    if Jobs[v.job_name] then
-      Jobs[v.job_name].grades[tostring(v.grade)] = v
-    else
-      print(('[^3WARNING^7] Ignoring job grades for ^5%s^0 due to missing job'):format(v.job_name))
-    end
-  end
-
-  for _, v in pairs(Jobs) do
-    if ESX.Table.SizeOf(v.grades) == 0 then
-      Jobs[v.name] = nil
-      print(('[^3WARNING^7] Ignoring job ^5%s^0 due to no job grades found'):format(v.name))
-    end
-  end
-
-  if not Jobs then
-    -- Fallback data, if no jobs exist
-    ESX.Jobs['unemployed'] = {label = 'Unemployed',
-                              grades = {['0'] = {grade = 0, label = 'Unemployed', salary = 200, skin_male = {}, skin_female = {}}}}
-  else
-    ESX.Jobs = Jobs
-  end
+  ESX.RefreshJobs()
 
   print('[^2INFO^7] ESX ^5Legacy 1.9.0^0 initialized!')
   StartDBSync()
