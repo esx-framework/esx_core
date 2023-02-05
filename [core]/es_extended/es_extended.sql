1642ad5927859ca5f9e3ce0fa053a09662e3f70d
TheFantomas <thegoodfantomas@gmail.com>
2023-02-05 10:32:50 +0100

    Revert "Fixed"
    This reverts commit 2004303d360df4cc905031cd33b73da9b14a7416.

--- [core]/es_extended/es_extended.sql
+++ [core]/es_extended/es_extended.sql
@@ -15,7 +15,7 @@ CREATE TABLE `users` (
 	`job` VARCHAR(20) NULL DEFAULT 'unemployed',
 	`job_grade` INT NULL DEFAULT 0,
 	`loadout` LONGTEXT NULL DEFAULT NULL,
-	`position` longtext NOT NULL,
+	`position` VARCHAR(255) NULL DEFAULT '{"x":-269.4,"y":-955.3,"z":31.2,"heading":205.8}',
 
 	PRIMARY KEY (`identifier`)
 ) ENGINE=InnoDB;
