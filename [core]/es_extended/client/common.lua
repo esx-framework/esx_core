5b3c0e6784f86512de322d7d95aa0c5701c81513
Naifen || Developer <marv.2005@web.de>
2023-02-20 22:30:43 +0100

    Update sharedevent link

--- [core]/es_extended/client/common.lua
+++ [core]/es_extended/client/common.lua
@@ -8,5 +8,5 @@ end
 
 AddEventHandler("esx:getSharedObject", function()
 	local Invoke = GetInvokingResource()
-	print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://documentation.esx-framework.org/tutorials/sharedevent for how to fix!"):format(Invoke))
+	print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://documentation.esx-framework.org/tutorials/tutorials-esx/sharedevent for how to fix!"):format(Invoke))
 end)
