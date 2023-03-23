74193a073694e7d016a56959a082355bccf56f15
StomperG <goncaloca7@gmail.com>
2023-03-23 23:08:17 +0000

    Portuguese translations added

--- [core]/esx_skin/locales/br.lua
+++
@@ -1,6 +0,0 @@
-Locales['br'] = {
-  ['skin_menu'] = 'Skin Menu',
-  ['use_rotate_view'] = 'Use ~INPUT_FRONTEND_LS~ e ~INPUT_CHARACTER_WHEEL~ para girar a visão.',
-  ['skin'] = 'Trocar',
-  ['saveskin'] = 'Salvar',
-}
