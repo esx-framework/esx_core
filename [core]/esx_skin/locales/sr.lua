891864b67bd4b9210861bcae8837ed909047278f
TheFantomas <117121911+TheFantomas@users.noreply.github.com>
2023-04-02 10:47:30 +0200

    Update sr.lua

--- [core]/esx_skin/locales/sr.lua
+++ [core]/esx_skin/locales/sr.lua
@@ -1,4 +1,4 @@
-Locales['en'] = {
+Locales['sr'] = {
   ['skin_menu'] = 'Skin Meni',
   ['use_rotate_view'] = 'koristi ~INPUT_FRONTEND_LS~ i ~INPUT_CHARACTER_WHEEL~ da rotiras kameru.',
   ['skin'] = 'promeni skin',
