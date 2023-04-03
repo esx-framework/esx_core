9703fc0a7ebb8d39ded0b243a298f583a27ec375
Nemanja <71012671+NeMyOriginal@users.noreply.github.com>
2023-04-04 00:26:49 +0200

    Create sr.js

---
+++ [core]/esx_multicharacter/html/locales/sr.js
@@ -0,0 +1,9 @@
+
+const translate = new Object();
+
+translate.name = "Ime";
+translate.job = "Posao";
+translate.bank = "Banka";
+translate.money = "Novac";
+translate.gender = "Pol";
+translate.dob = "Datum rodjenja";
