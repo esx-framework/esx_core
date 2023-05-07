c501f069756487599768147755101f7801404320
Thekuca <63980591+Thekuca@users.noreply.github.com>
2023-05-07 21:43:52 +0200

    refactor(modules/callback): format code

--- [core]/es_extended/server/modules/callback.lua
+++ [core]/es_extended/server/modules/callback.lua
@@ -6,71 +6,71 @@ local RequestId = 0
 ---@param eventName string
 ---@param callback function
 ESX.RegisterServerCallback = function(eventName, callback)
-  serverCallbacks[eventName] = callback
+	serverCallbacks[eventName] = callback
 end
 
 RegisterNetEvent('esx:triggerServerCallback', function(eventName, requestId, invoker, ...)
-  if not serverCallbacks[eventName] then
-    return print(('[^1ERROR^7] Server Callback not registered, name: ^5%s^7, invoker resource: ^5%s^7'):format(eventName, invoker))
-  end
+	if not serverCallbacks[eventName] then
+		return print(('[^1ERROR^7] Server Callback not registered, name: ^5%s^7, invoker resource: ^5%s^7'):format(eventName, invoker))
+	end
 
-  local source = source
+	local source = source
 
-  serverCallbacks[eventName](source, function(...)
-    TriggerClientEvent('esx:serverCallback', source, requestId, invoker, ...)
-  end, ...)
+	serverCallbacks[eventName](source, function(...)
+		TriggerClientEvent('esx:serverCallback', source, requestId, invoker, ...)
+	end, ...)
 end)
 
 ESX.TriggerClientCallback = setmetatable({
-  ---@param player number?
-  ---@param eventName string
-  ---@param ... any
-  Await = function(player, eventName, ...)
-    player = tonumber(player)
+	---@param player number?
+	---@param eventName string
+	---@param ... any
+	Await = function(player, eventName, ...)
+		player = tonumber(player)
 
-    assert(player, 'Argument 1 `player` is not a number!')
-    assert(type(eventName) == 'string', 'Argument 2 `eventName` is not a string!')
+		assert(player, 'Argument 1 `player` is not a number!')
+		assert(type(eventName) == 'string', 'Argument 2 `eventName` is not a string!')
 
-    local p = promise.new()
+		local p = promise.new()
 
-    clientRequests[RequestId] = p
+		clientRequests[RequestId] = p
 
-    TriggerClientEvent('esx:triggerClientCallback', player, eventName, RequestId, GetInvokingResource() or "unknown", ...)
+		TriggerClientEvent('esx:triggerClientCallback', player, eventName, RequestId, GetInvokingResource() or "unknown", ...)
 
-    RequestId = RequestId + 1
+		RequestId = RequestId + 1
 
-    return Citizen.Await(p)
-  end
+		return Citizen.Await(p)
+	end
 }, {
-  ---@param player number? playerId
-  ---@param eventName string
-  ---@param callback function
-  ---@param ... any
-  __call = function(_, player, eventName, callback, ...)
-    player = tonumber(player)
+	---@param player number? playerId
+	---@param eventName string
+	---@param callback function
+	---@param ... any
+	__call = function(_, player, eventName, callback, ...)
+		player = tonumber(player)
 
-    assert(player, 'Argument 1 `player` is not a number!')
-    assert(type(eventName) == 'string', 'Argument 2 `eventName` is not a string!')
-    assert(type(callback) == 'function', 'Argument 3 `callback` is not a function!')
+		assert(player, 'Argument 1 `player` is not a number!')
+		assert(type(eventName) == 'string', 'Argument 2 `eventName` is not a string!')
+		assert(type(callback) == 'function', 'Argument 3 `callback` is not a function!')
 
-    clientRequests[RequestId] = callback
+		clientRequests[RequestId] = callback
 
-    TriggerClientEvent('esx:triggerClientCallback', player, eventName, RequestId, GetInvokingResource() or "unknown", ...)
+		TriggerClientEvent('esx:triggerClientCallback', player, eventName, RequestId, GetInvokingResource() or "unknown", ...)
 
-    RequestId = RequestId + 1
-  end
+		RequestId = RequestId + 1
+	end
 })
 
 RegisterNetEvent('esx:clientCallback', function(requestId, invoker, ...)
-  if not clientRequests[requestId] then
-    return print(('[^1ERROR^7] Client Callback with requestId ^5%s^7 Was Called by ^5%s^7 but does not exist.'):format(requestId, invoker))
-  end
+	if not clientRequests[requestId] then
+		return print(('[^1ERROR^7] Client Callback with requestId ^5%s^7 Was Called by ^5%s^7 but does not exist.'):format(requestId, invoker))
+	end
 
-  if type(clientRequests[requestId]) == 'table' then
-    clientRequests[requestId]:resolve(...)
-    return
-  end
+	if type(clientRequests[requestId]) == 'table' then
+		clientRequests[requestId]:resolve(...)
+		return
+	end
 
-  clientRequests[requestId](...)
-  clientRequests[requestId] = nil
+	clientRequests[requestId](...)
+	clientRequests[requestId] = nil
 end)
