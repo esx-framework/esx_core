e945580e1e216938ad0a96db94474c839cf3b068
thefourCraft <me@thefourcraft.com>
2023-07-15 21:55:42 +0300

    fix format

---
+++ [core]/esx_skin/locales/he.lua
@@ -0,0 +1,7 @@
+Locales["he"] = {
+    ["skin_menu"] = "Skin Menu",
+    ["use_rotate_view"] = "use ~INPUT_FRONTEND_LS~ and ~INPUT_CHARACTER_WHEEL~ to rotate the view.",
+    ["skin"] = "change skin",
+    ["saveskin"] = "save skin to a file",
+  }
+  
