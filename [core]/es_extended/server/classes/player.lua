e67e7c1f8393175ba3b960342378429dc29fc6f4
Canis_Lupus <56067968+CanysLypys@users.noreply.github.com>
2023-07-27 12:54:36 +0200

    improvement: Using RPC Native

--- [core]/es_extended/server/classes/player.lua
+++ [core]/es_extended/server/classes/player.lua
@@ -504,7 +504,7 @@ function CreateExtendedPlayer(playerId, identifier, group, accounts, inventory, 
 						end
 					end
 
-					self.triggerEvent('esx:removeWeaponComponent', weaponName, weaponComponent)
+                    RemoveWeaponComponent(GetPlayerPed(self.source), GetHashKey(weaponName), component)
 					self.triggerEvent('esx:removeInventoryItem', component.label, false, true)
 				end
 			end
