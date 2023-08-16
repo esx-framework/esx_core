5e9a7d0860a20774c0966b2777786123db9b0fa4
thefourCraft <me@thefourcraft.com>
2023-08-17 00:19:01 +0300

    update last files to hebrew and fix typos

--- [core]/esx_skin/locales/he.lua
+++ [core]/esx_skin/locales/he.lua
@@ -1,7 +1,6 @@
 Locales["he"] = {
-    ["skin_menu"] = "Skin Menu",
-    ["use_rotate_view"] = "use ~INPUT_FRONTEND_LS~ and ~INPUT_CHARACTER_WHEEL~ to rotate the view.",
-    ["skin"] = "change skin",
-    ["saveskin"] = "save skin to a file",
-  }
-  
+  ["skin_menu"] = "תפריט עור",
+  ["use_rotate_view"] = "השתמש ~INPUT_FRONTEND_LS~ ו ~INPUT_CHARACTER_WHEEL~ כדי לסובב את התצוגה.",
+  ["skin"] = "שנה עור",
+  ["saveskin"] = "שמור עור לקובץ",
+}
