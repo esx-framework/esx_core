2993fa7d905c0b4ab1ae83f509d2e8585397abef
Ilias Rbayti <63159154+Kenshiin13@users.noreply.github.com>
2023-08-18 02:10:32 +0200

    Added CheckDuplicateLicense Config functionality

--- [core]/es_extended/config.lua
+++ [core]/es_extended/config.lua
@@ -32,7 +32,7 @@ Config.AdminGroups = {
 	['admin'] = true
 }
 
-
+Config.CheckDuplicateLicense     = true      -- Allows duplicate identifiers for development purposes if set to false. Should be set to true for production!
 Config.EnablePaycheck            = true      -- enable paycheck
 Config.LogPaycheck               = false     -- Logs paychecks to a nominated Discord channel via webhook (default is false)
 Config.EnableSocietyPayouts      = false     -- pay from the society account that the player is employed at? Requirement: esx_society
--- [core]/es_extended/server/main.lua
+++ [core]/es_extended/server/main.lua
@@ -45,24 +45,33 @@ else
 end
 
 function onPlayerJoined(playerId)
-	local identifier = ESX.GetIdentifier(playerId)
-	if identifier then
-		if ESX.GetPlayerFromIdentifier(identifier) then
-			DropPlayer(playerId,
-				('there was an error loading your character!\nError code: identifier-active-ingame\n\nThis error is caused by a player on this server who has the same identifier as you have. Make sure you are not playing on the same Rockstar account.\n\nYour Rockstar identifier: %s'):format(
-					identifier))
-		else
-			local result = MySQL.scalar.await('SELECT 1 FROM users WHERE identifier = ?', { identifier })
-			if result then
-				loadESXPlayer(identifier, playerId, false)
-			else
-				createESXPlayer(identifier, playerId)
-			end
-		end
-	else
-		DropPlayer(playerId,
-			'there was an error loading your character!\nError code: identifier-missing-ingame\n\nThe cause of this error is not known, your identifier could not be found. Please come back later or report this problem to the server administration team.')
-	end
+    local function handleESXPlayer(identifier)
+        local result = MySQL.scalar.await('SELECT 1 FROM users WHERE identifier = ?', {identifier})
+        if result then
+            loadESXPlayer(identifier, playerId, false)
+        else
+            createESXPlayer(identifier, playerId)
+        end
+    end
+
+    local identifier = ESX.GetIdentifier(playerId)
+    if identifier then
+        if ESX.GetPlayerFromIdentifier(identifier) then
+            if (Config.CheckDuplicateLicense) then
+                DropPlayer(playerId,
+                    ('there was an error loading your character!\nError code: identifier-active-ingame\n\nThis error is caused by a player on this server who has the same identifier as you have. Make sure you are not playing on the same Rockstar account.\n\nYour Rockstar identifier: %s'):format(
+                        identifier))
+            else
+                identifier = string.format("cl2:%s", identifier)
+                handleESXPlayer(identifier)
+            end
+        else
+            handleESXPlayer(identifier)
+        end
+    else
+        DropPlayer(playerId,
+            'there was an error loading your character!\nError code: identifier-missing-ingame\n\nThe cause of this error is not known, your identifier could not be found. Please come back later or report this problem to the server administration team.')
+    end
 end
 
 function createESXPlayer(identifier, playerId, data)
@@ -105,7 +114,7 @@ if not Config.Multichar then
 		end
 
 		if identifier then
-			if ESX.GetPlayerFromIdentifier(identifier) then
+			if Config.CheckDuplicateLicense and ESX.GetPlayerFromIdentifier(identifier) then
 				return deferrals.done(
 					('[ESX] There was an error loading your character!\nError code: identifier-active\n\nThis error is caused by a player on this server who has the same identifier as you have. Make sure you are not playing on the same account.\n\nYour identifier: %s'):format(
 						identifier))
