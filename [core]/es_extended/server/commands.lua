6f51cd5e9fe6524b4f5f3eae638e112559006378
Dominic Stanfford <64571534+Manta95@users.noreply.github.com>
2023-08-24 02:00:00 +0200

    Update commands.lua
    Added a check if spawned vehicle isn't on all wheels, then it rotates

--- [core]/es_extended/server/commands.lua
+++ [core]/es_extended/server/commands.lua
@@ -95,6 +95,11 @@ ESX.RegisterCommand('car', 'admin', function(xPlayer, args, showError)
 			if GetVehiclePedIsIn(playerPed, false) ~= vehicle then
 				showError('[^1ERROR^7] The player could not be seated in the vehicle')
 			end
+            if not IsVehicleOnAllWheels(vehicle) then
+				local carCoords = GetEntityRotation(vehicle, 2)
+				Citizen.Wait(500)
+				SetEntityRotation(vehicle, carCoords[1], 0, carCoords[3], 2, true)
+			end
 		end
 	end)
 end, false, {
