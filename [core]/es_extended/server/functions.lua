467590d56ef2aa3e064943a10d1c335d486d2925
Gellipapa <gellerthorvath0@gmail.com>
2023-09-04 22:40:27 +0200

    :rotating_light: Solving backwards compatibility

--- [core]/es_extended/server/functions.lua
+++ [core]/es_extended/server/functions.lua
@@ -241,8 +241,6 @@ local function checkTable(key, val, player, xPlayers)
 end
 
 function ESX.GetExtendedPlayers(key, val)
-	if not key then return ESX.Players end
-
 	local xPlayers = {}
 	if type(val) == "table" then
 		for _, v in pairs(ESX.Players) do
@@ -250,7 +248,11 @@ function ESX.GetExtendedPlayers(key, val)
 		end
 	else
 		for _, v in pairs(ESX.Players) do
-			if (key == 'job' and v.job.name == val) or v[key] == val then
+			if key then
+				if (key == 'job' and v.job.name == val) or v[key] == val then
+					xPlayers[#xPlayers + 1] = v
+				end
+			else
 				xPlayers[#xPlayers + 1] = v
 			end
 		end
