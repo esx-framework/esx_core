3d6b79ce6376515f268cfe7a6b01c78523a4cc0d
Dannezzzan <123192307+Dannezzzan@users.noreply.github.com>
2023-10-08 00:53:37 +0200

    Create sv.lua
    Swedish esx_skin translation

---
+++ [core]/esx_skin/locales/sv.lua
@@ -0,0 +1,7 @@
+Locales["en"] = {
+    ["skin_menu"] = "Skin Meny",
+    ["use_rotate_view"] = "Använd ~INPUT_FRONTEND_LS~ och ~INPUT_CHARACTER_WHEEL~ för att rotera.",
+    ["skin"] = "Ända utseende",
+    ["saveskin"] = "Spara",
+  }
+  
