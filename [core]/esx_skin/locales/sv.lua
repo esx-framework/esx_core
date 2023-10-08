43f2834980d2fd2e13484cf258a72e72be96d37a
Arctos2win <116841243+Arctos2win@users.noreply.github.com>
2023-10-08 13:10:06 +0200

    en to sv

--- [core]/esx_skin/locales/sv.lua
+++ [core]/esx_skin/locales/sv.lua
@@ -1,4 +1,4 @@
-Locales["en"] = {
+Locales["sv"] = {
     ["skin_menu"] = "Skin Meny",
     ["use_rotate_view"] = "Använd ~INPUT_FRONTEND_LS~ och ~INPUT_CHARACTER_WHEEL~ för att rotera.",
     ["skin"] = "Ända utseende",
