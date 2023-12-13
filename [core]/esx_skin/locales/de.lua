8dab60e73266c667831802be98ecd1cb9b7ce85d
JustZerooo <pohlmann_1987@web.de>
2023-12-13 21:05:02 +0100

    Adding German Language Support
    Signed-off-by: JustZerooo <pohlmann_1987@web.de>

---
+++ [core]/esx_skin/locales/de.lua
@@ -0,0 +1,6 @@
+Locales["de"] = {
+    ["skin_menu"] = "Skin Menu",
+    ["use_rotate_view"] = "Verwende ~INPUT_FRONTEND_LS~ und ~INPUT_CHARACTER_WHEEL~, um die Ansicht zu drehen.",
+    ["skin"] = "Skin ändern",
+    ["saveskin"] = "Skin in einer Datei speichern",
+  }
