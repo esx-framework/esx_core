a44452185ed30c68235e51ea5e24d21c4fac864a
wx <woox@duck.com>
2024-02-19 20:45:30 +0100

    revert(identity): remove redesigned parts

--- [core]/esx_identity/config.lua
+++ [core]/esx_identity/config.lua
@@ -13,7 +13,7 @@ Config.UseDeferrals = false
 Config.DateFormat = "DD/MM/YYYY"
 
 -- These values are for the second input validation in server/main.lua
-Config.MaxNameLength = 20 -- Max Name Length.
+Config.MaxNameLength = 32 -- Max Name Length.
 Config.MinHeight = 120 -- 120 cm lowest height
 Config.MaxHeight = 220 -- 220 cm max height.
 Config.LowestYear = 1900 -- 112 years old is the oldest you can be.
