7e0faeab4252742923d3bfc67267017cb20f644b
KinGesteR <139977276+AmKinGesteR@users.noreply.github.com>
2024-04-02 17:21:27 +0330

    Update legacy.sql

--- [SQL]/legacy.sql
+++ [SQL]/legacy.sql
@@ -995,7 +995,7 @@ INSERT INTO `jobs` (name, label) VALUES
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
 	('banker',0,'advisor','Consultant',10,'{}','{}'),
 	('banker',1,'banker','Banker',20,'{}','{}'),
-	('banker',2,'business_banker',"Investment banker",30,'{}','{}'),
+	('banker',2,'business_banker','Investment banker',30,'{}','{}'),
 	('banker',3,'trader','Broker',40,'{}','{}'),
 	('banker',4,'boss','Boss',0,'{}','{}')
 ;
@@ -1016,3 +1016,10 @@ CREATE TABLE IF NOT EXISTS `banking` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `users` ADD COLUMN `pincode` INT NULL;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES  
+('banker',0,'advisor','Consultant',10,'{}','{}'),  
+('banker',1,'banker','Banker',20,'{}','{}'),  
+('banker',2,'business_banker','Investment banker',30,'{}','{}'),  
+('banker',3,'trader','Broker',40,'{}','{}'),  
+('banker',4,'boss','Boss',0,'{}','{}');
