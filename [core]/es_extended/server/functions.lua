c85c2f57a287417621828c017cfa3ad197610145
Kenshin13 <63159154+Kenshiin13@users.noreply.github.com>
2024-04-04 23:48:02 +0400

    Added function ESX.TriggerClientEvent(

--- [core]/es_extended/server/functions.lua
+++ [core]/es_extended/server/functions.lua
@@ -4,6 +4,24 @@ function ESX.Trace(msg)
     end
 end
 
+--- Triggers an event for one or more clients.
+---@param eventName string The name of the event to trigger.
+---@param playerIds table|number If a number, represents a single player ID. If a table, represents an array of player IDs.
+---@param ... any Additional arguments to pass to the event handler.
+function ESX.TriggerClientEvent(eventName, playerIds, ...)
+    if type(playerIds) == "number" then
+        TriggerClientEvent(eventName, playerIds, ...)
+        return
+    end
+
+    local Payload = msgpack.pack_args(...)
+    local payloadLength = #Payload
+
+    for i = 1, #playerIds do
+        TriggerClientEventInternal(eventName, playerIds[i], Payload, payloadLength)
+    end
+end
+
 function ESX.RegisterCommand(name, group, cb, allowConsole, suggestion)
     if type(name) == "table" then
         for _, v in ipairs(name) do
