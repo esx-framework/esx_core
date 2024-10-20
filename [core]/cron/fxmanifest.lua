6d86afac68d771abccd830a53716c7023aea6204
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-20 19:00:39 +0000

    chore: bump manifest version to 1.10.10

--- [core]/cron/fxmanifest.lua
+++ [core]/cron/fxmanifest.lua
@@ -4,6 +4,6 @@ game 'gta5'
 author 'ESX-Framework'
 description 'Allows resources to Run tasks at specific intervals.'
 lua54 'yes'
-version '1.10.9'
+version '1.10.10'
 
 server_script 'server/main.lua'
