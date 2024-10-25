4992778db8229842978809708aed69ed00974806
Kasey FItton <kaseyfitton@hotmail.co.uk>
2024-10-25 13:07:19 +0100

    experimental: New base Resource - ESX Chat theme

---
+++ [core]/esx_chat_theme/fxmanifest.lua
@@ -0,0 +1,17 @@
+version '1.10.10'
+author 'ESX-Framework'
+description 'A ESX Stylised theme for the chat resource.'
+
+file 'style.css'
+file 'shadow.js'
+
+chat_theme 'esx' {
+    styleSheet = 'style.css',
+    script = 'shadow.js',
+    msgTemplates = {
+        default = '<b>{0}</b><span>{1}</span>'
+    }
+}
+
+game 'common'
+fx_version 'adamant'
