b25e11849a73a5a47bb29a7adbb52ffef07fdc5b
Kasey FItton <kaseyfitton@hotmail.co.uk>
2024-11-13 21:42:16 +0000

    tweak(es_extended/shared): correct values

--- [core]/es_extended/shared/config/main.lua
+++ [core]/es_extended/shared/config/main.lua
@@ -39,8 +39,12 @@ Config.EnablePaycheck = true -- enable paycheck
 Config.LogPaycheck = false -- Logs paychecks to a nominated Discord channel via webhook (default is false)
 Config.EnableSocietyPayouts = false -- pay from the society account that the player is employed at? Requirement: esx_society
 Config.MaxWeight = 24 -- the max inventory weight without a backpack
+Config.SaveInterval = 10 * 60 * 1000 -- how often to save player data in milliseconds (default is 10 minutes)
 Config.PaycheckInterval = 7 * 60000 -- how often to receive paychecks in milliseconds
 Config.EnableDebug = false -- Use Debug options?
+
+Config.MapName = "San Andreas"
+Config.GameType = "ESX-Framework"
 
 Config.DefaultJobDuty = true -- A players default duty status when changing jobs
 
