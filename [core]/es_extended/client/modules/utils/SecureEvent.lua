b3ee034fce729ce8d6499f853c60e5672e9283cd
Kasey FItton <kaseyfitton@hotmail.co.uk>
2024-11-13 17:34:06 +0000

    tweak(es_extended/client): dont format error

--- [core]/es_extended/client/modules/utils/SecureEvent.lua
+++ [core]/es_extended/client/modules/utils/SecureEvent.lua
@@ -21,7 +21,7 @@ function ESX.SecureNetEvent(name, func)
         local success, result = pcall(func, ...)
         if not success then
             result = result or ("Unknown error occurred in event ^5%s^1"):format(name)
-            error(("%s"):format(result))
+            error(result)
         end
     end)
 
