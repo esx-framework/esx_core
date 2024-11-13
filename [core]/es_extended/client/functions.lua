8777535e73e9ce3a2012ed44450aee9b5d21329d
Kasey FItton <kaseyfitton@hotmail.co.uk>
2024-11-13 14:58:49 +0000

    tweak(es_extended/client/functions): more validation in SecureNetEvent

--- [core]/es_extended/client/functions.lua
+++ [core]/es_extended/client/functions.lua
@@ -13,15 +13,16 @@ function ESX.SecureNetEvent(name, func)
     end
 
     local event = RegisterNetEvent(name, function(...)
-        if source == '' then
-            return
-        end
+        local source = source
+        if source ~= 65535 then return end
 
         local success, result = pcall(func, ...)
         if not success then
+            result = result or ("Unknown error occurred in event ^5%s^1"):format(name)
             error(("%s"):format(result))
         end
     end)
+
     local eventIndex = #Core.Events[invokingResource] + 1
     Core.Events[invokingResource][eventIndex] = event
 end
