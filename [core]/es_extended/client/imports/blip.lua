f97d479f1e1c67c4f0619a1106ef7d29ef8c1fa0
btwlouis <56355239+btwlouis@users.noreply.github.com>
2024-11-19 15:42:01 +0000

    fix(blips): typo

--- [core]/es_extended/client/imports/blip.lua
+++ [core]/es_extended/client/imports/blip.lua
@@ -1,24 +1,25 @@
 Blip = ESX.Class()
 
 function Blip:constructor(properties)
-    self.coords = type(properties.coords) == "vector3" and properties.coords or vector3(properties.coords.x, properties.coords.y, properties.coords.z)
+    self.coords = type(properties.coords) == "vector3" and properties.coords or
+        vector3(properties.coords.x, properties.coords.y, properties.coords.z)
     self.sprite = type(properties.sprite) == "number" and properties.sprite or 1
     self.colour = type(properties.colour) == "number" and properties.colour or 0
     self.label = type(properties.label) == "string" and properties.label or "Unknown"
     self.scale = type(properties.scale) == "number" and properties.scale or 1.0
     self.display = type(properties.display) == "number" and properties.display or 4
-    self.shortRange = type(properties.shortRange) == "boolean" and properties.shortRange or true 
+    self.shortRange = type(properties.shortRange) == "boolean" and properties.shortRange or true
     self.resource = GetInvokingResource() or GetCurrentResourceName() or "es_extended"
 
-    local handle = ESX.CreateBlipInternal(coords, sprite, colour, label, scale, display, shortRange, resource)
-    self.handle = handle 
-
+    local handle = ESX.CreateBlipInternal(self.coords, self.sprite, self.colour, self.label, self.scale, self.display,
+        self.shortRange, self.resource)
+    self.handle = handle
     return handle
 end
 
 function Blip:setCoords(coords)
     self.coords = type(coords) == "vector3" and coords or vector3(coords.x, coords.y, coords.z)
-    
+
     ESX.SetBlipCoords(self.handle, self.coords)
 end
 
@@ -53,7 +54,7 @@ function Blip:setDisplay(display)
 end
 
 function Blip:setShortRange(shortRange)
-    self.shortRange = type(shortRange) == "boolean" and shortRange or true 
+    self.shortRange = type(shortRange) == "boolean" and shortRange or true
 
     ESX.SetBlipShortRange(self.handle, self.shortRange)
 end
