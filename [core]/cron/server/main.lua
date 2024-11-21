6ca21fcae6d190e98a324856c92b7226e357d585
RoadToSix <92928205+roadtosixx@users.noreply.github.com>
2024-11-21 21:33:52 +0100

    fix(cron): validate arguments in RunAt and improve timestamp handling

--- [core]/cron/server/main.lua
+++ [core]/cron/server/main.lua
@@ -1,7 +1,15 @@
 local Jobs = {}
 local LastTime = nil
 
+---@param h number Hour (0-23)
+---@param m number Minute (0-59)
+---@param cb function Callback to execute
 function RunAt(h, m, cb)
+    if type(h) ~= "number" or type(m) ~= "number" or type(cb) ~= "function" then
+        print("[cron] Invalid arguments to RunAt. Expected (number, number, function).")
+        return
+    end
+
     Jobs[#Jobs + 1] = {
         h = h,
         m = m,
@@ -9,19 +17,25 @@ function RunAt(h, m, cb)
     }
 end
 
+---@return number Current timestamp
 function GetUnixTimestamp()
     return os.time()
 end
 
+---@param time number The current Unix timestamp
 function OnTime(time)
+    local currentDay = os.date("*t", time).day
+    local currentMonth = os.date("*t", time).month
+    local currentYear = os.date("*t", time).year
+
     for i = 1, #Jobs, 1 do
         local scheduledTimestamp = os.time({
             hour = Jobs[i].h,
             min = Jobs[i].m,
             sec = 0, -- Assuming tasks run at the start of the minute
-            day = os.date("%d", time),
-            month = os.date("%m", time),
-            year = os.date("%Y", time),
+            year = currentYear,
+            month = currentMonth,
+            day = currentDay
         })
 
         if time >= scheduledTimestamp and (not LastTime or LastTime < scheduledTimestamp) then
@@ -32,18 +46,17 @@ function OnTime(time)
 end
 
 function Tick()
-    local time = GetUnixTimestamp()
+    local currentTime = GetUnixTimestamp()
 
-    if not LastTime or os.date("%M", time) ~= os.date("%M", LastTime) then
-        OnTime(time)
-        LastTime = time
+    if not LastTime or os.date("%M", currentTime) ~= os.date("%M", LastTime) then
+        OnTime(currentTime)
+        LastTime = currentTime
     end
 
     SetTimeout(60000, Tick)
 end
 
 LastTime = GetUnixTimestamp()
-
 Tick()
 
 AddEventHandler("cron:runAt", function(h, m, cb)
