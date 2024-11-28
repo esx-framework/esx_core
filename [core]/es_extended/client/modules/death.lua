114c337d6c80766905704765b32e3b01ff2edc1a
Ilias Rbayti <63159154+Kenshiin13@users.noreply.github.com>
2024-11-28 18:51:26 +0100

    refactor(es_extended/client/modules/death): more appropriate function name

--- [core]/es_extended/client/modules/death.lua
+++ [core]/es_extended/client/modules/death.lua
@@ -43,7 +43,7 @@ function Death:Natural()
     TriggerServerEvent("esx:onPlayerDeath", data)
 end
 
-function Death:Damaged()
+function Death:Died()
     self.killerEntity = GetPedSourceOfDeath(ESX.PlayerData.ped)
     self.deathCause = GetPedCauseOfDeath(ESX.PlayerData.ped)
     self.killerId = NetworkGetPlayerIndexFromPed(self.killerEntity)
@@ -64,7 +64,7 @@ AddEventHandler("esx:onPlayerSpawn", function()
     Citizen.CreateThreadNow(function()
         while not ESX.PlayerData.dead do
             if IsPedDeadOrDying(ESX.PlayerData.ped, true) or IsPedFatallyInjured(ESX.PlayerData.ped) then
-                Death:Damaged()
+                Death:Died()
                 break
             end
             Citizen.Wait(250)
