7b30c710c955be7d93b14d587003bfb2a133fd87
Kasey FItton <kaseyfitton@hotmail.co.uk>
2024-12-15 23:24:28 +0000

    refactor(es_extended/shared): move weapons into its own module

--- [core]/es_extended/shared/functions.lua
+++ [core]/es_extended/shared/functions.lua
@@ -10,16 +10,6 @@ for i = 97, 122 do
     ESX.Charset[#ESX.Charset + 1] = string.char(i)
 end
 
-local weaponsByName = {}
-local weaponsByHash = {}
-
-CreateThread(function()
-    for index, weapon in pairs(Config.Weapons) do
-        weaponsByName[weapon.name] = index
-        weaponsByHash[joaat(weapon.name)] = weapon
-    end
-end)
-
 ---@param length number
 ---@return string
 function ESX.GetRandomString(length)
@@ -31,58 +21,6 @@ end
 ---@return table
 function ESX.GetConfig()
     return Config
-end
-
----@param weaponName string
----@return number, table
-function ESX.GetWeapon(weaponName)
-    weaponName = string.upper(weaponName)
-
-    assert(weaponsByName[weaponName], "Invalid weapon name!")
-
-    local index = weaponsByName[weaponName]
-    return index, Config.Weapons[index]
-end
-
----@param weaponHash number
----@return table
-function ESX.GetWeaponFromHash(weaponHash)
-    weaponHash = type(weaponHash) == "string" and joaat(weaponHash) or weaponHash
-
-    return weaponsByHash[weaponHash]
-end
-
----@param byHash boolean
----@return table
-function ESX.GetWeaponList(byHash)
-    return byHash and weaponsByHash or Config.Weapons
-end
-
----@param weaponName string
----@return string
-function ESX.GetWeaponLabel(weaponName)
-    weaponName = string.upper(weaponName)
-
-    assert(weaponsByName[weaponName], "Invalid weapon name!")
-
-    local index = weaponsByName[weaponName]
-    return Config.Weapons[index].label or ""
-end
-
----@param weaponName string
----@param weaponComponent string
----@return table | nil
-function ESX.GetWeaponComponent(weaponName, weaponComponent)
-    weaponName = string.upper(weaponName)
-
-    assert(weaponsByName[weaponName], "Invalid weapon name!")
-    local weapon = Config.Weapons[weaponsByName[weaponName]]
-
-    for _, component in ipairs(weapon.components) do
-        if component.name == weaponComponent then
-            return component
-        end
-    end
 end
 
 ---@param table table
