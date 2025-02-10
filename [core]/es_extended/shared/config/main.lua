707431f42b3154a515481b8458f2e8e08d3d6299
Kasey FItton <kaseyfitton@hotmail.co.uk>
2025-02-10 14:50:58 +0000

    tweak(es_extended/shared/config/main): remove more configs

--- [core]/es_extended/shared/config/main.lua
+++ [core]/es_extended/shared/config/main.lua
@@ -41,14 +41,10 @@ Config.AdminGroups = {
 
 Config.EnableDebug = false -- Use Debug options?
 
-Config.DefaultJobDuty = true -- A players default duty status when changing jobs
-Config.OffDutyPaycheckMultiplier = 0.5 -- The multiplier for off duty paychecks. 0.5 = 50% of the on duty paycheck
-
 Config.Multichar = GetResourceState("esx_multicharacter") ~= "missing"
 Config.Identity = true -- Select a character identity data before they have loaded in (this happens by default with multichar)
 Config.DistanceGive = 4.0 -- Max distance when giving items, weapons etc.
 
-Config.AdminLogging = false -- Logs the usage of certain commands by those with group.admin ace permissions (default is false)
 
 -------------------------------------
 -- DO NOT CHANGE BELOW THIS LINE !!!
