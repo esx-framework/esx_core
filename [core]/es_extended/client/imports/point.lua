51b04390da5a52622ed1bea8c1e6a62d01d118f7
Sergio <89666307+feelfreetofee@users.noreply.github.com>
2025-02-13 18:23:37 +0100

    feat(es_extended/client/imports/point.lua): Merge module into imports
    now the module is exclusively imported

--- [core]/es_extended/client/imports/point.lua
+++ [core]/es_extended/client/imports/point.lua
@@ -1,53 +1,76 @@
 local Point = ESX.Class()
 
-local nearby, loop = {}
+local points, nearbyPoints, mainThread, nearbyThread = {}, {}, false, false
+
+local function UnloadPoint(handle, point)
+    nearbyPoints[handle] = nil
+    if point.leave then
+        point:leave()
+    end
+    if #nearbyPoints == 0 then
+        nearbyThread = false
+    end
+end
 
 function Point:constructor(properties)
-	self.coords = properties.coords
-	self.hidden = properties.hidden
-	self.enter = properties.enter
-	self.leave = properties.leave
-	self.inside = properties.inside
-	self.handle = ESX.CreatePointInternal(properties.coords, properties.distance, properties.hidden, function()
-		nearby[self.handle] = self
-		if self.enter then
-			self:enter()
-		end
-		if not loop then
-			loop = true
-			CreateThread(function()
-				while loop do
-					local coords = GetEntityCoords(ESX.PlayerData.ped)
-					for handle, point in pairs(nearby) do
-						if point.inside then
-							point:inside(#(coords - point.coords))
-						end
-					end
-					Wait()
-				end
-			end)
-		end
-	end, function()
-		nearby[self.handle] = nil
-		if self.leave then
-			self:leave()
-		end
-		if #nearby == 0 then
-			loop = false
-		end
-	end)
+    self.coords = properties.coords
+    self.distance = properties.distance
+    self.hidden = properties.hidden
+    self.enter = properties.enter
+    self.leave = properties.leave
+    self.inside = properties.inside
+    self.handle = table.sizeOf(points) + 1
+    points[self.handle] = self
+    if not mainThread then
+        mainThread = true
+        CreateThread(function()
+            while mainThread do Wait(500)
+                local coords = PlayerObject:GetCoords()
+                for handle, point in pairs(points) do
+                    if not point.hidden and #(coords - point.coords) <= point.distance then
+                        if not nearbyPoints[handle] then
+                            nearbyPoints[handle] = point
+                            if point.enter then
+                                point:enter()
+                            end
+                            if not nearbyThread then
+                                nearbyThread = true
+                                CreateThread(function()
+                                    while nearbyThread do Wait()
+                                        coords = PlayerObject:GetCoords()
+                                        for handle, point in pairs(nearbyPoints) do
+                                            if point.inside then
+                                                point:inside(#(coords - point.coords))
+                                            end
+                                        end
+                                    end
+                                end)
+                            end
+                        end
+                    elseif nearbyPoints[handle] then
+                        UnloadPoint(handle, point)
+                    end
+                end
+            end
+        end)
+    end
 end
 
 function Point:delete()
-	ESX.RemovePointInternal(self.handle)
+    points[self.handle] = nil
+    if #points == 0 then
+        mainThread = false
+    end
+    if nearbyPoints[self.handle] then
+        UnloadPoint(self.handle, self)
+    end
 end
 
 function Point:toggle(hidden)
-	if hidden == nil then
-		hidden = not self.hidden
-	end
-	self.hidden = hidden
-	ESX.HidePointInternal(self.handle, hidden)
+    if hidden == nil then
+        hidden = not self.hidden
+    end
+    self.hidden = hidden 
 end
 
 return Point
