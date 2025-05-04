eb26aeac0e563be4e3a6b19177118ce51f195428
RK <rkfrmda3@gmail.com>
2025-05-04 10:46:23 -0500

    Delete [core]/esx_multicharacter/html directory

--- [core]/esx_multicharacter/html/locales/it.js
+++
@@ -1,8 +0,0 @@
-const translate = new Object();
-
-translate.name = "Nome";
-translate.job = "Lavoro";
-translate.bank = "Banca";
-translate.money = "Contanti";
-translate.gender = "Genere";
-translate.dob = "Data di nascita";
