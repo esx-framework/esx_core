4dd6df146c541f71d6a0000172c835ea37a0e2c7
RK <rkfrmda3@gmail.com>
2025-05-05 02:37:08 -0500

    Add files via upload

---
+++ [core]/esx_multicharacter/web/src/vue-shims.d.ts
@@ -0,0 +1,14 @@
+declare module 'vue' {
+  import { CompatVue } from '@vue/runtime-dom'
+  const Vue: CompatVue
+  export default Vue
+  export * from '@vue/runtime-dom'
+  const { configureCompat } = Vue
+  export { configureCompat }
+}
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+} 
