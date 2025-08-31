69fe76ca38fb4f1f9474d1f6b45085bbf7f84310
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-31 15:10:54 +0000

    chore: bump manifest version to 1.13.4

--- [core]/esx_chat_theme/fxmanifest.lua
+++ [core]/esx_chat_theme/fxmanifest.lua
@@ -1,4 +1,4 @@
-version '1.13.3'
+version '1.13.4'
 author 'ESX-Framework'
 description 'A ESX Stylised theme for the chat resource.'
 
--- [core]/skinchanger/fxmanifest.lua
+++ [core]/skinchanger/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'adamant'
 
 game 'gta5'
 description 'Saves/loads character appearances for ESX Legacy.'
-version '1.13.3'
+version '1.13.4'
 lua54 'yes'
 
 client_scripts {
