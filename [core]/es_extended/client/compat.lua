b7153d4ffa8d5efc20031c1bcb2b624f5902911a
Stane034 <mihailostanimirovic13@gmail.com>
2025-10-25 09:44:06 +0200

    moved ESX.Streaming to esx_lib

--- [core]/es_extended/client/compat.lua
+++ [core]/es_extended/client/compat.lua
@@ -1,2 +1,8 @@
---[[ All client-side functions outsourced from the Core to the lib will be stored here for compatability, e.g:
---]]
+--All client-side functions outsourced from the Core to the lib will be stored here for compatability, e.g:
+
+ESX.Streaming.RequestModel = xLib.requestModel
+ESX.Streaming.RequestStreamedTextureDict = xLib.requestStreamedTextureDict 
+ESX.Streaming.RequestNamedPtfxAsset = xLib.requestNamedPtfxAsset
+ESX.Streaming.RequestAnimSet = xLib.requestAnimSet
+ESX.Streaming.RequestAnimDict = xLib.requestAnimDict
+ESX.Streaming.RequestWeaponAsset = xLib.requestWeaponAsset
--- [core]/esx_lib/resource/streaming/client.lua
+++ [core]/esx_lib/resource/streaming/client.lua
@@ -1,9 +1,7 @@
-ESX.Streaming = {}
-
 ---@param modelHash number | string
 ---@param cb? function
 ---@return number | nil
-function ESX.Streaming.RequestModel(modelHash, cb)
+xLib.requestModel = function(modelHash, cb)
     modelHash = type(modelHash) == "number" and modelHash or joaat(modelHash)
 
     if not IsModelInCdimage(modelHash) then return end
@@ -17,7 +15,7 @@ end
 ---@param textureDict string
 ---@param cb? function
 ---@return string | nil
-function ESX.Streaming.RequestStreamedTextureDict(textureDict, cb)
+xLib.requestStreamedTextureDict = function(textureDict, cb)
 	RequestStreamedTextureDict(textureDict, false)
 
 	while not HasStreamedTextureDictLoaded(textureDict) do Wait(500) end
@@ -28,7 +26,7 @@ end
 ---@param assetName string
 ---@param cb? function
 ---@return string | nil
-function ESX.Streaming.RequestNamedPtfxAsset(assetName, cb)
+xLib.requestNamedPtfxAsset = function(assetName, cb)
 	RequestNamedPtfxAsset(assetName)
 
 	while not HasNamedPtfxAssetLoaded(assetName) do Wait(500) end
@@ -39,7 +37,7 @@ end
 ---@param animSet string
 ---@param cb? function
 ---@return string | nil
-function ESX.Streaming.RequestAnimSet(animSet, cb)
+xLib.requestAnimSet = function(animSet, cb)
 	RequestAnimSet(animSet)
 
 	while not HasAnimSetLoaded(animSet) do Wait(500) end
@@ -50,7 +48,7 @@ end
 ---@param animDict string
 ---@param cb? function
 ---@return string | nil
-function ESX.Streaming.RequestAnimDict(animDict, cb)
+xLib.requestAnimDict = function(animDict, cb)
 	RequestAnimDict(animDict)
 
 	while not HasAnimDictLoaded(animDict) do Wait(500) end
@@ -61,10 +59,21 @@ end
 ---@param weaponHash number | string
 ---@param cb? function
 ---@return string | number | nil
-function ESX.Streaming.RequestWeaponAsset(weaponHash, cb)
+xLib.requestWeaponAsset = function(weaponHash, cb)
 	RequestWeaponAsset(weaponHash, 31, 0)
 
 	while not HasWeaponAssetLoaded(weaponHash) do Wait(500) end
 
 	return cb and cb(weaponHash) or weaponHash
+end
+
+---@param bankName string
+---@param cb? function
+---@return string | nil
+xLib.requestAudioBank = function(bankName, cb)
+    RequestAudioBank(bankName, false) 
+
+    while not RequestScriptAudioBank(bankName, false) do Wait(500) end
+
+    return cb and cb(bankName) or bankName
 end
