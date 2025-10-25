d5f8feafae091d5c69c996100b845645b0dcd351
Stane034 <mihailostanimirovic13@gmail.com>
2025-10-26 00:14:41 +0200

    Moved Timeouts and Await to esx_lib

--- [core]/es_extended/server/compat.lua
+++ [core]/es_extended/server/compat.lua
@@ -1,2 +1,6 @@
 --[[ All server-side functions outsourced from the Core to the lib will be stored here for compatability, e.g:
 --]]
+
+ESX.SetTimeout = xLib.timeout.setTimeout
+ESX.ClearTimeout = xLib.timeout.clearTimeout
+ESX.Await = xLib.waitFor
