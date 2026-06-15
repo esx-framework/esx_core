d4f1d34182fab42ce6cd2774c9256fec60357616
Ihsan <122683406+rwixy@users.noreply.github.com>
2026-06-15 19:46:39 +0530

    Refactor math functions: move to compat.lua and remove math.lua

--- [core]/es_extended/shared/compat.lua
+++ [core]/es_extended/shared/compat.lua
@@ -1,1 +1,9 @@
 --All shared functions outsourced from the Core to the lib will be stored here for compatability, e.g:
+
+ESX.Math = {}
+
+ESX.Math.Round = xLib.math.Round
+ESX.Math.GroupDigits = xLib.math.GroupDigits
+ESX.Math.Trim = xLib.math.Trim
+ESX.Math.Random = xLib.math.Random
+ESX.Math.GetHeadingFromCoords = xLib.math.GetHeadingFromCoords
