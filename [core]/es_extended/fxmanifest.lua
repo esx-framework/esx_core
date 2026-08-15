b7efa23ebba19539f850c47940be6181a7d17b7a
_Not_ <160342111+N0tNvll@users.noreply.github.com>
2026-08-15 14:37:43 -0500

    Bump version from 1.14.0 to 1.14.1

--- [core]/es_extended/fxmanifest.lua
+++ [core]/es_extended/fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'cerulean'
 game 'gta5'
 description 'The Core resource that provides the functionalities for all other resources.'
 lua54 'yes'
-version '1.14.0'
+version '1.14.1'
 
 shared_scripts {
 	'@esx_lib/imports.lua',
